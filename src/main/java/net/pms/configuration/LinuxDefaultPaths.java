cfa5f68bf162ea1e1a26f1488c875d4debaf5400
SubJunk <subjunk@gmail.com>
2012-06-14 14:12:02 +1200

    LinuxDefaultPaths: return either absolute tool's path if executable found in project.binaries.dir or short to search in system-wide PATH.
    From https://github.com/ps3mediaserver/ps3mediaserver/commit/ca52785597465072adaa00c42907655c5cbe50a3

--- src/main/java/net/pms/configuration/LinuxDefaultPaths.java
+++ src/main/java/net/pms/configuration/LinuxDefaultPaths.java
@@ -2,50 +2,54 @@ package net.pms.configuration;
 
 import net.pms.util.PropertiesUtil;
 
+import java.io.File;
+
 class LinuxDefaultPaths implements ProgramPaths {
+	private final String BINARIES_SEARCH_PATH = getBinariesSearchPath();
+
 	@Override
 	public String getEac3toPath() {
-		return "eac3to";
+		return getBinaryPath("eac3to");
 	}
 
 	@Override
 	public String getFfmpegPath() {
-		return "ffmpeg";
+		return getBinaryPath("ffmpeg");
 	}
 
 	@Override
 	public String getFlacPath() {
-		return "flac";
+		return getBinaryPath("flac");
 	}
 
 	@Override
 	public String getMencoderPath() {
-		return "mencoder";
+		return getBinaryPath("mencoder");
 	}
 
 	@Override
 	public String getMplayerPath() {
-		return "mplayer";
+		return getBinaryPath("mplayer");
 	}
 
 	@Override
 	public String getTsmuxerPath() {
-		return getBinariesPath() + "linux/tsMuxeR";
+		return getBinaryPath("tsMuxeR");
 	}
 
 	@Override
 	public String getVlcPath() {
-		return "vlc";
+		return getBinaryPath("vlc");
 	}
 
 	@Override
 	public String getDCRaw() {
-		return "dcraw";
+		return getBinaryPath("dcraw");
 	}
 	
 	@Override
 	public String getIMConvertPath() {
-		return "convert";
+		return getBinaryPath("convert");
 	}
 
 	@Override
@@ -60,7 +64,7 @@ class LinuxDefaultPaths implements ProgramPaths {
 	 *
 	 * @return The path for binaries.
 	 */
-	private String getBinariesPath() {
+	private String getBinariesSearchPath() {
 		String path = PropertiesUtil.getProjectProperties().get("project.binaries.dir");
 
 		if (path != null && !"".equals(path)) {
@@ -71,6 +75,23 @@ class LinuxDefaultPaths implements ProgramPaths {
 			}
 		} else {
 			return "";
+		}
+	}
+
+	/**
+	 * Returns the path to requested binary tool.
+	 * Either absolute if executable found in project.binaries.dir or
+	 * short to search in system-wide  PATH.
+	 *
+	 * @param tool The name of binary tool
+	 * @return Path to binary
+	 */
+	private String getBinaryPath(String tool) {
+		File f = new File(BINARIES_SEARCH_PATH + tool);
+		if (f.canExecute()) {
+			return BINARIES_SEARCH_PATH + tool;
+		} else {
+			return tool;
 		}
 	}
 }
