90cfdf8753dcb9ba6403b6d8e9db42791d3c1b8f
SubJunk <subjunk@gmail.com>
2012-06-18 11:39:58 +1200

    Continued previous revision

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -562,7 +562,6 @@ public class PMS {
 		return mediaLibrary;
 	}
 
-	// FIXME this is duplicated in net/pms/encoders/PlayerFactory.java
 	private SystemUtils createSystemUtils() {
 		if (Platform.isWindows()) {
 			return new WinUtils();
