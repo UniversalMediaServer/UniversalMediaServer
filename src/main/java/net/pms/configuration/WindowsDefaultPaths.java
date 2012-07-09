9256ecee94f9699cff3f91dbd3c5f5b266f07a59
SubJunk <subjunk@gmail.com>
2012-07-09 14:25:39 +1200

    Removed 64-bit FFmpeg
    Not a good idea to introduce 64-bit with the memory problems we already have with 32-bit applications

--- src/main/java/net/pms/configuration/WindowsDefaultPaths.java
+++ src/main/java/net/pms/configuration/WindowsDefaultPaths.java
@@ -1,7 +1,6 @@
 package net.pms.configuration;
 
 import java.io.File;
-import net.pms.PMS;
 import net.pms.util.PropertiesUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,9 +15,6 @@ class WindowsDefaultPaths implements ProgramPaths {
 
 	@Override
 	public String getFfmpegPath() {
-		if (PMS.getOSBitness() == 64) {
-			return getBinariesPath() + "win32/ffmpeg64.exe";
-		}
 		return getBinariesPath() + "win32/ffmpeg.exe";
 	}
 
