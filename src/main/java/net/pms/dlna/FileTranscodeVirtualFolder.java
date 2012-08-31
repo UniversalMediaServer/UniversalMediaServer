5e9eda5d6ac124f43b1d6d6022b4b02d948a166a
SubJunk <subjunk@gmail.com>
2012-09-01 11:26:53 +1200

    Use generic List instead of ArrayList as return type
    Based on https://github.com/ps3mediaserver/ps3mediaserver/commit/5b526aaf9ef4e4e8c1fae9dcfe2776cdbae45b0e

--- src/main/java/net/pms/dlna/FileTranscodeVirtualFolder.java
+++ src/main/java/net/pms/dlna/FileTranscodeVirtualFolder.java
@@ -21,6 +21,7 @@ package net.pms.dlna;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import net.pms.PMS;
 import net.pms.configuration.RendererConfiguration;
 import net.pms.dlna.virtual.VirtualFolder;
@@ -127,8 +128,8 @@ public class FileTranscodeVirtualFolder extends VirtualFolder {
 			// List holding all combinations
 			ArrayList<DLNAResource> combos = new ArrayList<DLNAResource>();
 
-			ArrayList<DLNAMediaAudio> audioTracks = child.getMedia().getAudioTracksList();
-			ArrayList<DLNAMediaSubtitle> subtitles = child.getMedia().getSubtitleTracksList();
+			List<DLNAMediaAudio> audioTracks = child.getMedia().getAudioTracksList();
+			List<DLNAMediaSubtitle> subtitles = child.getMedia().getSubtitleTracksList();
 
 			// Make sure a combo with no subtitles will be added
 			DLNAMediaSubtitle noSubtitle = new DLNAMediaSubtitle();
