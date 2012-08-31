7c95f8f0801f3077ddd74bd569ed5af1e3b0c9e6
SubJunk <subjunk@gmail.com>
2012-08-31 19:49:58 +1200

    Added tests for detection of Russian and Chinese charsets
    Based on https://github.com/ps3mediaserver/ps3mediaserver/commit/c959c04b9e7488016ddb600876e00675bc3d7742

---
+++ src/test/java/net/pms/util/FileUtilTest.java
@@ -0,0 +1,68 @@
+/*
+ * PS3 Media Server, for streaming any medias to your PS3.
+ * Copyright (C) 2012  I. Sokolov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package net.pms.util;
+
+import java.io.File;
+import static org.fest.assertions.Assertions.assertThat;
+import org.junit.Test;
+import org.mozilla.universalchardet.Constants;
+
+public class FileUtilTest {
+	@Test
+	public void testGetFileCharset_WINDOWS_1251() throws Exception {
+		File file = new File(this.getClass().getResource("russian-cp1251.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_WINDOWS_1251);
+	}
+
+	@Test
+	public void testGetFileCharset_IBM866() throws Exception {
+		File file = new File(this.getClass().getResource("russian-ibm866.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_IBM866);
+	}
+
+	@Test
+	public void testGetFileCharset_KOI8_R() throws Exception {
+		File file = new File(this.getClass().getResource("russian-koi8-r.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_KOI8_R);
+	}
+
+	@Test
+	public void testGetFileCharset_UTF8_without_BOM() throws Exception {
+		File file = new File(this.getClass().getResource("russian-utf8-without-bom.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_UTF_8);
+	}
+
+	@Test
+	public void testGetFileCharset_UTF8_with_BOM() throws Exception {
+		File file = new File(this.getClass().getResource("russian-utf8-with-bom.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_UTF_8);
+	}
+
+	@Test
+	public void testGetFileCharset_BIG5() throws Exception {
+		File file = new File(this.getClass().getResource("chinese-gb18030.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_GB18030);
+	}
+
+	@Test
+	public void testGetFileCharset_GB2312() throws Exception {
+		File file = new File(this.getClass().getResource("chinese-big5.srt").getFile());
+		assertThat(FileUtil.getFileCharset(file)).isEqualTo(Constants.CHARSET_BIG5);
+	}
+}
