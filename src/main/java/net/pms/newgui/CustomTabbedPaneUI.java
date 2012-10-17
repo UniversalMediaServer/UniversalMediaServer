c53270328c55f2f480bd94918582a57f7417f433
SubJunk <subjunk@gmail.com>
2012-10-17 15:48:14 +1300

    Slightly dim the text of inactive tabs to make the active one stand out more

--- src/main/java/net/pms/newgui/CustomTabbedPaneUI.java
+++ src/main/java/net/pms/newgui/CustomTabbedPaneUI.java
@@ -4,12 +4,24 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.plaf.*;
 import javax.swing.plaf.basic.*;
+import sun.swing.SwingUtilities2;
 
 public class CustomTabbedPaneUI extends BasicTabbedPaneUI {
 	private int anchoFocoH = 4;
 
 	public static ComponentUI createUI(JComponent c) {
 		return new CustomTabbedPaneUI();
+	}
+
+	@Override
+	protected void paintText(Graphics g, int tabPlacement, Font font, FontMetrics metrics, int tabIndex, String title, Rectangle textRect, boolean isSelected) {
+		int mnemIndex = tabPane.getDisplayedMnemonicIndexAt(tabIndex);
+
+		if (isSelected) {
+			g.setColor(new Color(0, 0, 0));
+		}
+
+		SwingUtilities2.drawStringUnderlineCharAt(tabPane, g, title, mnemIndex, textRect.x, textRect.y + metrics.getAscent());
 	}
 
 	@Override
