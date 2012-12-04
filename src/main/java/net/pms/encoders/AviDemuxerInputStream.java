d260ac059a0812479c406f8aa8e20d4508b73b95
SubJunk <subjunk@gmail.com>
2012-12-04 16:38:07 +1300

    Revert "PMS: Enhanced ProcessWrapperLiteImpl to run a command and return the results"
    This reverts commit 5a278dd0a140735084ce55a018aabb541e0d7120.

--- src/main/java/net/pms/encoders/AviDemuxerInputStream.java
+++ src/main/java/net/pms/encoders/AviDemuxerInputStream.java
@@ -125,12 +125,19 @@ public class AviDemuxerInputStream extends InputStream {
 					pipe_process.runInNewThread();
 					tsPipe.deleteLater();
 
-					// Execute the command and ignore the result
 					String[] cmd = new String[]{ts.executable(), f.getAbsolutePath(), tsPipe.getInputPipe()};
-					ProcessWrapperLiteImpl pwl = new ProcessWrapperLiteImpl(cmd);
-					pwl.getResult();
+					ProcessBuilder pb = new ProcessBuilder(cmd);
+					process = pb.start();
+					ProcessWrapper pwi = new ProcessWrapperLiteImpl(process);
+					attachedProcesses.add(pwi);
+
+					// "Gob": a cryptic name for (e.g.) StreamGobbler - i.e. a stream
+					// consumer that reads and discards the stream
+					new Gob(process.getErrorStream()).start();
+					new Gob(process.getInputStream()).start();
 
 					realIS = tsPipe.getInputStream();
+					ProcessUtil.waitFor(process);
 					LOGGER.trace("tsMuxeR muxing finished");
 				} catch (IOException e) {
 					LOGGER.error(null, e);
