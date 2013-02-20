bc1fbdcb520b3b093b813982bc65c9c66da0158f
valib <liborvala@seznam.cz>
2013-02-20 01:01:32 +0100

    Changed FFMpeg debug info back to warning I again forgot to change back the testing setting

--- src/main/java/net/pms/encoders/FFMpegVideo.java
+++ src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -445,7 +445,7 @@ public class FFMpegVideo extends Player {
 		cmdList.add("-y");
 
 		cmdList.add("-loglevel");
-		cmdList.add("info");
+		cmdList.add("warning");
 
 		if (params.timeseek > 0) {
 			cmdList.add("-ss");
