4cd3d3ae3719027ab7ac306114bf9e42659b62dd
SubJunk <subjunk@gmail.com>
2013-04-12 18:49:54 +1200

    Only offer to share iTunes if we can find an iTunes library on the computer

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -23,6 +23,8 @@ import com.sun.jna.Platform;
 import java.awt.*;
 import java.io.*;
 import java.net.BindException;
+import java.net.URI;
+import java.net.URLDecoder;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Map.Entry;
@@ -633,19 +635,58 @@ public class PMS {
 
 				// Ask if they want to share their iTunes library
 				if (!(Platform.isMac() || Platform.isWindows())) {
-					int whetherToShareITunes = JOptionPane.showConfirmDialog(
-					(Component) PMS.get().getFrame(),
-					Messages.getString("Wizard.6"),
-					Messages.getString("Wizard.2") + " 3 " + Messages.getString("Wizard.4") + " " + numberOfQuestions,
-					JOptionPane.YES_NO_OPTION);
-					if (whetherToShareITunes == JOptionPane.YES_OPTION) {
-						configuration.setItunesEnabled(true);
-						NavigationShareTab.itunes.setSelected(true);
-						save();
-					} else if (whetherToShareITunes == JOptionPane.NO_OPTION) {
-						configuration.setItunesEnabled(false);
-						NavigationShareTab.itunes.setSelected(false);
-						save();
+					// Check if the iTunes library exists
+					String line;
+					String iTunesFile = null;
+					if (Platform.isMac()) {
+						Process process = Runtime.getRuntime().exec("defaults read com.apple.iApps iTunesRecentDatabases");
+						try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+							// we want the 2nd line
+							if ((line = in.readLine()) != null && (line = in.readLine()) != null) {
+								line = line.trim(); // remove extra spaces
+								line = line.substring(1, line.length() - 1); // remove quotes and spaces
+								URI tURI = new URI(line);
+								iTunesFile = URLDecoder.decode(tURI.toURL().getFile(), "UTF8");
+							}
+						}
+					} else if (Platform.isWindows()) {
+						Process process = Runtime.getRuntime().exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v \"My Music\"");
+						String location;
+						try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+							location = null;
+							while ((line = in.readLine()) != null) {
+								final String LOOK_FOR = "REG_SZ";
+								if (line.contains(LOOK_FOR)) {
+									location = line.substring(line.indexOf(LOOK_FOR) + LOOK_FOR.length()).trim();
+								}
+							}
+						}
+
+						if (location != null) {
+							// Add the iTunes folder to the end
+							location = location + "\\iTunes\\iTunes Music Library.xml";
+							iTunesFile = location;
+						} else {
+							LOGGER.info("Could not find the My Music folder");
+						}
+					}
+
+					if (iTunesFile != null && (new File(iTunesFile)).exists()) {
+						// The iTunes library does exist
+						int whetherToShareITunes = JOptionPane.showConfirmDialog(
+						(Component) PMS.get().getFrame(),
+						Messages.getString("Wizard.6"),
+						Messages.getString("Wizard.2") + " 3 " + Messages.getString("Wizard.4") + " " + numberOfQuestions,
+						JOptionPane.YES_NO_OPTION);
+						if (whetherToShareITunes == JOptionPane.YES_OPTION) {
+							configuration.setItunesEnabled(true);
+							NavigationShareTab.itunes.setSelected(true);
+							save();
+						} else if (whetherToShareITunes == JOptionPane.NO_OPTION) {
+							configuration.setItunesEnabled(false);
+							NavigationShareTab.itunes.setSelected(false);
+							save();
+						}
 					}
 				}
 
