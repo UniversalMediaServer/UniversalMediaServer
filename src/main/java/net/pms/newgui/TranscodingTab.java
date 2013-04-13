7b45e363c67e301c251496d7e56907308b993562
SubJunk <subjunk@gmail.com>
2013-04-13 18:59:27 +1200

    Put column and row stuff at the top
    Conflicts:
    	src/main/java/net/pms/newgui/TranscodingTab.java

--- src/main/java/net/pms/newgui/TranscodingTab.java
+++ src/main/java/net/pms/newgui/TranscodingTab.java
@@ -26,7 +26,6 @@ import com.sun.jna.Platform;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.Locale;
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -56,6 +55,14 @@ public class TranscodingTab {
 	private static final String LEFT_ROW_SPEC = "fill:10:grow, 3dlu, p, 3dlu, p, 3dlu, p";
 	private static final String MAIN_COL_SPEC = "left:pref, pref, 7dlu, pref, pref, fill:10:grow";
 	private static final String MAIN_ROW_SPEC = "fill:10:grow";
+
+	// Subtabs layout spacing
+	private static final String VIDEO_COL_SPEC = "left:pref, 3dlu, pref:grow"; // "left:pref, 2dlu, pref:grow"
+	private static final String VIDEO_ROW_SPEC = "$lgap, 2*(pref, 3dlu), 10dlu, 10dlu, 4*(pref, 3dlu), pref"; //"$lgap, 2*(pref, 2dlu), 10dlu, 10dlu, 3*(pref, 2dlu), pref";
+	private static final String AUDIO_COL_SPEC = "left:pref, 3dlu, pref:grow"; //"left:pref, 2dlu, pref:grow";
+	private static final String AUDIO_ROW_SPEC = "$lgap, pref, 3dlu, 5*(pref, 3dlu), pref, 12dlu, 3*(pref, 3dlu), pref:grow"; //"$lgap, pref, 2dlu, 4*(pref, 2dlu), pref, 12dlu, 3*(pref, 2dlu), pref:grow";
+	private static final String SUBTITLE_COL_SPEC = "left:pref, 3dlu, p:grow, 3dlu, right:p:grow, 3dlu, p:grow, 3dlu, right:p:grow,3dlu, p:grow, 3dlu, right:p:grow,3dlu, pref:grow"; //"left:pref, 3dlu, pref:grow, 3dlu, right:pref:grow, 3dlu, pref:grow, 3dlu, right:pref:grow, 3dlu, pref:grow, 3dlu, pref:grow";
+	private static final String SUBTITLE_ROW_SPEC = "$lgap, 11*(pref, 3dlu), pref"; //"$lgap, 3*(pref, 3dlu), pref";
 
 	private final PmsConfiguration configuration;
 	private ComponentOrientation orientation;
@@ -499,8 +506,8 @@ public class TranscodingTab {
 	}
 
 	private JComponent buildVideoSetupPanel() {
-		String colSpec = FormLayoutUtil.getColSpec("left:pref, 3dlu, pref:grow", orientation);
-		FormLayout layout = new FormLayout(colSpec, "$lgap, 2*(pref, 3dlu), 10dlu, 10dlu, 4*(pref, 3dlu), pref");
+		String colSpec = FormLayoutUtil.getColSpec(VIDEO_COL_SPEC, orientation);
+		FormLayout layout = new FormLayout(colSpec, VIDEO_ROW_SPEC);
 		PanelBuilder builder = new PanelBuilder(layout);
 		builder.border(Borders.DLU4);
 		CellConstraints cc = new CellConstraints();
@@ -626,8 +633,8 @@ public class TranscodingTab {
 	}
 
 	private JComponent buildAudioSetupPanel() {
-		String colSpec = FormLayoutUtil.getColSpec("left:pref, 3dlu, pref:grow", orientation);
-		FormLayout layout = new FormLayout(colSpec, "$lgap, pref, 3dlu, 5*(pref, 3dlu), pref, 12dlu, 3*(pref, 3dlu), pref:grow");
+		String colSpec = FormLayoutUtil.getColSpec(AUDIO_COL_SPEC, orientation);
+		FormLayout layout = new FormLayout(colSpec, AUDIO_ROW_SPEC);
 		PanelBuilder builder = new PanelBuilder(layout);
 		builder.border(Borders.DLU4);
 		CellConstraints cc = new CellConstraints();
@@ -729,8 +736,8 @@ public class TranscodingTab {
 	}
 
 	private JComponent buildSubtitlesSetupPanel() {
-		String colSpec = FormLayoutUtil.getColSpec("left:pref, 3dlu, p:grow, 3dlu, right:p:grow, 3dlu, p:grow, 3dlu, right:p:grow,3dlu, p:grow, 3dlu, right:p:grow,3dlu, pref:grow", orientation);
-		FormLayout layout = new FormLayout(colSpec, "$lgap, 11*(pref, 3dlu), pref");
+		String colSpec = FormLayoutUtil.getColSpec(SUBTITLE_COL_SPEC, orientation);
+		FormLayout layout = new FormLayout(colSpec, SUBTITLE_ROW_SPEC);
 		final PanelBuilder builder = new PanelBuilder(layout);
 		builder.border(Borders.DLU4);
 		CellConstraints cc = new CellConstraints();
