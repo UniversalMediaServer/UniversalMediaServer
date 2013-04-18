1c9444a321bad5eabaed6179ea99e181c2a9922a
SubJunk <subjunk@gmail.com>
2013-04-19 09:25:38 +1200

    Formatting

--- src/main/java/net/pms/configuration/PmsConfiguration.java
+++ src/main/java/net/pms/configuration/PmsConfiguration.java
@@ -161,6 +161,9 @@ public class PmsConfiguration {
 	private static final String KEY_MENCODER_USE_PCM_FOR_HQ_AUDIO_ONLY = "mencoder_usepcm_for_hq_audio_only";
 	private static final String KEY_MENCODER_VOBSUB_SUBTITLE_QUALITY = "mencoder_vobsub_subtitle_quality";
 	private static final String KEY_MENCODER_YADIF = "mencoder_yadif";
+	private static final String KEY_MIN_PLAY_TIME = "min_playtime";
+	private static final String KEY_MIN_PLAY_TIME_WEB = "min_playtime_web";
+	private static final String KEY_MIN_PLAY_TIME_FILE = "min_playtime_file";
 	private static final String KEY_MINIMIZED = "minimized";
 	private static final String KEY_MIN_MEMORY_BUFFER_SIZE = "minvideobuffer";
 	private static final String KEY_MIN_STREAM_BUFFER = "minwebbuffer";
@@ -182,9 +185,6 @@ public class PmsConfiguration {
 	private static final String KEY_RENDERER_DEFAULT = "renderer_default";
 	private static final String KEY_RENDERER_FORCE_DEFAULT = "renderer_force_default";
 	private static final String KEY_RESUMABLE = "resume";
-	private static final String KEY_MIN_PLAY_TIME = "min_playtime";
-	private static final String KEY_MIN_PLAY_TIME_WEB = "min_playtime_web";
-	private static final String KEY_MIN_PLAY_TIME_FILE = "min_playtime_file";
 	private static final String KEY_RUN_WIZARD = "run_wizard";
 	private static final String KEY_SEARCH_FOLDER = "search_folder";
 	private static final String KEY_SEARCH_RECURSE = "search_recurse";
