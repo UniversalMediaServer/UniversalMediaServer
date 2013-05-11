9120e77d01439e1ec2ee79d611c4dbac7b7a49e4
valib <liborvala@seznam.cz>
2013-05-11 12:26:38 +0200

    Revert Commit fbb659f

--- src/main/java/net/pms/encoders/MEncoderVideo.java
+++ src/main/java/net/pms/encoders/MEncoderVideo.java
@@ -1042,10 +1042,6 @@ public class MEncoderVideo extends Player {
 		String rendererMencoderOptions = params.mediaRenderer.getCustomMencoderOptions(); // default: empty string
 		String globalMencoderOptions = configuration.getMencoderCustomOptions(); // default: empty string
 
-		if (params.mediaRenderer.isKeepAspectRatio()) {
-			rendererMencoderOptions += " -vf softskip,expand=:::::16/9:4";
-		}
-
 		String combinedCustomOptions = defaultString(globalMencoderOptions)
 			+ " "
 			+ defaultString(rendererMencoderOptions);
