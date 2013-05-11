5c1851d9c96729b08228e914c0ec180262eb0482
SubJunk <subjunk@gmail.com>
2013-05-11 13:21:48 +1200

    Increased bandwidth limit for wireless connections

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -387,7 +387,7 @@ public class PMS {
 					save();
 				} else if (networkType == JOptionPane.CANCEL_OPTION) {
 					// Wireless
-					configuration.setMaximumBitrate("30");
+					configuration.setMaximumBitrate("50");
 					configuration.setMPEG2MainSettings("Automatic (Wireless)");
 					save();
 				}
