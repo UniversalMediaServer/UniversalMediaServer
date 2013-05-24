5b8060f2e84554af4b3c31944de98effd5bb0ca0
valib <liborvala@seznam.cz>
2013-05-25 00:21:14 +0200

    Extend UniversalDetector

--- src/main/java/net/pms/formats/v2/SubtitleUtils.java
+++ src/main/java/net/pms/formats/v2/SubtitleUtils.java
@@ -26,6 +26,7 @@ import net.pms.PMS;
 import net.pms.configuration.PmsConfiguration;
 import net.pms.dlna.DLNAMediaSubtitle;
 import net.pms.io.OutputParams;
+import static net.pms.util.universalchardet.ConstantsExtended.*;
 import static org.apache.commons.io.FilenameUtils.getBaseName;
 import static org.apache.commons.lang3.StringUtils.*;
 import static org.mozilla.universalchardet.Constants.*;
@@ -52,6 +53,8 @@ public class SubtitleUtils {
 			// Greek
 			put(CHARSET_WINDOWS_1253, "cp1253");
 			put(CHARSET_ISO_8859_7, "ISO-8859-7");
+			// Central Europe
+			put(CHARSET_ISO_8859_2, "ISO-8859-2");
 			// Western Europe
 			put(CHARSET_WINDOWS_1252, "cp1252");
 			// Hebrew
@@ -70,6 +73,8 @@ public class SubtitleUtils {
 			put(CHARSET_ISO_2022_JP, "ISO-2022-JP");
 			put(CHARSET_EUC_JP, "euc-jp");
 			put(CHARSET_SHIFT_JIS, "shift-jis");
+			// Turkish
+			put(CHARSET_ISO_8859_9, "ISO-8859-9");
 		}
 	};
 
---
+++ src/main/java/net/pms/util/universalchardet/Win1252SpanishModel.java
@@ -0,0 +1,41 @@
+// based on https://github.com/PyYoshi/cChardet.git
+
+package net.pms.util.universalchardet;
+
+import org.mozilla.universalchardet.Constants;
+import net.pms.util.universalchardet.SpanishModel;
+
+public class Win1252SpanishModel extends SpanishModel {
+public static final float TYPICAL_POSITIVE_RATIO = 0.983906f;
+	
+
+	public Win1252SpanishModel() { 
+		super(spanishhWin1252CharToOrderMap, Constants.CHARSET_WINDOWS_1252);
+	}
+
+	/**
+	255: Control characters that usually does not exist in any text
+	254: Carriage/Return
+	253: symbol (punctuation) that does not belong to word
+	252: 0 - 9
+	 */
+
+	private static final short[] spanishhWin1252CharToOrderMap = new short[] {
+		255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+		255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+		255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+		255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+		255, 32, 44, 29, 38, 26, 48, 49, 53, 37, 55, 63, 31, 35, 40, 46,
+		 30, 58, 36, 33, 41, 47, 54, 52, 64, 51, 67,255,255,255,255,255,
+		255, 2, 15, 11, 9, 1, 19, 16, 21, 6, 27, 43, 8, 13, 4, 3,
+		 14, 22, 7, 5, 10, 12, 17, 42, 39, 18, 24,255,255,255,255,255,
+		180,179,178,177,176, 71,175,174,173,172,171,170,169,168,167,166,
+		165, 76, 70, 61, 62,164, 57,163,162,161,160,159,158,157,156,155,
+		154, 56,153,152,151,150,149,148,147, 79, 73, 59,146,145,144,143,
+		 83, 82,142,141,140,139,138,137,136,135, 78, 60,134,133,132, 50,
+		131, 72,130,129,128,127,126,125,124, 66,123,122,121, 68,120,119,
+		118, 74,117, 69,116,115,114,113,112,111, 81,110,109,108,107,106,
+		105, 25,104,103,102,101,100, 77, 75, 28, 99, 98, 97, 23, 96, 95,
+		 94, 34, 80, 20, 93, 92, 91, 90, 89, 88, 45, 87, 65, 86, 85, 84,
+	};
+}
