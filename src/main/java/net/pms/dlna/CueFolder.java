784e55bf4db834e3ba7f1a55e04566a95b8c3726
SubJunk <subjunk@gmail.com>
2013-06-14 15:43:06 +1200

    Fix for NullPointerException
    From https://github.com/ps3mediaserver/ps3mediaserver/commit/761a0632ed76edc62ca87f941572925c453cf1fa

--- src/main/java/net/pms/dlna/CueFolder.java
+++ src/main/java/net/pms/dlna/CueFolder.java
@@ -1,3 +1,21 @@
+/*
+ * PS3 Media Server, for streaming any medias to your PS3.
+ * Copyright (C) 2008  A.Brochard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package net.pms.dlna;
 
 import java.io.File;
@@ -8,6 +26,7 @@ import java.util.List;
 import jwbroek.cuelib.*;
 import net.pms.PMS;
 import net.pms.configuration.PmsConfiguration;
+import net.pms.dlna.Range.Time;
 import net.pms.encoders.MEncoderVideo;
 import net.pms.encoders.MPlayerAudio;
 import net.pms.encoders.Player;
@@ -166,17 +185,18 @@ public class CueFolder extends DLNAResource {
 					}
 
 					if (tracks.size() > 0 && addedResources.size() > 0) {
-						// last track
-						DLNAResource prec = addedResources.get(addedResources.size() - 1);
-						try {
-							prec.getSplitRange().setEnd(prec.getMedia().getDurationInSeconds());
-							prec.getMedia().setDuration(prec.getSplitRange().getDuration());
-						} catch (NullPointerException e) { }
-						LOGGER.debug("Track #" + childrenNumber() + " split range: " + prec.getSplitRange().getStartOrZero() + " - " + prec.getSplitRange().getDuration());
+						DLNAResource lastTrack = addedResources.get(addedResources.size() - 1);
+						Time lastTrackSplitRange = lastTrack.getSplitRange();
+						DLNAMediaInfo lastTrackMedia = lastTrack.getMedia();
+
+						if (lastTrackSplitRange != null && lastTrackMedia != null) {
+							lastTrackSplitRange.setEnd(lastTrackMedia.getDurationInSeconds());
+							lastTrackMedia.setDuration(lastTrackSplitRange.getDuration());
+							LOGGER.debug("Track #" + childrenNumber() + " split range: " + lastTrackSplitRange.getStartOrZero() + " - " + lastTrackSplitRange.getDuration());
+						}
 					}
 
 					PMS.get().storeFileInCache(playlistfile, Format.PLAYLIST);
-
 				}
 			}
 		}
