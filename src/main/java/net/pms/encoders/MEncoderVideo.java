9b06be891b5ba18726b06ae1f40455bbd38976a9
SubJunk <subjunk@gmail.com>
2013-09-22 17:39:25 +1200

    Support muxing WEB-DL files with tsMuxeR on Windows

--- src/main/java/net/pms/encoders/MEncoderVideo.java
+++ src/main/java/net/pms/encoders/MEncoderVideo.java
@@ -919,7 +919,10 @@ public class MEncoderVideo extends Player {
 				intOCW == 0 &&
 				intOCH == 0
 			) &&
-			!filename.contains("WEB-DL") &&
+			!(
+				filename.contains("WEB-DL") &&
+				!Platform.isWindows()
+			) &&
 			aspectRatiosMatch
 		) {
 			String expertOptions[] = getSpecificCodecOptions(
