2341cd2026094d3d5cc01301c97b1ee69c8533c6
valib <liborvala@seznam.cz>
2013-11-26 18:55:55 +0100

    Revert "Temporary fix for commas in duration strings"
    This reverts commit 0b748128bcc15487d1c5b2226696da76abaf2a45.

--- src/main/java/net/pms/dlna/DLNAMediaInfo.java
+++ src/main/java/net/pms/dlna/DLNAMediaInfo.java
@@ -1115,10 +1115,7 @@ public class DLNAMediaInfo implements Cloneable {
 	 * @deprecated Use {@link #StringUtil.convertTimeToString(durationSec, StringUtil.DURATION_TIME_FORMAT)} instead.
 	 */
 	public static String getDurationString(double d) {
-		int s = ((int) d) % 60;
-		int h = (int) (d / 3600);
-		int m = ((int) (d / 60)) % 60;
-		return String.format("%02d:%02d:%02d.00", h, m, s);
+		return convertTimeToString(d, DURATION_TIME_FORMAT);
 	}
 
 	public static Double parseDurationString(String duration) {
