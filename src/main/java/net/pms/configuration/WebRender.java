90fbf57636d5a497d2705c89b9b2f5dd07b07153
SubJunk <subjunk@gmail.com>
2013-12-04 13:16:50 +1300

    Formatting

--- src/main/java/net/pms/configuration/WebRender.java
+++ src/main/java/net/pms/configuration/WebRender.java
@@ -1,67 +1,68 @@
 package net.pms.configuration;
 
+import java.net.InetAddress;
 import net.pms.Messages;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.lang.StringUtils;
 
-import java.net.InetAddress;
-
 public class WebRender extends RendererConfiguration {
+	private String name;
+	private String ip;
+	private int port;
+	private String ua;
 
-    private String name;
-    private String ip;
-    private int port;
-    private String ua;
+	public WebRender(String name) throws ConfigurationException {
+		super(null);
+		this.name = name;
+		ip = "";
+		port = 0;
+		ua = "";
+	}
 
-    public WebRender(String name)  throws ConfigurationException {
-        super(null);
-        this.name = name;
-        ip = "";
-        port = 0;
-        ua = "";
-    }
+	@Override
+	public String getRendererName() {
+		String ipStr;
+		if (StringUtils.isNotEmpty(ip)) {
+			ipStr = "\n(" + ip + (port != 0 ? ":" + port : "") + ")";
+		} else {
+			ipStr = "";
+		}
+		return name + "@\n" + Messages.getString("PMS.142") + ipStr;
+	}
 
-    public String getRendererName() {
-        String ipStr;
-        if (StringUtils.isNotEmpty(ip)) {
-            ipStr = "\n(" + ip + (port != 0 ? ":" + port : "") + ")";
-        }
-        else {
-            ipStr = "";
-        }
-        return name + "@\n" + Messages.getString("PMS.142") + ipStr;
-    }
+	@Override
+	public void associateIP(InetAddress sa) {
+		super.associateIP(sa);
+		ip = sa.getHostAddress();
+	}
 
-    public void associateIP(InetAddress sa) {
-        super.associateIP(sa);
-        ip = sa.getHostAddress();
-    }
+	public void associatePort(int port) {
+		this.port = port;
+	}
 
-    public void associatePort(int port) {
-        this.port = port;
-    }
+	public void setUA(String ua) {
+		this.ua = ua.toLowerCase();
+	}
 
-    public void setUA(String ua) {
-        this.ua = ua.toLowerCase();
-    }
+	@Override
+	public String getRendererIcon() {
+		if (StringUtils.isEmpty(ua)) {
+			return super.getRendererIcon();
+		}
+		if (ua.contains("chrome")) {
+			return "chrome.png";
+		}
+		if (ua.contains("msie")) {
+			return "ie.png";
+		}
+		if (ua.contains("firefox")) {
+			return "ff.png";
+		}
+		return super.getRendererIcon();
+	}
 
-    public String getRendererIcon() {
-        if (StringUtils.isEmpty(ua)) {
-            return super.getRendererIcon();
-        }
-        if (ua.contains("chrome")) {
-            return "chrome.png";
-        }
-        if (ua.contains("msie")) {
-            return "ie.png";
-        }
-        if(ua.contains("firefox")) {
-           return "ff.png";
-        }
-        return super.getRendererIcon();
-    }
-
-    public String toString() {
-        return getRendererName();
-    }
+	@Override
+	public String toString() {
+		return getRendererName();
+	}
 }
