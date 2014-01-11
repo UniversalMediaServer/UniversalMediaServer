5b30b8f369f1e93e81f5239394cf7c7f88067761
SubJunk <subjunk@gmail.com>
2014-01-12 10:16:25 +1300

    Updated copyright year

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -344,7 +344,7 @@ public class PMS {
 
 	private void displayBanner() throws IOException {
 		LOGGER.info("Starting " + PropertiesUtil.getProjectProperties().get("project.name") + " " + getVersion());
-		LOGGER.info("Based on PS3 Media Server by shagrath, copyright 2008-2013");
+		LOGGER.info("Based on PS3 Media Server by shagrath, copyright 2008-2014");
 		LOGGER.info("http://www.universalmediaserver.com");
 		LOGGER.info("");
 
