83fd0999cc4156fecf59df9a6db7281f553a7fc9
valib <liborvala@seznam.cz>
2014-05-15 01:16:06 +0200

    Fix the issue at http://www.universalmediaserver.com/forum/viewtopic.php?f=12&t=1707

--- src/main/java/net/pms/configuration/RendererConfiguration.java
+++ src/main/java/net/pms/configuration/RendererConfiguration.java
@@ -1027,9 +1027,8 @@ public class RendererConfiguration {
 	 * @return The maximum video width.
 	 */
 	public int getMaxVideoWidth() {
-		// FIXME why is this 1920 if the default value is 0 (unlimited)?
-		// XXX we should also require width and height to both be 0 or both be > 0
-		return getInt(MAX_VIDEO_WIDTH, 1920);
+		// XXX we should require width and height to both be 0 or both be > 0
+		return getInt(MAX_VIDEO_WIDTH, 0);
 	}
 
 	/**
@@ -1039,9 +1038,8 @@ public class RendererConfiguration {
 	 * @return The maximum video height.
 	 */
 	public int getMaxVideoHeight() {
-		// FIXME why is this 1080 if the default value is 0 (unlimited)?
-		// XXX we should also require width and height to both be 0 or both be > 0
-		return getInt(MAX_VIDEO_HEIGHT, 1080);
+		// XXX we should require width and height to both be 0 or both be > 0
+		return getInt(MAX_VIDEO_HEIGHT, 0);
 	}
 
 	/**
