49fdbcdd59f915f53d83fcf42bfdf671d6b5e4a0
Dreamcat4 <dreamcat4@gmail.com>
2014-07-03 07:31:56 +0100

    Get past exception point on #334 @subjunk

--- src/main/java/net/pms/newgui/DbgPacker.java
+++ src/main/java/net/pms/newgui/DbgPacker.java
@@ -37,7 +37,12 @@ public class DbgPacker implements ActionListener {
 		init = true;
 		items = new LinkedHashMap<>();
 		debug_log = LoggingConfigFileLoader.getLogFilePaths().get("debug.log");
-		dbg_zip = debug_log.replace("debug.log", "ums_dbg.zip");
+
+		if (debug_log != null) {
+			dbg_zip = debug_log.replace("debug.log", "ums_dbg.zip");
+		} else {
+			dbg_zip = "/tmp/ums_dbg.zip";
+		}
 	}
 
 	public JComponent config() {
