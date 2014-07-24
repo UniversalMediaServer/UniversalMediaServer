0a7fc2f3ddc43f8850c1618c74acfd2b3b14975a
infidel <aval57@yahoo.com>
2014-07-24 15:57:03 -0400

    Lightweight Global id repo + allow ids with tags

--- src/main/java/net/pms/dlna/GlobalIdRepo.java
+++ src/main/java/net/pms/dlna/GlobalIdRepo.java
@@ -1,100 +1,85 @@
 package net.pms.dlna;
 
-
-import java.lang.ref.WeakReference;
-import java.util.Iterator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.ArrayList;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GlobalIdRepo {
-	private int globalId;
-	// This map will need to be explicitly expunged 
-	private HashMap<Integer, WeakReference<DLNAResource>> map;
-	// WeakHashMap is self-expunging
-	private WeakHashMap<DLNAResource, Integer> revMap;
-	private int ct;
 	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalIdRepo.class);
 
+	private int globalId = 0;
+
+	class ID {
+		int id;
+		DLNAResource dlna;
+		ID(DLNAResource d) {
+			id = ++globalId;
+			d.setIndexId(id);
+			dlna = d;
+		}
+	}
+	private ArrayList<ID> ids = new ArrayList<>();
+
 	public GlobalIdRepo() {
-		globalId = 1;
-		map = new HashMap<Integer, WeakReference<DLNAResource>>();
-		revMap = new WeakHashMap<DLNAResource, Integer>();
 	}
 
-	public synchronized void add(DLNAResource res) {
-		int id = globalId++;
-		res.setIndexId(id);
-		map.put(id, new WeakReference<DLNAResource>(res));
-		revMap.put(res, id);
-		
-		// Unnecessary to expunge every single time
-		if (++ct % 100 == 0) {
-			expunge();
+	public synchronized void add(DLNAResource d) {
+		String id = d.getId();
+		if (id != null) {
+			remove(id);
 		}
-	}
-
-	public DLNAResource get(int id) {
-		WeakReference<DLNAResource> ref = map.get(id);
-		if (ref != null) {
-			DLNAResource d = ref.get();
-			if (d == null) {
-				// Object no longer exists
-				map.remove(id);
-				LOGGER.debug("GlobalIdRepo: expunged id {}", id);
-			}
-			return d;
-		}
-		return null;
+		ids.add(new ID(d));
 	}
 
 	public DLNAResource get(String id) {
-		try {
-			return get(Integer.parseInt(id));
-		} catch (NumberFormatException e) {
-			return null;
+		return get(parseIndex(id));
+	}
+
+	public DLNAResource get(int id) {
+		int index = indexOf(id);
+		return index > -1 ? ids.get(index).dlna : null;
+	}
+
+	public void remove(DLNAResource d) {
+		remove(d.getId());
+	}
+
+	public void remove(String id) {
+		remove(parseIndex(id));
+	}
+
+	public synchronized void remove(int id) {
+		int index = indexOf(id);
+		if (index > -1) {
+			LOGGER.debug("GlobalIdRepo: removing id {} - {}", id, ids.get(index).dlna.getName());
+			ids.remove(index);
 		}
 	}
 
-	public void remove(int id) {
-		LOGGER.debug("GlobalIdRepo: removing by id {}", id);
-		revMap.remove(get(id));
-		map.remove(id);
+	public int parseIndex(String id) {
+		try {
+			// Id strings may have optional tags beginning with $ appended, e.g. '1234$Temp'
+			return Integer.parseInt(StringUtils.substringBefore(id, "$"));
+		} catch (NumberFormatException e) {
+			return -1;
+		}
 	}
 
-	public void remove(DLNAResource res) {
-		LOGGER.debug("GlobalIdRepo: removing by object {} - {}", res.getId(), res.getName());
-		revMap.remove(res);
-		map.remove(res.getId());
-	}
-
-	public int getId(DLNAResource d) {
-		return revMap.get(d);
-	}
-
-	private void expunge() {
-		int x = 0, s = map.size();
-		gc(); // for debugging only
-		for(Iterator<Map.Entry<Integer, WeakReference<DLNAResource>>> it = map.entrySet().iterator(); it.hasNext();) {
-			Map.Entry<Integer, WeakReference<DLNAResource>> e = it.next();
-			if (e.getValue().get() == null) {
-			 	// Object no longer exists
-			 	it.remove();
-			 	x++;
+	private int indexOf(int id) {
+		// We're in sequence by definition, so binary search is quickest
+		int lo = 0, hi = ids.size() - 1;
+		while (lo <= hi) {
+			int mid = lo + (hi - lo) / 2;
+			int idm = ids.get(mid).id;
+			if (id < idm) {
+				hi = mid - 1;
+			} else if (id > idm) {
+				lo = mid + 1;
+			} else {
+				return mid;
 			}
 		}
-		LOGGER.debug("GlobalIdRepo: expunged {}/{} ids. revMap size is {}", x, s, revMap.size());
-	}
-
-	// Force garbage collection - for debugging only
-	public static void gc() {
-		Object obj = new Object();
-		WeakReference ref = new WeakReference<Object>(obj);
-		obj = null;
-		while(ref.get() != null) {
-			System.gc();
-		}
+		return -1;
 	}
 }
