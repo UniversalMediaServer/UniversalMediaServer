fba5a0082d51c5033adde62fddc4ae1b4228c8ac
SubJunk <subjunk@gmail.com>
2014-08-08 14:41:03 +1200

    Reverted accidentally committed code

--- src/main/java/net/pms/encoders/MEncoderVideo.java
+++ src/main/java/net/pms/encoders/MEncoderVideo.java
@@ -1732,12 +1732,6 @@ public class MEncoderVideo extends Player {
 			scaleHeight = media.getHeight();
 		}
 
-		double videoAspectRatio = (double) media.getWidth() / (double) media.getHeight();
-		double rendererAspectRatio = 1.777777777777778;
-		if (params.mediaRenderer.isMaximumResolutionSpecified()) {
-			rendererAspectRatio = (double) params.mediaRenderer.getMaxVideoWidth() / (double) params.mediaRenderer.getMaxVideoHeight();
-		}
-
 		if ((deinterlace || scaleBool) && !avisynth()) {
 			StringBuilder vfValueOverscanPrepend = new StringBuilder();
 			StringBuilder vfValueOverscanMiddle  = new StringBuilder();
@@ -1745,6 +1739,10 @@ public class MEncoderVideo extends Player {
 			StringBuilder vfValueComplete        = new StringBuilder();
 
 			String deinterlaceComma = "";
+			double rendererAspectRatio = 1.777777777777778;
+			if (params.mediaRenderer.isMaximumResolutionSpecified()) {
+				rendererAspectRatio = (double) params.mediaRenderer.getMaxVideoWidth() / (double) params.mediaRenderer.getMaxVideoHeight();
+			}
 
 			/*
 			 * Implement overscan compensation settings
@@ -1816,6 +1814,8 @@ public class MEncoderVideo extends Player {
 				vfValueVS.append("scale=").append(scaleWidth).append(":").append(scaleHeight);
 			} else if (isResolutionTooHighForRenderer) {
 				// The video resolution is too big for the renderer so we need to scale it down
+
+				double videoAspectRatio = (double) media.getWidth() / (double) media.getHeight();
 
 				/*
 				 * First we deal with some exceptions, then if they are not matched we will
@@ -1901,27 +1901,17 @@ public class MEncoderVideo extends Player {
 			) &&
 			!configuration.isMencoderScaler()
 		) {
-			String vfValuePrepend = "expand=";
+			int expandBorderWidth;
+			int expandBorderHeight;
+
+			expandBorderWidth  = scaleWidth % 4;
+			expandBorderHeight = scaleHeight % 4;
+
+			String vfValuePrepend = "";
+			vfValuePrepend += "expand=-" + expandBorderWidth + ":-" + expandBorderHeight;
 
 			if (params.mediaRenderer.isKeepAspectRatio()) {
-				if (videoAspectRatio > rendererAspectRatio) {
-					scaleHeight = (int) Math.round(scaleWidth / videoAspectRatio);
-				} else {
-					scaleWidth  = (int) Math.round(scaleHeight * videoAspectRatio);
-				}
-
-				if (params.mediaRenderer.isMuxNonMod4Resolution()) {
-					// Make sure the new dimensions are mod2
-					scaleWidth  = 2*((int) Math.floor(Math.abs(scaleWidth/2)));
-					scaleHeight = 2*((int) Math.floor(Math.abs(scaleHeight/2)));
-				} else {
-					// Make sure the new dimensions are mod4
-					scaleWidth  = 4*((int) Math.floor(Math.abs(scaleWidth/4)));
-					scaleHeight = 4*((int) Math.floor(Math.abs(scaleHeight/4)));
-				}
-				vfValuePrepend += scaleWidth + ":" + scaleHeight;
-			} else {
-				vfValuePrepend += "-" + (scaleWidth % 4) + ":-" + (scaleHeight % 4);
+				vfValuePrepend += ":::0:16/9";
 			}
 
 			vfValuePrepend += ",softskip";
