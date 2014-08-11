cc9f131477d0f121b36ca8c06dba108e7cba0e1b
SubJunk <subjunk@gmail.com>
2014-08-11 18:07:00 +1200

    Revert "Fixed a bug reported in"
    This reverts commit 8dadb9e6fb8658d1d8a531eaad363364889975fe.

--- src/main/java/net/pms/network/Request.java
+++ src/main/java/net/pms/network/Request.java
@@ -307,8 +307,12 @@ public class Request extends HTTPResource {
 					// This is a request for a subtitle file
 					output(output, "Content-Type: text/plain");
 					output(output, "Expires: " + getFUTUREDATE() + " GMT");
-					DLNAMediaSubtitle sub = dlna.getMediaSubtitle();
-					if (sub != null) {
+					List<DLNAMediaSubtitle> subs = dlna.getMedia().getSubtitleTracksList();
+
+					if (subs != null && !subs.isEmpty()) {
+						// TODO: maybe loop subs to get the requested subtitle type instead of using the first one
+						DLNAMediaSubtitle sub = subs.get(0);
+
 						try {
 							// XXX external file is null if the first subtitle track is embedded:
 							// http://www.ps3mediaserver.org/forum/viewtopic.php?f=3&t=15805&p=75534#p75534
