46ed5ecea92d35b6e24c551e0251b4878edea323
infidel <aval57@yahoo.com>
2014-08-15 18:45:06 -0400

    Subtitle logging contd

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -555,7 +555,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 						// Media is transcodable
 						LOGGER.trace("File \"{}\" can be transcoded", child.getName());
 					}
-				} else if (child.media != null) {
+				} else if (child.media != null && defaultRenderer != null) {
 					LOGGER.trace("Did not check for media_subtitle for \"{}\" because {} does not use MediaInfo, we will check for it soon", child.getName(), defaultRenderer);
 				}
 
