68a98d752d3582f07e698f72c0374c3c6f716d19
SubJunk <subjunk@gmail.com>
2014-09-04 18:34:38 +1200

    Reverted d288b34
    It didn't work

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -1178,14 +1178,12 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 	 * @since 1.90.0
 	 */
 	protected void resolveFormat() {
-		if (!isFolder()) {
-			if (format == null) {
-				format = FormatFactory.getAssociatedFormat(getSystemName());
-			}
+		if (format == null) {
+			format = FormatFactory.getAssociatedFormat(getSystemName());
+		}
 
-			if (format != null && format.isUnknown()) {
-				format.setType(getSpecificType());
-			}
+		if (format != null && format.isUnknown()) {
+			format.setType(getSpecificType());
 		}
 	}
 
