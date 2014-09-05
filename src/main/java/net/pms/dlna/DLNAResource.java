UniversalMediaServer/UniversalMediaServer

commit 61f1acb81911f6139281c372e66e1b6f63ae0d2c

Skip figuring out details about subtitles that don't exist

src/main/java/net/pms/dlna/DLNAResource.java
@@ -633,13 +633,15 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 							}
 
 							boolean hasEmbeddedSubs = false;
+							boolean hasAnySubs = false;
 
 							if (child.media != null) {
 								for (DLNAMediaSubtitle s : child.media.getSubtitleTracksList()) {
 									hasEmbeddedSubs = (hasEmbeddedSubs || s.isEmbedded());
+									hasAnySubs = true;
 								}
 
-								if (!parserV2) {
+								if (!parserV2 && hasAnySubs) {
 									if (!configuration.isDisableSubtitles() && child.isSubsFile() && defaultRenderer.isSubtitlesStreamingSupported()) {
 										OutputParams params = new OutputParams(configuration);
 										Player.setAudioAndSubs(child.getSystemName(), child.media, params); // set proper subtitles in accordance with user setting
@@ -657,7 +659,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 
 							boolean hasSubsToTranscode = false;
 
-							if (!configuration.isDisableSubtitles()) {
+							if (!configuration.isDisableSubtitles() && hasAnySubs) {
 								if (child.isSubsFile()) {
 									if (child.media_subtitle == null) {
 										// Subtitles are not set for streaming

commit 1d0bbc33768c58bf452b7ef3f2d34c9e0cb3f02f

Fixed external subtitle streaming support
The feature was broken in 61f1acb

src/main/java/net/pms/dlna/DLNAResource.java
@@ -633,7 +633,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 							}
 
 							boolean hasEmbeddedSubs = false;
-							boolean hasAnySubs = child.media.getSubtitleTracksList().size() > 0;
+							boolean hasAnySubs = child.media.getSubtitleTracksList().size() > 0 || child.isSubsFile();
 							boolean hasSubsToTranscode = false;
 
 							if (!configuration.isDisableSubtitles() && hasAnySubs) {