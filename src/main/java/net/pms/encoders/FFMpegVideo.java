66f90edfacac6336c75637ca3c02adc73f4eb3b5
SubJunk <subjunk@gmail.com>
2014-09-09 18:18:48 +1200

    Revert "Update output params when needed"
    This reverts commit 3df853ef708af420a326cad38fff698dedb5a2f8.
    
    Conflicts:
    	src/main/java/net/pms/encoders/FFMpegVideo.java
    	src/main/java/net/pms/encoders/FFmpegWebVideo.java
    	src/main/java/net/pms/encoders/MEncoderVideo.java
    	src/main/java/net/pms/encoders/VLCVideo.java

--- src/main/java/net/pms/encoders/FFMpegVideo.java
+++ src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -702,14 +702,7 @@ public class FFMpegVideo extends Player {
 			params.waitbeforestart = 2500;
 		}
 
-		if (params.aid == null) {
-			setAudioOutputParameters(media, params);
-		}
-
-		if (params.sid == null || (params.sid != null && StringUtils.isNotEmpty(params.sid.getLiveSubURL()))) {
-			setSubtitleOutputParameters(filename, media, params);
-		}
-
+		setAudioAndSubs(filename, media, params);
 		cmdList.add(executable());
 
 		// Prevent FFmpeg timeout
