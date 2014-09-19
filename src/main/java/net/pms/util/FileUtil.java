UniversalMediaServer/UniversalMediaServer

commit 8c7a5e23048969603678141531dc72dd6ccdc82f

Simplified code

src/main/java/net/pms/util/FileUtil.java
@@ -195,9 +195,8 @@ public class FileUtil {
 		String formattedName;
 		String formattedNameTemp;
 
-		// The following two booleans are only changed and used if InfoDb is enabled
-		boolean hasEpisodeNameInFilename = false;
-		boolean isEpisode = false;
+		// This is false unless we recognize that we could use some info on the video from IMDB
+		boolean videoShouldUseInfoDb = false;
 
 		// Remove file extension
 		fileNameWithoutExtension = getFileNameWithoutExtension(f);
@@ -247,9 +246,8 @@ public class FileUtil {
 			if (PMS.getConfiguration().isLoadEpisodeTitles()) {
 				if (!formattedName.equals(formattedNameTemp)) {
 					formattedName = formattedNameTemp;
-					hasEpisodeNameInFilename = true;
+					videoShouldUseInfoDb = true;
 				}
-				isEpisode = true;
 			}
 
 			// Remove stuff at the end of the filename like release group, quality, source, etc.
@@ -272,9 +270,8 @@ public class FileUtil {
 			if (PMS.getConfiguration().isLoadEpisodeTitles()) {
 				if (!formattedName.equals(formattedNameTemp)) {
 					formattedName = formattedNameTemp;
-					hasEpisodeNameInFilename = true;
+					videoShouldUseInfoDb = true;
 				}
-				isEpisode = true;
 			}
 
 			// Remove stuff at the end of the filename like release group, quality, source, etc.
@@ -296,9 +293,8 @@ public class FileUtil {
 			if (PMS.getConfiguration().isLoadEpisodeTitles()) {
 				if (!formattedName.equals(formattedNameTemp)) {
 					formattedName = formattedNameTemp;
-					hasEpisodeNameInFilename = true;
+					videoShouldUseInfoDb = true;
 				}
-				isEpisode = true;
 			}
 
 			// Remove stuff at the end of the filename like release group, quality, source, etc.
@@ -319,9 +315,8 @@ public class FileUtil {
 			if (PMS.getConfiguration().isLoadEpisodeTitles()) {
 				if (!formattedName.equals(formattedNameTemp)) {
 					formattedName = formattedNameTemp;
-					hasEpisodeNameInFilename = true;
+					videoShouldUseInfoDb = true;
 				}
-				isEpisode = true;
 			}
 
 			// Remove stuff at the end of the filename like release group, quality, source, etc.
@@ -426,7 +421,7 @@ public class FileUtil {
 		}
 
 		// Add episode name (if not there)
-		if (isEpisode && !hasEpisodeNameInFilename) {
+		if (videoShouldUseInfoDb) {
 			InfoDb.InfoDbData info = PMS.get().infoDb().get(file);
 			if (info == null) {
 				PMS.get().infoDbAdd(file);

commit fd69dcc5d6ed2efdc98ca3ddc7fc64528ac59393

Fixed the logic broken in 8c7a5e2 and 1b5e58a

src/main/java/net/pms/util/FileUtil.java
@@ -220,7 +220,7 @@ public class FileUtil {
 			// If it matches this then it didn't match the previous one, which means there is probably an episode title in the filename
 			formattedNameTemp = formattedName.replaceAll("(?i)[\\s\\.]S0(\\d)E(\\d)(\\d)E(\\d)(\\d)[\\s\\.]", " - $1$2$3-$1$4$5 - ");
 
-			if (PMS.getConfiguration().isLoadEpisodeTitles() && !formattedName.equals(formattedNameTemp)) {
+			if (PMS.getConfiguration().isLoadEpisodeTitles() && formattedName.equals(formattedNameTemp)) {
 				videoShouldUseInfoDb = true;
 			}
 
@@ -240,7 +240,7 @@ public class FileUtil {
 			// If it matches this then it didn't match the previous one, which means there is probably an episode title in the filename
 			formattedNameTemp = formattedName.replaceAll("(?i)[\\s\\.]S([1-9]\\d)E(\\d)(\\d)E(\\d)(\\d)[\\s\\.]", " - $1$2$3-$1$4$5 - ");
 
-			if (PMS.getConfiguration().isLoadEpisodeTitles() && !formattedName.equals(formattedNameTemp)) {
+			if (PMS.getConfiguration().isLoadEpisodeTitles() && formattedName.equals(formattedNameTemp)) {
 				videoShouldUseInfoDb = true;
 			}
 
@@ -261,7 +261,7 @@ public class FileUtil {
 			// If it matches this then it didn't match the previous one, which means there is probably an episode title in the filename
 			formattedNameTemp = formattedName.replaceAll("(?i)[\\s\\.]S0(\\d)E(\\d)(\\d)[\\s\\.]", " - $1$2$3 - ");
 
-			if (PMS.getConfiguration().isLoadEpisodeTitles() && !formattedName.equals(formattedNameTemp)) {
+			if (PMS.getConfiguration().isLoadEpisodeTitles() && formattedName.equals(formattedNameTemp)) {
 				videoShouldUseInfoDb = true;
 			}
 
@@ -281,7 +281,7 @@ public class FileUtil {
 			// If it matches this then it didn't match the previous one, which means there is probably an episode title in the filename
 			formattedNameTemp = formattedName.replaceAll("(?i)[\\s\\.]S([1-9]\\d)E(\\d)(\\d)[\\s\\.]", " - $1$2$3 - ");
 
-			if (PMS.getConfiguration().isLoadEpisodeTitles() && !formattedName.equals(formattedNameTemp)) {
+			if (PMS.getConfiguration().isLoadEpisodeTitles() && formattedName.equals(formattedNameTemp)) {
 				videoShouldUseInfoDb = true;
 			}
 
@@ -300,7 +300,7 @@ public class FileUtil {
 			// If it matches this then it didn't match the previous one, which means there is probably an episode title in the filename
 			formattedNameTemp = formattedName.replaceAll("(?i)[\\s\\.](19|20)(\\d\\d)[\\s\\.]([0-1]\\d)[\\s\\.]([0-3]\\d)[\\s\\.]", " - $1$2/$3/$4 - ");
 
-			if (PMS.getConfiguration().isLoadEpisodeTitles() && !formattedName.equals(formattedNameTemp)) {
+			if (PMS.getConfiguration().isLoadEpisodeTitles() && formattedName.equals(formattedNameTemp)) {
 				videoShouldUseInfoDb = true;
 			}
 