e01e52b4a194aed947f80f490b0624d99ea21f36
SubJunk <subjunk@gmail.com>
2014-09-20 17:12:10 +1200

    Fixed processing of movie year

--- src/main/java/net/pms/util/InfoDb.java
+++ src/main/java/net/pms/util/InfoDb.java
@@ -76,10 +76,10 @@ public class InfoDb implements DbHandler {
 			data.ep_name = "";
 		}
 
-		data.season = FileDb.safeGetArg(args, off + 2);
+		data.year = FileDb.safeGetArg(args, off + 2);
 		data.episode = FileDb.safeGetArg(args, off + 3);
 		data.title = FileDb.safeGetArg(args, off + 4);
-		data.year = FileDb.safeGetArg(args, off + 5);
+		data.season = FileDb.safeGetArg(args, off + 5);
 
 		return data;
 	}
