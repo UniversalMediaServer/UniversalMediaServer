50135597c77d4357db8afec44af3c8b91afe3b1c
valib <liborvala@seznam.cz>
2014-09-27 22:30:46 +0200

    Try to fix LG browsing. Works on Panasonic TV. Need testing on other renderers.

--- src/main/java/net/pms/network/RequestV2.java
+++ src/main/java/net/pms/network/RequestV2.java
@@ -633,9 +633,7 @@ public class RequestV2 extends HTTPResource {
 				response.append(HTTPXMLHelper.RESULT_HEADER);
 				response.append(HTTPXMLHelper.DIDL_HEADER);
 
-				boolean browseDirectChildren = browseFlag != null &&
-						browseFlag.equals("BrowseDirectChildren") &&
-						(!mediaRenderer.notAggressiveBrowsing() || objectID.equals("0"));
+				boolean browseDirectChildren = browseFlag != null && browseFlag.equals("BrowseDirectChildren");
 
 				if (soapaction != null && soapaction.contains("ContentDirectory:1#Search")) {
 					browseDirectChildren = true;
