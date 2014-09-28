aaaf6f71263384014cdc67342f3e3f69314ced3e
valib <liborvala@seznam.cz>
2014-09-28 12:50:20 +0200

    Fix NPE in commit 3ef2ff64883ffcaa5ae36fef57aac0ae88248b24

--- src/main/java/net/pms/newgui/NavigationShareTab.java
+++ src/main/java/net/pms/newgui/NavigationShareTab.java
@@ -241,7 +241,9 @@ public class NavigationShareTab {
 				try {
 					int ab = Integer.parseInt(seekpos.getText());
 					configuration.setThumbnailSeekPos(ab);
-					PMS.get().getDatabase().init(true);
+					if (configuration.getUseCache()) {
+						PMS.get().getDatabase().init(true);
+					}
 				} catch (NumberFormatException nfe) {
 					LOGGER.debug("Could not parse thumbnail seek position from \"" + seekpos.getText() + "\"");
 				}
