30323d8e636cf683093e00f6a36ad86ea7094bd2
valib <liborvala@seznam.cz>
2014-09-30 00:01:54 +0200

    Fix possible NPE

--- src/main/java/net/pms/encoders/MEncoderVideo.java
+++ src/main/java/net/pms/encoders/MEncoderVideo.java
@@ -1348,11 +1348,14 @@ public class MEncoderVideo extends Player {
 			false
 		);
 
-		for (String s : expertOptions) {
-			if (s.equals("-noass")) {
-				foundNoassParam = true;
+		if (expertOptions != null) {
+			for (String s : expertOptions) {
+				if (s.equals("-noass")) {
+					foundNoassParam = true;
+				}
 			}
 		}
+		
 
 		StringBuilder sb = new StringBuilder();
 		// Set subtitles options
