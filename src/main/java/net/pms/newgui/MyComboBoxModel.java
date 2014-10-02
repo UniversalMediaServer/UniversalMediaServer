c5ec4c4f04b28d9e2ce7b75cef46120f7e0e4029
SubJunk <subjunk@gmail.com>
2014-10-02 14:06:18 +1300

    Fixed compilation

--- src/main/java/net/pms/newgui/MyComboBoxModel.java
+++ src/main/java/net/pms/newgui/MyComboBoxModel.java
@@ -20,7 +20,7 @@ package net.pms.newgui;
 
 import javax.swing.DefaultComboBoxModel;
 
-public class MyComboBoxModel extends DefaultComboBoxModel<Object> {
+public class MyComboBoxModel extends DefaultComboBoxModel {
 	private static final long serialVersionUID = -9094365556516842551L;
 
 	public MyComboBoxModel() {
