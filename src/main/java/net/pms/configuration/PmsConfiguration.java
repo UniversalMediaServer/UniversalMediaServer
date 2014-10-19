c02e8420322298fa405993495df7e40d2c1ff549
SubJunk <subjunk@gmail.com>
2014-10-19 20:54:35 +1300

    Renamed "3D_subtitles_depth" to "3d_subtitles_depth"

--- src/main/java/net/pms/configuration/PmsConfiguration.java
+++ src/main/java/net/pms/configuration/PmsConfiguration.java
@@ -71,7 +71,7 @@ public class PmsConfiguration {
 	// TODO: Get this out of here
 	private static boolean avsHackLogged = false;
 
-	private static final String KEY_3D_SUBTITLES_DEPTH = "3D_subtitles_depth";
+	private static final String KEY_3D_SUBTITLES_DEPTH = "3d_subtitles_depth";
 	private static final String KEY_ALTERNATE_SUBTITLES_FOLDER = "alternate_subtitles_folder";
 	private static final String KEY_ALTERNATE_THUMB_FOLDER = "alternate_thumb_folder";
 	private static final String KEY_APPEND_PROFILE_NAME = "append_profile_name";
