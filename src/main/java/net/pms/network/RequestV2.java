3db976e0ca3e8eb98204da6abdd374e9275f07cf
SubJunk <subjunk@gmail.com>
2015-02-09 14:05:40 +1300

    Revert "Removes extra close on InputStream"
    This reverts commit 54caad2acbc24f0d2e3cefcb94550bf9f8eebccc.

--- src/main/java/net/pms/network/RequestV2.java
+++ src/main/java/net/pms/network/RequestV2.java
@@ -922,7 +922,12 @@ public class RequestV2 extends HTTPResource {
 						LOGGER.trace("  isSuccess: " + future.isSuccess());
 						LOGGER.trace("  isCancelled: " + future.isCancelled());
 						LOGGER.trace("  getCause: ", future.cause());
-						PMS.get().getRegistry().reenableGoToSleep();
+						try {
+							PMS.get().getRegistry().reenableGoToSleep();
+							inputStream.close();
+						} catch (IOException e) {
+							LOGGER.debug("Caught exception", e);
+						}
 
 						// Always close the channel after the response is sent because of
 						// a freeze at the end of video when the channel is not closed.
