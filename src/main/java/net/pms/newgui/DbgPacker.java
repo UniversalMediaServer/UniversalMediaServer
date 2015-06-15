98fddfd02db90eaa0fbb89ddbac4aebc8d0b50e1
Nadahar <magne.sandve@gmail.com>
2015-06-15 22:28:04 +0200

    Fixes a bug where UMS would crash if debug.log doesn't exist.

--- src/main/java/net/pms/newgui/DbgPacker.java
+++ src/main/java/net/pms/newgui/DbgPacker.java
@@ -38,7 +38,9 @@ public class DbgPacker implements ActionListener {
 	public DbgPacker() {
 		items = new LinkedHashMap<>();
 		debug_log = LoggingConfigFileLoader.getLogFilePaths().get("debug.log");
-		dbg_zip = debug_log.replace("debug.log", "ums_dbg.zip");
+		if (debug_log != null) {
+			dbg_zip = debug_log.replace("debug.log", "ums_dbg.zip");
+		}
 	}
 
 	public JComponent config() {
