60c60066795f6b0ed4bc40aef365b2b023eb7264
Nadahar <Nadahar@users.noreply.github.com>
2015-09-12 15:28:26 +0200

    Temporary disable of isAdmin() check in PMS.killOld().

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -1295,7 +1295,7 @@ public class PMS {
 	 * Restart handling
 	 */
 	private static void killOld() {
-		if (configuration.isAdmin()) {
+		if (!Platform.isWindows() || configuration.isAdmin()) {
 			try {
 				killProc();
 			} catch (IOException e) {
