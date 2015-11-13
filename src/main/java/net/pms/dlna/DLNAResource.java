1540c2b3acfd8509e7fd82a335524573e4a383b0
SubJunk <subjunk@gmail.com>
2015-11-13 19:04:38 +1300

    Revert "Removed unnecessary change"
    This reverts commit 8b3e0b8b9b7eaa1dd2589690a8ccd8a246d70482.
    
    Conflicts:
    	src/main/java/net/pms/dlna/DLNAResource.java

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -558,11 +558,19 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 
 		try {
 			if (child.isValid()) {
-				// Do not add unsupported media formats to the list
-				if (child.format != null && defaultRenderer != null && !defaultRenderer.supportsFormat(child.format)) {
-					LOGGER.trace("Ignoring file \"{}\" because it is not supported by renderer \"{}\"", child.getName(), defaultRenderer.getRendererName());
-					children.remove(child);
-					return;
+				if (child.format != null) {
+					// Do not add unsupported media formats to the list
+					if (defaultRenderer != null && !defaultRenderer.supportsFormat(child.format)) {
+						LOGGER.trace("Ignoring file \"{}\" because it is not supported by renderer \"{}\"", child.getName(), defaultRenderer.getRendererName());
+						children.remove(child);
+						return;
+					}
+
+					// Hide watched videos depending user preference
+					if ("2".equals(configuration.getWatchedVideoAction()) && child.format.isVideo() && MediaMonitor.isWatched(child.getSystemName())) {
+						LOGGER.trace("Ignoring file \"{}\" because it has been watched", child.getName());
+						return;
+					}
 				}
 
 				LOGGER.trace("{} child \"{}\" with class \"{}\"", isNew ? "Adding new" : "Updating", child.getName(), child.getClass().getName());
