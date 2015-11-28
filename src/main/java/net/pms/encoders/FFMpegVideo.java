2bac7157d0a2d47f56e150a0cd895d1ea68e775b
valib <liborvala@seznam.cz>
2015-11-29 00:12:28 +0100

    Let FFmpeg do their job with subtitles configuration.

--- src/main/java/net/pms/encoders/FFMpegVideo.java
+++ src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -219,42 +219,34 @@ public class FFMpegVideo extends Player {
 						subsFilter.append(":si=").append(params.sid.getId());
 					}
 
-					// Set the resolution for subtitles to use
-					int subtitlesWidth = scaleWidth; 
-					int subtitlesHeight = scaleHeight;
 					if (params.sid.isExternal() && params.sid.getType() != SubtitleType.ASS || configuration.isFFmpegFontConfig()) {
-						if (subtitlesWidth > 0 && subtitlesHeight > 0) {
-							// Let ASS/SSA subtitles specify their own resolution
-							if (params.sid.getType() == SubtitleType.ASS) {
-								setSubtitlesResolution(originalSubsFilename, subtitlesWidth, subtitlesHeight);
+						// Set the input subtitles character encoding if not UTF-8
+						if (!params.sid.isExternalFileUtf8()) {
+							String encoding = isNotBlank(configuration.getSubtitlesCodepage()) ?
+								configuration.getSubtitlesCodepage() : params.sid.getExternalFileCharacterSet() != null ?
+								params.sid.getExternalFileCharacterSet() : null;
+							if (encoding != null) {
+								subsFilter.append(":charenc=").append(encoding);
 							}
+						}
 
+						// If the FFmpeg font config is enabled than we need to add settings to the filter. TODO there could be also changed the font type. See http://ffmpeg.org/ffmpeg-filters.html#subtitles-1
+						if (configuration.isFFmpegFontConfig()) {
+							subsFilter.append(":force_style=");
+							subsFilter.append("'");
+							// XXX (valib) If the font size is not acceptable it could be calculated better taking in to account the original video size. Unfortunately I don't know how to do that.
 							if (!is3D) {
-								subsFilter.append(":original_size=").append(subtitlesWidth).append("x").append(subtitlesHeight);
-							}
-
-							// Set the input subtitles character encoding if not UTF-8
-							if (!params.sid.isExternalFileUtf8()) {
-								String encoding = isNotBlank(configuration.getSubtitlesCodepage()) ?
-									configuration.getSubtitlesCodepage() : params.sid.getExternalFileCharacterSet() != null ?
-									params.sid.getExternalFileCharacterSet() : null;
-								if (encoding != null) {
-									subsFilter.append(":charenc=").append(encoding);
-								}
-							}
-
-							// If the FFmpeg font config is enabled than we need to add settings to the filter. TODO there could be also changed the font type. See http://ffmpeg.org/ffmpeg-filters.html#subtitles-1
-							if (configuration.isFFmpegFontConfig() && !is3D) {
-								subsFilter.append(":force_style=");
-								subsFilter.append("'");
-								// XXX (valib) If the font size is not acceptable it could be calculated better taking in to account the original video size. Unfortunately I don't know how to do that.
 								subsFilter.append("Fontsize=").append((int) 16 * Double.parseDouble(configuration.getAssScale()));
-								subsFilter.append(",PrimaryColour=").append(SubtitleUtils.convertColourToASSColourString(configuration.getSubsColor()));
-								subsFilter.append(",Outline=").append(configuration.getAssOutline());
-								subsFilter.append(",Shadow=").append(configuration.getAssShadow());
-								subsFilter.append(",MarginV=").append(configuration.getAssMargin());
-								subsFilter.append("'");
 							}
+							
+							subsFilter.append(",PrimaryColour=").append(SubtitleUtils.convertColourToASSColourString(configuration.getSubsColor()));
+							subsFilter.append(",Outline=").append(configuration.getAssOutline());
+							subsFilter.append(",Shadow=").append(configuration.getAssShadow());
+							if (!is3D) {
+								subsFilter.append(",MarginV=").append(configuration.getAssMargin());
+							}
+							;
+							subsFilter.append("'");
 						}
 					}
 				}
