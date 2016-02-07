ecd2724f927040e481453899644d99ddeac7769a
SubJunk <subjunk@gmail.com>
2016-02-07 19:47:50 +1300

    Fixed video-only output

--- src/main/java/net/pms/encoders/FFMpegVideo.java
+++ src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -250,7 +250,7 @@ public class FFMpegVideo extends Player {
 			} else if (params.sid.getType().isPicture()) {
 				if (params.sid.getId() < 100) {
 					// Embedded
-					subsFilter.append("[0:v][0:s:").append(media.getSubtitleTracksList().indexOf(params.sid)).append("]overlay[v]");
+					subsFilter.append("[0:v][0:s:").append(media.getSubtitleTracksList().indexOf(params.sid)).append("]overlay");
 					isSubsManualTiming = false;
 				} else {
 					// External
