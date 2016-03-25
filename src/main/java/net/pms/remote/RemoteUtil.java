bc382cfb42e5e1492738b9b4f149be79678eb519
SubJunk <subjunk@gmail.com>
2016-03-25 23:55:59 +1300

    Formatting

--- src/main/java/net/pms/remote/RemoteUtil.java
+++ src/main/java/net/pms/remote/RemoteUtil.java
@@ -176,8 +176,21 @@ public class RemoteUtil {
 	}
 
 	public static boolean directmime(String mime) {
-		return mime != null && (mime.equals(MIME_MP4) || mime.equals(MIME_WEBM) || mime.equals(MIME_OGG) ||
-			mime.equals(MIME_MP3) || mime.equals(MIME_PNG) || mime.equals(MIME_JPG)/*|| mime.equals(MIME_WAV)*/);
+		if (
+			mime != null &&
+			(
+				mime.equals(MIME_MP4) ||
+				mime.equals(MIME_WEBM) ||
+				mime.equals(MIME_OGG) ||
+				mime.equals(MIME_MP3) ||
+				mime.equals(MIME_PNG) ||
+				mime.equals(MIME_JPG)
+			)
+		) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public static String userName(HttpExchange t) {
@@ -213,7 +226,7 @@ public class RemoteUtil {
 
 	public static String getCookie(String name, HttpExchange t) {
 		String cstr = t.getRequestHeaders().getFirst("Cookie");
-		if (! StringUtils.isEmpty(cstr)) {
+		if (!StringUtils.isEmpty(cstr)) {
 			name += "=";
 			for (String str : cstr.trim().split("\\s*;\\s*")) {
 				if (str.startsWith(name)) {
@@ -324,7 +337,7 @@ public class RemoteUtil {
 	}
 
 	public static String getMsgString(String key, HttpExchange t) {
-		if(PMS.getConfiguration().useWebLang()) {
+		if (PMS.getConfiguration().useWebLang()) {
 			String lang = getFirstSupportedLanguage(t);
 			if (!lang.isEmpty()) {
 				return Messages.getString(key, Locale.forLanguageTag(lang));
