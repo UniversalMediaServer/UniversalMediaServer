e509f644b1e8abdb5dabf02183435505ab9f9cb4
valib <liborvala@seznam.cz>
2016-05-19 01:23:07 +0200

    Make sending DLNA messages compatible to the DLNA specification.

--- src/main/java/net/pms/network/UPNPHelper.java
+++ src/main/java/net/pms/network/UPNPHelper.java
@@ -332,24 +332,19 @@ public class UPNPHelper extends UPNPControl {
 	 */
 	private static void sendMessage(DatagramSocket socket, String nt, String message) throws IOException {
 		String msg = buildMsg(nt, message);
-		//Random rand = new Random();
-
-		// LOGGER.trace( "Sending this SSDP packet: " + CRLF + StringUtils.replace(msg, CRLF, "<CRLF>")));
-
+		Random rand = new Random();
+//		LOGGER.trace( "Sending the SSDP packet: " + CRLF + StringUtils.replace(msg, CRLF, "<CRLF>"));
 		InetAddress upnpAddress = getUPNPAddress();
 		DatagramPacket ssdpPacket = new DatagramPacket(msg.getBytes(), msg.length(), upnpAddress, UPNP_PORT);
 		socket.send(ssdpPacket);
-
-		// XXX Why is it necessary to sleep for this random time? What would happen when random equals 0?
-		//sleep(rand.nextInt(1800 / 2));
-
-		// XXX Why send the same packet twice?
-		//socket.send(ssdpPacket);
-
-		// XXX Why is it necessary to sleep for this random time (again)?
-		//sleep(rand.nextInt(1800 / 2));
+		sleep(rand.nextInt(delay / 2));
+		// Send the SSDP packet twice in accordance with the DLNA specification
+		socket.send(ssdpPacket);
+//		LOGGER.trace( "Repeating the SSDP packet: " + CRLF + StringUtils.replace(msg, CRLF, "<CRLF>"));
 	}
 
+	private static int delay = 10000;
+	
 	/**
 	 * Starts up two threads: one to broadcast UPnP ALIVE messages and another
 	 * to listen for responses.
@@ -360,14 +355,12 @@ public class UPNPHelper extends UPNPControl {
 		Runnable rAlive = new Runnable() {
 			@Override
 			public void run() {
-				int delay = 10000;
-
 				while (true) {
 					sleep(delay);
 					sendAlive();
 
 					// If a renderer is connected, broadcast every 30 seconds, otherwise every 10.
-					if (getRenderers(1).size() > 0) {
+					if (PMS.get().getFoundRenderers().size() > 0) {
 						delay = 30000;
 					} else {
 						delay = 10000;
@@ -548,13 +541,9 @@ public class UPNPHelper extends UPNPControl {
 
 		if (message.equals(ALIVE)) {
 			sb.append("CACHE-CONTROL: max-age=1800").append(CRLF);
-		}
-
-		if (message.equals(ALIVE)) {
 			sb.append("SERVER: ").append(PMS.get().getServerName()).append(CRLF);
 		}
 
-		sb.append(CRLF);
 		return sb.toString();
 	}
 
