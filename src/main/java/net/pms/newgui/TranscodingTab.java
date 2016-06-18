d56e45ad050f00619879d6205c99ee4543326ca6
Samuel <sirimangala@googlemail.com>
2016-06-19 00:27:53 +0100

    Thai

--- src/main/java/net/pms/newgui/TranscodingTab.java
+++ src/main/java/net/pms/newgui/TranscodingTab.java
@@ -840,7 +840,7 @@ public class TranscodingTab {
 			"ISO-8859-1", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4",
 			"ISO-8859-5", "ISO-8859-6", "ISO-8859-7", "ISO-8859-8",
 			"ISO-8859-9", "ISO-8859-10", "ISO-8859-11", "ISO-8859-13",
-			"ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "Big5", "EUC-JP",
+			"ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "TIS-620", "Big5", "EUC-JP",
 			"EUC-KR", "GB18030", "IBM420", "IBM424", "KOI8-R", "Shift_JIS"
 		};
 		String[] values = new String[]{
