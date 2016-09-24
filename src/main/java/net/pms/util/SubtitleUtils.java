5a7850d7ae401b651e4a4618f2c3147f06368b50
valib <liborvala@seznam.cz>
2016-09-24 23:57:16 +0200

    Update regex to simplify it

--- src/main/java/net/pms/util/SubtitleUtils.java
+++ src/main/java/net/pms/util/SubtitleUtils.java
@@ -639,7 +639,7 @@ public class SubtitleUtils {
 		BufferedReader input = FileUtil.bufferedReaderWithCorrectCharset(file);
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		Writer writer = new OutputStreamWriter(os, Charset.forName(CHARSET_UTF_8));
-		Pattern pattern = Pattern.compile("\\</?(?:b|i|s|u|font[^\\>]*)\\>|\\{\\\\[^\\}]+\\}|\\\\h|\\\\N");
+		Pattern pattern = Pattern.compile("\\</?(?:b|i|s|u|font[^\\>]*)\\>|\\{\\\\.*?}|\\\\h|\\\\N");
 		String line;
 		while ((line = input.readLine()) != null) {
 			line = pattern.matcher(line).replaceAll("") + "\n";
