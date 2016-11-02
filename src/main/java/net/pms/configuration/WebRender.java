830921803057cca16959475e63510264f6b23133
Samuel <sirimangala@googlemail.com>
2016-11-02 02:22:06 +0000

    Update WebRender.java
    http://www.webmproject.org/docs/container/
    http://www.nextinpact.com/news/101229-firefox-51-gerera-nativement-codec-audio-flac.htm
    https://bugzilla.mozilla.org/show_bug.cgi?id=1195723
    https://developer.mozilla.org/en-US/docs/Web/HTML/Supported_media_formats#Browser_compatibility

--- src/main/java/net/pms/configuration/WebRender.java
+++ src/main/java/net/pms/configuration/WebRender.java
@@ -107,13 +107,16 @@ public class WebRender extends DeviceConfiguration implements RendererConfigurat
 		// FIXME: These are just preliminary
 		configuration.addProperty(MEDIAPARSERV2, true);
 		configuration.addProperty(MEDIAPARSERV2_THUMB, true);
+		configuration.addProperty(SUPPORTED, "f:flac m:audio/x-flac");
 		configuration.addProperty(SUPPORTED, "f:flv v:h264|hls a:aac m:video/flash");
-		configuration.addProperty(SUPPORTED, "f:mp4 m:video/mp4");
+		configuration.addProperty(SUPPORTED, "f:mp4 v:h264 a:aac|mp3 m:video/mp4");
+		configuration.addProperty(SUPPORTED, "f:mp4 a:aac|mp3 m:audio/mp4");
 		configuration.addProperty(SUPPORTED, "f:mp3 n:2 m:audio/mpeg");
 		configuration.addProperty(SUPPORTED, "f:ogg v:theora m:video/ogg");
 		configuration.addProperty(SUPPORTED, "f:ogg a:vorbis|flac m:audio/ogg");
-		configuration.addProperty(SUPPORTED, "f:wav n:2 m:audio/wav");
+		configuration.addProperty(SUPPORTED, "f:wav n:2 a:lpcm m:audio/wav");
 		configuration.addProperty(SUPPORTED, "f:webm v:vp8|vp9 m:video/webm");
+		configuration.addProperty(SUPPORTED, "f:webm a:vorbis|opus m:audio/webm");
 		configuration.addProperty(SUPPORTED, "f:bmp m:image/bmp");
 		configuration.addProperty(SUPPORTED, "f:jpg m:image/jpeg");
 		configuration.addProperty(SUPPORTED, "f:png m:image/png");
