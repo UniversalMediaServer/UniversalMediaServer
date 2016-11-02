08feda895b1292e24509ced965d7b6989b9f9e81
Samuel <sirimangala@googlemail.com>
2016-11-02 16:01:13 +0000

    Update WebRender.java

--- src/main/java/net/pms/configuration/WebRender.java
+++ src/main/java/net/pms/configuration/WebRender.java
@@ -108,8 +108,8 @@ public class WebRender extends DeviceConfiguration implements RendererConfigurat
 		configuration.addProperty(MEDIAPARSERV2, true);
 		configuration.addProperty(MEDIAPARSERV2_THUMB, true);
 		configuration.addProperty(SUPPORTED, "f:flac m:audio/flac");
-		configuration.addProperty(SUPPORTED, "f:flv v:h263|h264|mp4 a:aac|mp3 m:video/flash");
 		configuration.addProperty(SUPPORTED, "f:flv v:hls a:aac|aac-he|ac3|mp3 m:video/flash");
+		configuration.addProperty(SUPPORTED, "f:flv v:h263|h264|mp4 a:aac|mp3 m:video/flash");
 		configuration.addProperty(SUPPORTED, "f:mp4 v:h264 a:aac|mp3 m:video/mp4");
 		configuration.addProperty(SUPPORTED, "f:mp4 a:aac|mp3 m:audio/mp4");
 		configuration.addProperty(SUPPORTED, "f:mp3 n:2 m:audio/mpeg");
