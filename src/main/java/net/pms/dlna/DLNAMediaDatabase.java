48515f21cfeb69406b563bb0fc83fe4bb38dd283
Nadahar <Nadahar@users.noreply.github.com>
2016-11-05 19:47:37 +0100

    DMS 3f0760aacea9865323d571c22a01519e3441bdce

--- src/main/java/net/pms/dlna/DLNAMediaDatabase.java
+++ src/main/java/net/pms/dlna/DLNAMediaDatabase.java
@@ -104,7 +104,7 @@ public class DLNAMediaDatabase implements Runnable {
 		dbName = name;
 		File profileDirectory = new File(configuration.getProfileDirectory());
 		dbDir = new File(PMS.isRunningTests() || profileDirectory.isDirectory() ? configuration.getProfileDirectory() : null, "database").getAbsolutePath();
-		url = Constants.START_URL + dbDir + File.separator + dbName;
+		url = Constants.START_URL + dbDir + File.separator + dbName + (configuration.getLoggingDatabase() ? ";TRACE_LEVEL_FILE=4" : "");
 		LOGGER.debug("Using database URL: " + url);
 		LOGGER.info("Using database located at: " + dbDir);
 
@@ -122,7 +122,7 @@ public class DLNAMediaDatabase implements Runnable {
 	}
 
 	/**
-	 * Gets the name of the database file.
+	 * Gets the name of the database file
 	 *
 	 * @return The filename
 	 */
@@ -141,7 +141,7 @@ public class DLNAMediaDatabase implements Runnable {
 	 * <strong>Important: Every connection must be closed after use</strong>
 	 *
 	 * @return the new connection
-	 * @throws SQLException if an SQL error occurs during the operation.
+	 * @throws SQLException
 	 */
 	public Connection getConnection() throws SQLException {
 		return cp.getConnection();
@@ -254,7 +254,6 @@ public class DLNAMediaDatabase implements Runnable {
 					LOGGER.trace("", se);
 				}
 			}
-
 			try {
 				StringBuilder sb = new StringBuilder();
 				sb.append("CREATE TABLE FILES (");
@@ -299,7 +298,6 @@ public class DLNAMediaDatabase implements Runnable {
 					LOGGER.trace("Creating table FILES with:\n\n{}\n", sb.toString());
 				}
 				executeUpdate(conn, sb.toString());
-
 				sb = new StringBuilder();
 				sb.append("CREATE TABLE AUDIOTRACKS (");
 				sb.append("  ID                INT              NOT NULL");
@@ -328,7 +326,6 @@ public class DLNAMediaDatabase implements Runnable {
 					LOGGER.trace("Creating table AUDIOTRACKS with:\n\n{}\n", sb.toString());
 				}
 				executeUpdate(conn, sb.toString());
-
 				sb = new StringBuilder();
 				sb.append("CREATE TABLE SUBTRACKS (");
 				sb.append("  ID       INT              NOT NULL");
