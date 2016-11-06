efde6e9079f6304e417a1e44097fa71532a36495
Samuel <sirimangala@googlemail.com>
2016-11-06 23:02:36 +0000

    Update DLNAMediaInfo.java
    @Valib So, that is fine ?

--- src/main/java/net/pms/dlna/DLNAMediaInfo.java
+++ src/main/java/net/pms/dlna/DLNAMediaInfo.java
@@ -1384,7 +1384,7 @@ public class DLNAMediaInfo implements Cloneable {
 					mimeType = HTTPResource.BMP_TYPEMIME;
 				} else if ("webp".equals(codecV) || "webp".equals(container)) {
 					mimeType = HTTPResource.WEBP_TYPEMIME;	
-				} else if (codecV.startsWith("h264") || codecV.equals("h263") || codecV.toLowerCase().equals("mpeg4") || codecV.toLowerCase().equals("mp4")) {
+				} else if (codecV.startsWith("h264") || codecV.equals("h263") || codecV().equals("mpeg4") || codecV().equals("mp4")) {
 					mimeType = HTTPResource.MP4_TYPEMIME;
 				} else if (codecV.contains("mpeg") || codecV.contains("mpg")) {
 					mimeType = HTTPResource.MPEG_TYPEMIME;
