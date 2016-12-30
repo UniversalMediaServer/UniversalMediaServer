796940c9f32d435fb8d7917bdce16ccaa2e2541b
Nadahar <Nadahar@users.noreply.github.com>
2016-12-30 09:56:00 +0100

    - Refactored RAW.parse():   - Only parse RAW using DCRaw if DCRaw is enabled   - Parse RAW using the normal image parser if DCRaw is disabled   - Improve RAW parsing with more/better information - Created ImagesUtil.getThumbnailFromMetadata() to get Exif thumbnail if available and ImageIO can't parse (RAW)

--- src/main/java/net/pms/formats/RAW.java
+++ src/main/java/net/pms/formats/RAW.java
@@ -1,20 +1,33 @@
 package net.pms.formats;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
-import net.coobird.thumbnailator.Thumbnails;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.pms.PMS;
+import net.pms.configuration.FormatConfiguration;
 import net.pms.configuration.PmsConfiguration;
 import net.pms.configuration.RendererConfiguration;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAThumbnail;
+import net.pms.dlna.ImageInfo;
 import net.pms.dlna.InputFile;
+import net.pms.encoders.PlayerFactory;
 import net.pms.encoders.RAWThumbnailer;
 import net.pms.io.OutputParams;
 import net.pms.io.ProcessWrapperImpl;
+import net.pms.util.ImagesUtil;
+import net.pms.util.ImagesUtil.ScaleType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.drew.imaging.FileType;
+import com.drew.imaging.FileTypeDetector;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
 
 public class RAW extends JPG {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RAW.class);
@@ -83,57 +96,92 @@ public class RAW extends JPG {
 	public void parse(DLNAMediaInfo media, InputFile file, int type, RendererConfiguration renderer) {
 		PmsConfiguration configuration = PMS.getConfiguration(renderer);
 		try {
-			OutputParams params = new OutputParams(configuration);
-			params.waitbeforestart = 1;
-			params.minBufferSize = 1;
-			params.maxBufferSize = 6;
-			params.hidebuffer = true;
+			// Only parse using DCRaw if it is enabled
+			if (PlayerFactory.getEnabledPlayer(RAWThumbnailer.class, this) != null) {
+				OutputParams params = new OutputParams(configuration);
+				params.waitbeforestart = 1;
+				params.minBufferSize = 1;
+				params.maxBufferSize = 6;
+				params.hidebuffer = true;
 
-			String cmdArray[] = new String[4];
-			cmdArray[0] = configuration.getDCRawPath();
-			cmdArray[1] = "-i";
-			cmdArray[2] = "-v";
-			if (file.getFile() != null) {
-				cmdArray[3] = file.getFile().getAbsolutePath();
-			}
-
-			params.log = true;
-			ProcessWrapperImpl pw = new ProcessWrapperImpl(cmdArray, params, true, false);
-			pw.runInSameThread();
-
-			List<String> list = pw.getOtherResults();
-			for (String s : list) {
-				if (s.startsWith("Thumb size:  ")) {
-					String sz = s.substring(13);
-					media.setWidth(Integer.parseInt(sz.substring(0, sz.indexOf('x')).trim()));
-					media.setHeight(Integer.parseInt(sz.substring(sz.indexOf('x') + 1).trim()));
-					break;
+				String cmdArray[] = new String[4];
+				cmdArray[0] = configuration.getDCRawPath();
+				cmdArray[1] = "-i";
+				cmdArray[2] = "-v";
+				if (file.getFile() != null) {
+					cmdArray[3] = file.getFile().getAbsolutePath();
 				}
-			}
 
-			if (media.getWidth() > 0) {
-				byte[] image = RAWThumbnailer.getThumbnail(params, file.getFile().getAbsolutePath());
-				media.setSize(image.length);
-				// XXX why the image size is set to thumbnail size and the codecV and container is set to RAW when thumbnail is in the JPEG format
-				media.setCodecV("raw");
-				media.setContainer("raw");
+				params.log = true;
+				ProcessWrapperImpl pw = new ProcessWrapperImpl(cmdArray, params, true, false);
+				pw.runInSameThread();
 
-				if (configuration.getImageThumbnailsEnabled()) {
-					// Resize the thumbnail image using the Thumbnailator library
-					ByteArrayOutputStream out = new ByteArrayOutputStream();
-					Thumbnails.of(new ByteArrayInputStream(image))
-								.size(320, 180)
-								.outputFormat("JPEG")
-								.outputQuality(1.0f)
-								.toOutputStream(out);
-					media.setThumb(DLNAThumbnail.toThumbnail(out.toByteArray()));
+				List<String> list = pw.getOtherResults();
+				Pattern pattern = Pattern.compile("^Output size:\\s*(\\d+)\\s*x\\s*(\\d+)");
+				Matcher matcher;
+				for (String s : list) {
+					matcher = pattern.matcher(s);
+					if (matcher.find()) {
+						media.setWidth(Integer.parseInt(matcher.group(1)));
+						media.setHeight(Integer.parseInt(matcher.group(2)));
+						break;
+					}
 				}
-			}
 
+				media.setCodecV(FormatConfiguration.RAW);
+				media.setContainer(FormatConfiguration.RAW);
+
+				ImageInfo imageInfo = null;
+				if (file.getFile() != null) {
+					Metadata metadata = null;
+					FileType fileType = null;
+					try (BufferedInputStream inputStream = new BufferedInputStream(Files.newInputStream(file.getFile().toPath()))) {
+						try {
+							fileType = FileTypeDetector.detectFileType(inputStream);
+							metadata = ImagesUtil.getMetadata(inputStream, fileType);
+						} catch (IOException e) {
+							metadata = new Metadata();
+							LOGGER.debug("Error reading \"{}\": {}", file.getFile().getAbsolutePath(), e.getMessage());
+							LOGGER.trace("", e);
+						} catch (ImageProcessingException e) {
+							metadata = new Metadata();
+							LOGGER.debug(
+								"Error parsing {} metadata for \"{}\": {}",
+								fileType.toString().toUpperCase(Locale.ROOT),
+								file.getFile().getAbsolutePath(),
+								e.getMessage()
+							);
+							LOGGER.trace("", e);
+						}
+					}
+					try {
+						imageInfo = new ImageInfo(metadata, ImageFormat.toImageFormat(fileType), file.getSize(), false);
+					} catch (MetadataException e) {
+						LOGGER.warn("Unable to parse metadata for \"{}\": {}", file.getFile().getAbsolutePath(), e.getMessage());
+						LOGGER.trace("", e);
+					}
+				}
+				media.setImageInfo(imageInfo);
+
+				if (media.getWidth() > 0 && configuration.getImageThumbnailsEnabled()) {
+					byte[] image = RAWThumbnailer.getThumbnail(params, file.getFile().getAbsolutePath());
+					media.setThumb(DLNAThumbnail.toThumbnail(image, 320, 320, ScaleType.MAX, ImageFormat.JPEG, false));
+				}
+			} else {
+				ImagesUtil.parseImage(file.getFile(), media);
+			}
+			media.setSize(file.getSize());
+			media.setImageCount(1);
 			media.finalize(type, file);
 			media.setMediaparsed(true);
-		} catch (Exception e) {
-			LOGGER.debug("Caught exception", e);
+
+		} catch (IOException e) {
+			LOGGER.error(
+				"Error parsing RAW file \"{}\": {}",
+				file.getFile() != null ? file.getFile().getAbsolutePath() : "Unknown",
+				e.getMessage()
+			);
+			LOGGER.trace("", e);
 		}
 	}
 }
