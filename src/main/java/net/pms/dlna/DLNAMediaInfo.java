12ed25eec0c3b6bff418aac39f8432f829957813
SubJunk <subjunk@gmail.com>
2017-01-28 20:27:36 +1300

    Log AVC level

--- src/main/java/net/pms/dlna/DLNAMediaInfo.java
+++ src/main/java/net/pms/dlna/DLNAMediaInfo.java
@@ -1693,6 +1693,11 @@ public class DLNAMediaInfo implements Cloneable {
 			result.append(matrixCoefficients);
 		}
 
+		if (isNotBlank(avcLevel)) {
+			result.append(", avc level: ");
+			result.append(avcLevel);
+		}
+
 		if (isNotBlank(fileTitleFromMetadata)) {
 			result.append(", file title from metadata: ");
 			result.append(fileTitleFromMetadata);
