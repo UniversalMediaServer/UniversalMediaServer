UniversalMediaServer/UniversalMediaServer

commit 3d65caecad099c5504f4687f58ac15b38cf9bab7

Make commit ef4e8a4 more general

src/main/java/net/pms/dlna/LibMediaInfoParser.java
@@ -1,6 +1,7 @@
 package net.pms.dlna;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,6 +10,8 @@ import net.pms.configuration.RendererConfiguration;
 import net.pms.dlna.MediaInfo.InfoType;
 import net.pms.dlna.MediaInfo.StreamType;
 import net.pms.formats.v2.SubtitleType;
+import net.pms.util.FileUtil;
+
 import org.apache.commons.codec.binary.Base64;
 import static org.apache.commons.lang3.StringUtils.*;
 import org.slf4j.Logger;
@@ -398,7 +401,7 @@ public class LibMediaInfoParser {
 			format = FormatConfiguration.WEBM;
 		} else if (value.equals("qt") || value.equals("quicktime")) {
 			format = FormatConfiguration.MOV;
-		} else if (value.equals("isom") || value.startsWith("mp4") || value.equals("20") || value.equals("m4v") || value.startsWith("mpeg-4") || value.startsWith("hvc1")) {
+		} else if (value.equals("isom") || value.startsWith("mp4") || value.equals("20") || value.equals("m4v") || value.startsWith("mpeg-4")) {
 			format = FormatConfiguration.MP4;
 		} else if (value.contains("mpeg-ps")) {
 			format = FormatConfiguration.MPEGPS;
@@ -545,6 +548,9 @@ public class LibMediaInfoParser {
 			} else if (streamType == StreamType.Audio) {
 				audio.setCodecA(format);
 			}
+		// format not found so set container type based on the file extension. It will be overwritten when the correct type will be found
+		} else if (streamType == StreamType.General && media.getContainer() == null) { 
+			media.setContainer(FileUtil.getExtension(file.getAbsolutePath()));
 		}
 	}
 

commit ecbeea9ad7eedb7fa925f5420e79bccf52a0930c

- Fixed regression in 3d65caecad099c5504f4687f58ac15b38cf9bab7 and extension case handling - Formatting

src/main/java/net/pms/configuration/FormatConfiguration.java
@@ -169,13 +169,12 @@ public class FormatConfiguration {
 			if (StringUtils.isBlank(format)) { // required
 				LOGGER.warn("No format supplied");
 				return false;
-			} else {
-				try {
-					pFormat = Pattern.compile(format);
-				} catch (PatternSyntaxException pse) {
-					LOGGER.error("Error parsing format: " + format, pse);
-					return false;
-				}
+			}
+			try {
+				pFormat = Pattern.compile(format);
+			} catch (PatternSyntaxException pse) {
+				LOGGER.error("Error parsing format: " + format, pse);
+				return false;
 			}
 
 			if (videoCodec != null) {
@@ -512,31 +511,30 @@ public class FormatConfiguration {
 				media.getHeight(),
 				media.getExtras()
 			);
-		} else {
-			String finalMimeType = null;
-
-			for (DLNAMediaAudio audio : media.getAudioTracksList()) {
-				String mimeType = match(
-					media.getContainer(),
-					media.getCodecV(),
-					audio.getCodecA(),
-					audio.getAudioProperties().getNumberOfChannels(),
-					audio.getSampleRate(),
-					media.getBitrate(),
-					media.getWidth(),
-					media.getHeight(),
-					media.getExtras()
-				);
-
-				finalMimeType = mimeType;
-
-				if (mimeType == null) { // if at least one audio track is not compatible, the file must be transcoded.
-					return null;
-				}
-			}
-
-			return finalMimeType;
 		}
+		String finalMimeType = null;
+
+		for (DLNAMediaAudio audio : media.getAudioTracksList()) {
+			String mimeType = match(
+				media.getContainer(),
+				media.getCodecV(),
+				audio.getCodecA(),
+				audio.getAudioProperties().getNumberOfChannels(),
+				audio.getSampleRate(),
+				media.getBitrate(),
+				media.getWidth(),
+				media.getHeight(),
+				media.getExtras()
+			);
+
+			finalMimeType = mimeType;
+
+			if (mimeType == null) { // if at least one audio track is not compatible, the file must be transcoded.
+				return null;
+			}
+		}
+
+		return finalMimeType;
 	}
 
 	public String match(String container, String videoCodec, String audioCodec) {
@@ -586,7 +584,7 @@ public class FormatConfiguration {
 		return matchedMimeType;
 	}
 
-	private SupportSpec parseSupportLine(String line) {
+	private static SupportSpec parseSupportLine(String line) {
 		StringTokenizer st = new StringTokenizer(line, "\t ");
 		SupportSpec supportSpec = new SupportSpec();
 

src/main/java/net/pms/dlna/LibMediaInfoParser.java
@@ -658,7 +658,7 @@ public class LibMediaInfoParser {
 			}
 		// format not found so set container type based on the file extension. It will be overwritten when the correct type will be found
 		} else if (streamType == StreamType.General && media.getContainer() == null) {
-			media.setContainer(FileUtil.getExtension(file.getAbsolutePath()));
+			media.setContainer(FileUtil.getExtension(file.getAbsolutePath()).toLowerCase(Locale.ROOT));
 		}
 	}
 
@@ -718,20 +718,18 @@ public class LibMediaInfoParser {
 		final String avcLevel = substringAfterLast(lowerCase(value), "@l");
 		if (isNotBlank(avcLevel)) {
 			return avcLevel;
-		} else {
-			LOGGER.warn("Could not parse AvcLevel value {}." , value);
-			return null;
 		}
+		LOGGER.warn("Could not parse AvcLevel value {}." , value);
+		return null;
 	}
 
 	public static String getAvcProfile(String value) {
 		String profile = substringBefore(lowerCase(value), "@l");
 		if (isNotBlank(profile)) {
 			return profile;
-		} else {
-			LOGGER.warn("Could not parse AvcProfile value {}." , value);
-			return null;
 		}
+		LOGGER.warn("Could not parse AvcProfile value {}." , value);
+		return null;
 	}
 
 	public static int getBitrate(String value) {

src/main/java/net/pms/formats/Format.java
@@ -18,6 +18,7 @@
  */
 package net.pms.formats;
 
+import java.util.Locale;
 import net.pms.configuration.RendererConfiguration;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.InputFile;
@@ -220,6 +221,7 @@ public abstract class Format implements Cloneable {
 	 *
 	 * @deprecated
 	 */
+	@Deprecated
 	public void setIcon(String filename) {
 		icon = filename;
 	}
@@ -241,7 +243,7 @@ public abstract class Format implements Cloneable {
 			return false;
 		}
 
-		filename = filename.toLowerCase();
+		filename = filename.toLowerCase(Locale.ROOT);
 		String[] supportedExtensions = getSupportedExtensions();
 
 		if (supportedExtensions != null) {
@@ -251,7 +253,7 @@ public abstract class Format implements Cloneable {
 			}
 
 			for (String extension : supportedExtensions) {
-				String ext = extension.toLowerCase();
+				String ext = extension.toLowerCase(Locale.ROOT);
 				if (filename.endsWith("." + ext)) {
 					setMatchedExtension(ext);
 					return true;

src/main/java/net/pms/formats/WEB.java
@@ -48,11 +48,10 @@ public class WEB extends Format {
 
 		if (protocol == null) {
 			return false;
-		} else {
-			url = filename;
-			setMatchedExtension(protocol);
-			return true;
 		}
+		url = filename;
+		setMatchedExtension(protocol);
+		return true;
 	}
 
 	@Override

src/main/java/net/pms/util/FileUtil.java
@@ -16,6 +16,7 @@ import java.nio.file.Path;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
@@ -205,7 +206,7 @@ public class FileUtil {
 	public static String getProtocol(String filename) {
 		// Intentionally avoids URI.getScheme(), see above
 		if (isUrl(filename)) {
-			return filename.split("://")[0].toLowerCase();
+			return filename.split("://")[0].toLowerCase(Locale.ROOT);
 		}
 		return null;
 	}