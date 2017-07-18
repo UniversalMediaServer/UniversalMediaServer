1960619f492d31f781fddbf8c855d2a74f07d55d
Nadahar <Nadahar@users.noreply.github.com>
2017-07-18 18:02:15 +0200

    Fixed a ContentDirectory bug where <TotalMatches> would be 0 if startingIndex over bounds (which is perfectly valid)

--- src/main/java/net/pms/network/RequestV2.java
+++ src/main/java/net/pms/network/RequestV2.java
@@ -852,6 +852,8 @@ public class RequestV2 extends HTTPResource {
 
 				if (files != null && filessize > 0) {
 					parentFolder = files.get(0).getParent();
+				} else {
+					parentFolder = PMS.get().getRootFolder(mediaRenderer).getDLNAResource(objectID, mediaRenderer);
 				}
 
 				if (browseDirectChildren && mediaRenderer.isUseMediaInfo() && mediaRenderer.isDLNATreeHack()) {
