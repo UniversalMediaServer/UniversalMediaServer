0cf824b0eda26241264f0448cb69561425a1b559
valib <liborvala@seznam.cz>
2018-01-03 23:52:46 +0100

    Update the web supported mime type

--- src/main/java/net/pms/remote/RemoteUtil.java
+++ src/main/java/net/pms/remote/RemoteUtil.java
@@ -179,7 +179,9 @@ public class RemoteUtil {
 				mime.equals(HTTPResource.AUDIO_OGA_TYPEMIME) ||
 				mime.equals(HTTPResource.AUDIO_MP3_TYPEMIME) ||
 				mime.equals(HTTPResource.PNG_TYPEMIME) ||
-				mime.equals(HTTPResource.JPEG_TYPEMIME)
+				mime.equals(HTTPResource.JPEG_TYPEMIME) ||
+				mime.equals(HTTPResource.GIF_TYPEMIME) ||
+				mime.equals(HTTPResource.TIFF_TYPEMIME)
 			)
 		) {
 			return true;
