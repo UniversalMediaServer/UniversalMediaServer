UniversalMediaServer/UniversalMediaServer

commit 8abd818602e94c1693edf196ecfe341aa9fe8696

Improve external subs streaming support

src/main/java/net/pms/dlna/DLNAMediaSubtitle.java
@@ -45,6 +45,7 @@ public class DLNAMediaSubtitle extends DLNAMediaLang implements Cloneable {
 
 	private String liveSubURL;
 	private String liveSubFile;
+	private boolean isStreamable = false;
 
 	/**
 	 * Returns whether or not the subtitles are embedded.
@@ -225,5 +226,13 @@ public class DLNAMediaSubtitle extends DLNAMediaLang implements Cloneable {
 
 	public String getLiveSubFile() {
 		return liveSubFile;
+	}
+
+	public boolean isStreamable() {
+		return isStreamable;
+	}
+
+	public void setSubsStreamable(boolean isStreamable) {
+		this.isStreamable = isStreamable;
 	}
 }

src/main/java/net/pms/dlna/DLNAResource.java
@@ -535,6 +535,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 							Player.setAudioAndSubs(child.getSystemName(), child.media, params); // set proper subtitles in accordance with user setting
 							if (defaultRenderer.isSubtitlesFormatSupported(params.sid)) {
 								child.media_subtitle = params.sid;
+								child.media_subtitle.setSubsStreamable(true);
 								LOGGER.trace("Set media_subtitle");
 							} else {
 								LOGGER.trace("Did not set media_subtitle because the subtitle format is not supported by this renderer");
@@ -647,6 +648,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 										Player.setAudioAndSubs(child.getSystemName(), child.media, params); // set proper subtitles in accordance with user setting
 										if (defaultRenderer.isSubtitlesFormatSupported(params.sid)) {
 											child.media_subtitle = params.sid;
+											child.media_subtitle.setSubsStreamable(true);
 											LOGGER.trace("Set media_subtitle");
 										} else {
 											LOGGER.trace("Did not set media_subtitle because the subtitle format is not supported by this renderer");
@@ -1498,10 +1500,9 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 		if (!isFolder()) {
 			if (
 				!configuration.isDisableSubtitles() &&
-				media != null &&
-				media_subtitle != null &&
 				player == null &&
-				mediaRenderer.isSubtitlesFormatSupported(media_subtitle)
+				media_subtitle != null &&
+				media_subtitle.isStreamable()
 			) {
 				subsAreValidForStreaming = true;
 				LOGGER.trace("Setting subsAreValidForStreaming to true for " + getName());

commit 110a23b512319b11900f451adfaab656765e5b77

Reversed bug introduced in 8abd818602e94c1693edf196ecfe341aa9fe8696

src/main/java/net/pms/dlna/DLNAMediaSubtitle.java
@@ -47,7 +47,6 @@ public class DLNAMediaSubtitle extends DLNAMediaLang implements Cloneable {
 
 	private String liveSubURL;
 	private String liveSubFile;
-	private boolean isStreamable = false;
 	private File convertedFile;
 
 	/**
@@ -295,14 +294,6 @@ public class DLNAMediaSubtitle extends DLNAMediaLang implements Cloneable {
 
 	public String getLiveSubFile() {
 		return liveSubFile;
-	}
-
-	public boolean isStreamable() {
-		return isExternal() && isStreamable;
-	}
-
-	public void setSubsStreamable(boolean isStreamable) {
-		this.isStreamable = isStreamable;
 	}
 
 	public void setConvertedFile (File convertedFile) {

src/main/java/net/pms/dlna/DLNAResource.java
@@ -869,7 +869,6 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 					if (params.sid.isExternal()) {
 						if (renderer != null && renderer.isExternalSubtitlesFormatSupported(params.sid, media)) {
 							media_subtitle = params.sid;
-							media_subtitle.setSubsStreamable(true);
 							LOGGER.trace("This video has external subtitles that could be streamed");
 						} else {
 							hasSubsToTranscode = true;
@@ -1611,7 +1610,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 		String subtitleFormat;
 		String subtitleLanguage;
 		boolean isNamedNoEncoding = false;
-		boolean subsAreValidForStreaming = media_subtitle != null && media_subtitle.isStreamable() && mediaRenderer != null && mediaRenderer.streamSubsForTranscodedVideo();
+		boolean subsAreValidForStreaming = media_subtitle != null && mediaRenderer != null && mediaRenderer.streamSubsForTranscodedVideo() && mediaRenderer.isExternalSubtitlesFormatSupported(media_subtitle, media);
 		if (this instanceof RealFile && !isFolder()) {
 			RealFile rf = (RealFile) this;
 			if (configurationSpecificToRenderer.isPrettifyFilenames() && getFormat() != null && getFormat().isVideo()) {
@@ -2177,9 +2176,6 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 										if (configurationSpecificToRenderer.isDisableSubtitles() || (matchedSub.getLang() != null && matchedSub.getLang().equals("off"))) {
 											LOGGER.trace("Disabled the subtitles: " + matchedSub);
 										} else {
-											if (mediaRenderer.isExternalSubtitlesFormatSupported(matchedSub, media)) {
-												matchedSub.setSubsStreamable(true);
-											}
 											params.sid = matchedSub;
 											media_subtitle = params.sid;
 										}
@@ -2214,9 +2210,6 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 																LOGGER.trace("Found external forced file: " + sub.getExternalFile().getAbsolutePath());
 															}
 
-															if (mediaRenderer.isExternalSubtitlesFormatSupported(sub, media)) {
-																sub.setSubsStreamable(true);
-															}
 															params.sid = sub;
 															media_subtitle = params.sid;
 															forcedSubsFound = true;
@@ -2231,9 +2224,6 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 													LOGGER.trace("Found subtitles track: " + sub);
 													if (sub.getExternalFile() != null) {
 														LOGGER.trace("Found external file: " + sub.getExternalFile().getAbsolutePath());
-														if (mediaRenderer.isExternalSubtitlesFormatSupported(sub, media)) {
-															sub.setSubsStreamable(true);
-														}
 														params.sid = sub;
 														media_subtitle = params.sid;
 														break;
@@ -2264,9 +2254,6 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 															sub.getExternalFile() != null
 														)
 													) {
-														if (mediaRenderer.isExternalSubtitlesFormatSupported(sub, media)) {
-															sub.setSubsStreamable(true);
-														}
 														params.sid = sub;
 														LOGGER.trace("Matched subtitles track: " + params.sid);
 														break;
@@ -2415,9 +2402,9 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 			if (format != null && format.isVideo()) {
 				if (
 					!configurationSpecificToRenderer.isDisableSubtitles() &&
-					(player != null && mediaRenderer.streamSubsForTranscodedVideo() || player == null) &&
+					(player == null || mediaRenderer.streamSubsForTranscodedVideo()) &&
 					media_subtitle != null &&
-					media_subtitle.isStreamable()
+					mediaRenderer.isExternalSubtitlesFormatSupported(media_subtitle, media)
 				) {
 					subsAreValidForStreaming = true;
 					LOGGER.trace("Setting subsAreValidForStreaming to true for " + media_subtitle.getExternalFile().getName());

src/main/java/net/pms/dlna/FileTranscodeVirtualFolder.java
@@ -207,25 +207,6 @@ public class FileTranscodeVirtualFolder extends VirtualFolder {
 			addChildInternal(noTranscode);
 			addChapterFolder(noTranscode);
 
-			// add options for renderer capable to handle streamed subtitles
-			if (!configuration.isDisableSubtitles() && renderer != null && renderer.isSubtitlesStreamingSupported()) {
-				for (DLNAMediaSubtitle subtitle : subtitleTracks) {
-					// only add the option if the renderer supports the given format
-					if (subtitle.isExternal()) { // do not check for embedded subs
-						if (renderer.isExternalSubtitlesFormatSupported(subtitle, child.getMedia())) {
-							DLNAResource copy = createResourceWithAudioSubtitlePlayer(child, null, subtitle, null);
-							copy.getMediaSubtitle().setSubsStreamable(true);
-							entries.add(copy);
-							LOGGER.trace(
-								"Duplicating {} for direct streaming subtitles {}",
-								child.getName(),
-								subtitle.toString()
-							);
-						}
-					}
-				}
-			}
-
 			/*
 			 we add (or may add) a null entry to the audio list and/or subtitle list
 			 to ensure the inner loop is always entered:

src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -193,9 +193,11 @@ public class FFMpegVideo extends Player {
 						originalSubsFilename = params.sid.getExternalFile().getAbsolutePath();
 					}
 				} else if (params.sid.isExternal()) {
-					if (params.sid.isStreamable() && renderer.streamSubsForTranscodedVideo()) { // when subs are streamable do not transcode them
-						originalSubsFilename = null;
-					} else {
+					if (
+						!renderer.streamSubsForTranscodedVideo() ||
+						!renderer.isExternalSubtitlesFormatSupported(params.sid, media)
+					) {
+						// Only transcode subtitles if they aren't streamable
 						originalSubsFilename = params.sid.getExternalFile().getAbsolutePath();
 					}
 				} else if (params.sid.isEmbedded()) {

src/main/java/net/pms/encoders/MEncoderVideo.java
@@ -1668,7 +1668,11 @@ public class MEncoderVideo extends Player {
 					cmdList.add(externalSubtitlesFileName.substring(0, externalSubtitlesFileName.length() - 4));
 					cmdList.add("-slang");
 					cmdList.add("" + params.sid.getLang());
-				} else if (!params.sid.isStreamable() && !params.mediaRenderer.streamSubsForTranscodedVideo()) { // when subs are streamable do not transcode them
+				} else if (
+					!params.mediaRenderer.streamSubsForTranscodedVideo() ||
+					!params.mediaRenderer.isExternalSubtitlesFormatSupported(params.sid, media)
+				) {
+					// Only transcode subtitles if they aren't streamable
 					cmdList.add("-sub");
 					DLNAMediaSubtitle convertedSubs = dlna.getMediaSubtitle();
 					if (media.is3d()) {

src/main/java/net/pms/encoders/VLCVideo.java
@@ -518,7 +518,13 @@ public class VLCVideo extends Player {
 
 		// Handle subtitle language
 		if (params.sid != null) { // User specified language at the client, acknowledge it
-			if (params.sid.isExternal() && !params.sid.isStreamable() && !params.mediaRenderer.streamSubsForTranscodedVideo()) {
+			if (
+				params.sid.isExternal() &&
+				(
+					!params.mediaRenderer.streamSubsForTranscodedVideo() ||
+					!params.mediaRenderer.isExternalSubtitlesFormatSupported(params.sid, media)
+				)
+			) {
 				String externalSubtitlesFileName;
 
 				// External subtitle file

src/main/java/net/pms/network/Request.java
@@ -474,7 +474,12 @@ public class Request extends HTTPResource {
 						startStopListenerDelegate.start(dlna);
 						appendToHeader(responseHeader, "Content-Type: " + getRendererMimeType(dlna.mimeType(), mediaRenderer, dlna.getMedia()));
 
-						if (dlna.getMedia() != null && !configuration.isDisableSubtitles() && dlna.getMediaSubtitle() != null && dlna.getMediaSubtitle().isStreamable()) {
+						if (
+							dlna.getMedia() != null &&
+							dlna.getMediaSubtitle() != null &&
+							!configuration.isDisableSubtitles() &&
+							mediaRenderer.isExternalSubtitlesFormatSupported(dlna.getMediaSubtitle(), dlna.getMedia())
+						) {
 							// Some renderers (like Samsung devices) allow a custom header for a subtitle URL
 							String subtitleHttpHeader = mediaRenderer.getSubtitleHttpHeader();
 							if (isNotBlank(subtitleHttpHeader)) {
@@ -506,8 +511,8 @@ public class Request extends HTTPResource {
 							}
 							if (dlna.getMediaSubtitle() == null) {
 								reasons.add("dlna.getMediaSubtitle() is null");
-							} else if (!dlna.getMediaSubtitle().isStreamable()) {
-								reasons.add("dlna.getMediaSubtitle().isStreamable() is false");
+							} else if (!mediaRenderer.isExternalSubtitlesFormatSupported(dlna.getMediaSubtitle(), dlna.getMedia())) {
+								reasons.add("the external subtitles format isn't supported by the renderer");
 							}
 							LOGGER.trace("Did not send subtitle headers because {}", StringUtil.createReadableCombinedString(reasons));
 						}

src/main/java/net/pms/network/RequestV2.java
@@ -491,7 +491,12 @@ public class RequestV2 extends HTTPResource {
 
 					Format format = dlna.getFormat();
 					if (format != null && format.isVideo()) {
-						if (dlna.getMedia() != null && !configuration.isDisableSubtitles() && dlna.getMediaSubtitle() != null && dlna.getMediaSubtitle().isStreamable()) {
+						if (
+							dlna.getMedia() != null &&
+							dlna.getMediaSubtitle() != null &&
+							!configuration.isDisableSubtitles() &&
+							mediaRenderer.isExternalSubtitlesFormatSupported(dlna.getMediaSubtitle(), dlna.getMedia())
+						) {
 							// Some renderers (like Samsung devices) allow a custom header for a subtitle URL
 							String subtitleHttpHeader = mediaRenderer.getSubtitleHttpHeader();
 							if (isNotBlank(subtitleHttpHeader)) {
@@ -523,8 +528,8 @@ public class RequestV2 extends HTTPResource {
 							}
 							if (dlna.getMediaSubtitle() == null) {
 								reasons.add("dlna.getMediaSubtitle() is null");
-							} else if (!dlna.getMediaSubtitle().isStreamable()) {
-								reasons.add("dlna.getMediaSubtitle().isStreamable() is false");
+							} else if (!mediaRenderer.isExternalSubtitlesFormatSupported(dlna.getMediaSubtitle(), dlna.getMedia())) {
+								reasons.add("the external subtitles format isn't supported by the renderer");
 							}
 							LOGGER.trace("Did not send subtitle headers because {}", StringUtil.createReadableCombinedString(reasons));
 						}