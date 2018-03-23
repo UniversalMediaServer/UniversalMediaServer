dfc8f84512b54608b17eead1f2ba6c2f5a4e2fcf
SubJunk <subjunk@gmail.com>
2018-03-24 12:58:32 +1300

    Do faster hashing

--- src/main/java/net/pms/util/OpenSubtitle.java
+++ src/main/java/net/pms/util/OpenSubtitle.java
@@ -93,8 +93,18 @@ public class OpenSubtitle {
 
 	public static String computeHash(File file) throws IOException {
 		long size = file.length();
-		FileInputStream fis = new FileInputStream(file);
-		return computeHash(fis, size);
+		long chunkSizeForFile = Math.min(HASH_CHUNK_SIZE, size);
+
+		FileChannel fileChannel = new FileInputStream(file).getChannel();
+
+		try {
+			long head = computeHashForChunk(fileChannel.map(MapMode.READ_ONLY, 0, chunkSizeForFile));
+			long tail = computeHashForChunk(fileChannel.map(MapMode.READ_ONLY, Math.max(size - HASH_CHUNK_SIZE, 0), chunkSizeForFile));
+
+			return String.format("%016x", size + head + tail);
+		} finally {
+			fileChannel.close();
+		}
 	}
 
 	public static String computeHash(InputStream inputStream, long length) throws IOException {
