4d56ce74b4022f8dfe1b3b2835bb875a285593b4
SubJunk <subjunk@gmail.com>
2018-07-14 17:15:52 +1200

    Stop setting codecv to und instead of null

--- src/main/java/net/pms/dlna/LibMediaInfoParser.java
+++ src/main/java/net/pms/dlna/LibMediaInfoParser.java
@@ -412,10 +412,6 @@ public class LibMediaInfoParser {
 				media.setContainer(DLNAMediaLang.UND);
 			}
 
-			if (media.getCodecV() == null) {
-				media.setCodecV(DLNAMediaLang.UND);
-			}
-
 			media.setMediaparsed(true);
 		}
 	}
