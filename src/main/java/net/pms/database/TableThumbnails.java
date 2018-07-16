89dd4d1b8b5e703667f356f69b88d12c7de3ffb8
SubJunk <subjunk@gmail.com>
2018-07-16 17:16:13 +1200

    Removed unused code

--- src/main/java/net/pms/database/TableThumbnails.java
+++ src/main/java/net/pms/database/TableThumbnails.java
@@ -137,38 +137,6 @@ public final class TableThumbnails extends Tables{
 	}
 
 	/**
-	 * Removes an entry or entries based on its FILENAME. If {@code useLike} is
-	 * {@code true}, {@code filename} must be properly escaped.
-	 *
-	 * @see Tables#sqlLikeEscape(String)
-	 *
-	 * @param filename the filename to remove
-	 * @param useLike {@code true} if {@code LIKE} should be used as the compare
-	 *            operator, {@code false} if {@code =} should be used.
-	 */
-//	public static void remove(final String filename, boolean useLike) {
-//		try (Connection connection = database.getConnection()) {
-//			String query =
-//				"DELETE FROM " + TABLE_NAME + " WHERE FILENAME " +
-//				(useLike ? "LIKE " : "= ") + sqlQuote(filename);
-//			TABLE_LOCK.writeLock().lock();
-//			try (Statement statement = connection.createStatement()) {
-//				int rows = statement.executeUpdate(query);
-//				LOGGER.trace("Removed entries {} in " + TABLE_NAME + " for filename \"{}\"", rows, filename);
-//			} finally {
-//				TABLE_LOCK.writeLock().unlock();
-//			}
-//		} catch (SQLException e) {
-//			LOGGER.error(
-//				"Database error while removing entries from " + TABLE_NAME + " for \"{}\": {}",
-//				filename,
-//				e.getMessage()
-//			);
-//			LOGGER.trace("", e);
-//		}
-//	}
-
-	/**
 	 * Checks and creates or upgrades the table as needed.
 	 *
 	 * @param connection the {@link Connection} to use
