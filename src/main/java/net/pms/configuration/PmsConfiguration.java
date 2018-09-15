cd3ec94382dc13010d05a2c17418575ee066deba
onon765trb <24803846+onon765trb@users.noreply.github.com>
2018-09-15 12:39:10 +0900

    Change names

--- src/main/java/net/pms/configuration/PmsConfiguration.java
+++ src/main/java/net/pms/configuration/PmsConfiguration.java
@@ -109,7 +109,6 @@ public class PmsConfiguration extends RendererConfiguration {
 	protected static final String KEY_AUDIO_USE_PCM = "audio_use_pcm";
 	protected static final String KEY_AUTO_UPDATE = "auto_update";
 	protected static final String KEY_AUTOLOAD_SUBTITLES = "autoload_external_subtitles";
-	protected static final String KEY_AVAILABLE_FFMPEG_GPU_ACCELERATION_METHODS = "available_ffmpeg_gpu_acceleration_methods";
 	protected static final String KEY_AVISYNTH_CONVERT_FPS = "avisynth_convert_fps";
 	protected static final String KEY_AVISYNTH_INTERFRAME = "avisynth_interframe";
 	protected static final String KEY_AVISYNTH_INTERFRAME_GPU = "avisynth_interframegpu";
@@ -144,13 +143,14 @@ public class PmsConfiguration extends RendererConfiguration {
 	protected static final String KEY_ENCODED_AUDIO_PASSTHROUGH = "encoded_audio_passthrough";
 	protected static final String KEY_ENGINES = "engines";
 	protected static final String KEY_FFMPEG_ALTERNATIVE_PATH = "alternativeffmpegpath"; // TODO: FFmpegDVRMSRemux will be removed and DVR-MS will be transcoded
+	protected static final String KEY_FFMPEG_AVAILABLE_GPU_DECODING_ACCELERATION_METHODS = "fmpeg_available_fgpu_decoding_acceleration_methods";
 	protected static final String KEY_FFMPEG_AVISYNTH_CONVERT_FPS = "ffmpeg_avisynth_convertfps";
 	protected static final String KEY_FFMPEG_AVISYNTH_INTERFRAME = "ffmpeg_avisynth_interframe";
 	protected static final String KEY_FFMPEG_AVISYNTH_INTERFRAME_GPU = "ffmpeg_avisynth_interframegpu";
 	protected static final String KEY_FFMPEG_AVISYNTH_MULTITHREADING = "ffmpeg_avisynth_multithreading";
 	protected static final String KEY_FFMPEG_FONTCONFIG = "ffmpeg_fontconfig";
-	protected static final String KEY_FFMPEG_GPU_ACCELERATION_METHOD = "ffmpeg_gpu_acceleration_method";
-	protected static final String KEY_FFMPEG_GPU_ACCELERATION_THREAD_NUMBER = "ffmpeg_gpu_acceleration_thread_number";
+	protected static final String KEY_FFMPEG_GPU_DECODING_ACCELERATION_METHOD = "ffmpeg_gpu_decoding_acceleration_method";
+	protected static final String KEY_FFMPEG_GPU_DECODING_ACCELERATION_THREAD_NUMBER = "ffmpeg_gpu_decoding_acceleration_thread_number";
 	protected static final String KEY_FFMPEG_MENCODER_PROBLEMATIC_SUBTITLES = "ffmpeg_mencoder_problematic_subtitles";
 	protected static final String KEY_FFMPEG_MULTITHREADING = "ffmpeg_multithreading";
 	protected static final String KEY_FFMPEG_MUX_TSMUXER_COMPATIBLE = "ffmpeg_mux_tsmuxer_compatible";
@@ -2134,28 +2134,28 @@ public class PmsConfiguration extends RendererConfiguration {
 		return getBoolean(KEY_FFMPEG_MULTITHREADING, isMultiCore);
 	}
 
-	public String getFFmpegGPUAccelerationMethod() {
-		return getString(KEY_FFMPEG_GPU_ACCELERATION_METHOD, "auto");
+	public String getFFmpegGPUDecodingAccelerationMethod() {
+		return getString(KEY_FFMPEG_GPU_DECODING_ACCELERATION_METHOD, "auto");
 	}
 
-	public void setFFmpegGPUAccelerationMethod(String value) {
-		configuration.setProperty(KEY_FFMPEG_GPU_ACCELERATION_METHOD, value);
+	public void setFFmpegGPUDecodingAccelerationMethod(String value) {
+		configuration.setProperty(KEY_FFMPEG_GPU_DECODING_ACCELERATION_METHOD, value);
 	}
 
-	public String getFFmpegGPUAccelationThreadNumber() {
-		return getString(KEY_FFMPEG_GPU_ACCELERATION_THREAD_NUMBER,"1");
+	public String getFFmpegGPUDecodingAccelationThreadNumber() {
+		return getString(KEY_FFMPEG_GPU_DECODING_ACCELERATION_THREAD_NUMBER,"1");
 	}
 
-	public void setFFmpegGPUAccelationThreadNumber(String value) {
-		configuration.setProperty(KEY_FFMPEG_GPU_ACCELERATION_THREAD_NUMBER, value);
+	public void setFFmpegGPUADecodingAccelationThreadNumber(String value) {
+		configuration.setProperty(KEY_FFMPEG_GPU_DECODING_ACCELERATION_THREAD_NUMBER, value);
 	}
 
-	public String[] getAvailableGPUAccelerationMethods() {
-		return getString(KEY_AVAILABLE_FFMPEG_GPU_ACCELERATION_METHODS, "auto").split(",");
+	public String[] getFFmpegAvailableGPUDecodingAccelerationMethods() {
+		return getString(KEY_FFMPEG_AVAILABLE_GPU_DECODING_ACCELERATION_METHODS, "auto").split(",");
 	}
 
-	public void setAvailableGPUAccelerationMethods(List<String> methods) {
-		configuration.setProperty(KEY_AVAILABLE_FFMPEG_GPU_ACCELERATION_METHODS, listToString(methods));
+	public void setFFmpegAvailableGPUDecodingAccelerationMethods(List<String> methods) {
+		configuration.setProperty(KEY_FFMPEG_AVAILABLE_GPU_DECODING_ACCELERATION_METHODS, listToString(methods));
 	}
 
 	public void setFfmpegAviSynthMultithreading(boolean value) {
