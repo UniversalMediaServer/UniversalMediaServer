683cb4c2952c26272067cec643c7c17dc56e8bba
drakulis <1709738+drakulis@users.noreply.github.com>
2018-11-11 21:05:15 +0100

    Enable Samsung built-in resume functionality (#1662)
    * feat: add support for samsung built-in resume
    
    Closes #665
    
    * feat(bookmark): restrict 'set bookmark' to samsung devices
    
    Closes #665
    
    * feat(bookmark): code cleanup
    
    #665
    
    * feat(bookmark): code cleanup
    
    #665

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -33,6 +33,7 @@ import net.pms.PMS;
 import net.pms.configuration.FormatConfiguration;
 import net.pms.configuration.PmsConfiguration;
 import net.pms.configuration.RendererConfiguration;
+import net.pms.database.TableFilesStatus;
 import net.pms.database.TableThumbnails;
 import net.pms.dlna.DLNAImageProfile.HypotheticalResult;
 import net.pms.dlna.virtual.TranscodeVirtualFolder;
@@ -2511,6 +2512,10 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 			"dc:title",
 			encodeXML(mediaRenderer.getDcTitle(title, nameSuffix, this))
 		);
+		
+		if (mediaRenderer.isSamsung() && this instanceof RealFile) {
+			addBookmark(sb, mediaRenderer.getDcTitle(title, nameSuffix, this));
+		}
 		wireshark.append("\"").append(title).append("\"");
 		if (firstAudioTrack != null) {
 			if (StringUtils.isNotBlank(firstAudioTrack.getAlbum())) {
@@ -2808,6 +2813,22 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 		}
 
 		return sb.toString();
+	}
+	
+	private void addBookmark(StringBuilder sb, String title){
+		try {
+			File file = new File(getFileName());
+			String path = file.getCanonicalPath();
+			int bookmark = TableFilesStatus.getBookmark(path);
+			LOGGER.debug("Setting bookmark for " + path + " => " + bookmark);
+			addXMLTagAndAttribute(
+					sb,
+					"sec:dcmInfo",
+					encodeXML(String.format("CREATIONDATE=0,FOLDER=%s,BM=%d",title,bookmark))
+				);
+		} catch(Exception e) {
+			LOGGER.error("Cannot set bookmark tag for " + title, e);
+		}
 	}
 
 	/**
