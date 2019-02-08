c2b666a1a876d0a25a42fcc21ae219a19a1ece6e
SubJunk <SubJunk@users.noreply.github.com>
2019-02-08 14:23:47 +1300

    Use move instead of renameTo (#1757)
    * Use move instead of renameTo
    
    * Fixed moved boolean

--- src/main/java/net/pms/dlna/MediaMonitor.java
+++ src/main/java/net/pms/dlna/MediaMonitor.java
@@ -4,7 +4,10 @@ import com.sun.jna.Platform;
 import com.sun.jna.platform.FileUtils;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -232,30 +235,29 @@ public class MediaMonitor extends VirtualFolder {
 							boolean moved = false;
 							File newFile = null;
 
-							if (playedFile.renameTo(new File(newDirectory + playedFile.getName()))) {
+							try {
+								Files.move(Paths.get(playedFile.getAbsolutePath()), Paths.get(newDirectory + playedFile.getName()), StandardCopyOption.REPLACE_EXISTING);
 								LOGGER.debug("Moved {} because it has been fully played", playedFile.getName());
 								newFile = new File(newDirectory + playedFile.getName());
 								moved = true;
-							} else {
-								LOGGER.debug("Moving {} failed, trying again in 3 seconds", playedFile.getName());
+							} catch (IOException e) {
+								LOGGER.debug("Moving {} failed, trying again in 3 seconds: {}", playedFile.getName(), e.getMessage());
 
 								try {
 									Thread.sleep(3000);
-
-									if (playedFile.renameTo(new File(newDirectory + playedFile.getName()))) {
-										LOGGER.debug("Moved {} because it has been fully played", playedFile.getName());
-										newFile = new File(newDirectory + playedFile.getName());
-										moved = true;
-									} else {
-										LOGGER.info("Failed to move {}", playedFile.getName());
-									}
-								} catch (InterruptedException e) {
-									LOGGER.warn(
-										"Abandoning moving of {} because the thread was interrupted, probably due to UMS shutdown",
-										e.getMessage()
+									Files.move(Paths.get(playedFile.getAbsolutePath()), Paths.get(newDirectory + playedFile.getName()), StandardCopyOption.REPLACE_EXISTING);
+									LOGGER.debug("Moved {} because it has been fully played", playedFile.getName());
+									newFile = new File(newDirectory + playedFile.getName());
+									moved = true;
+								} catch (InterruptedException e2) {
+									LOGGER.debug(
+										"Abandoning moving of {} because the thread was interrupted, probably due to program shutdown: {}",
+										playedFile.getName(),
+										e2.getMessage()
 									);
-									LOGGER.trace("", e);
 									Thread.currentThread().interrupt();
+								} catch (IOException e3) {
+									LOGGER.debug("Moving {} failed a second time: {}", playedFile.getName(), e3.getMessage());
 								}
 							}
 
