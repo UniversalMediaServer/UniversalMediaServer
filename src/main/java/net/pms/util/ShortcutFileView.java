86292c5b96115ec76993bbee85718eae91d38f89
Klaus Townsend <subjunk@gmail.com>
2020-02-12 20:49:27 +1300

    Support symlinks on Windows

---
+++ src/main/java/net/pms/util/ShortcutFileView.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import javax.swing.filechooser.FileView;
+
+/**
+ * @see https://www.oreilly.com/library/view/swing-hacks/0596009070/chapter-64.html
+ */
+public class ShortcutFileView extends FileView {
+  public boolean isDirLink(File f) {
+    if (f.getName().toLowerCase().endsWith(".lnk")) {
+      return true;
+    }
+    return false;
+  }
+  
+  public Boolean isTraversable(File f) {
+    if (isDirLink(f)) {
+      return new Boolean(true);
+    }
+    return null;
+  }
+}
