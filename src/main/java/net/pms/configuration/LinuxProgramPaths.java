7edbda71e28ef502490f5a1d6f409fadd97a941d
SubJunk <subjunk@gmail.com>
2020-03-29 12:38:05 +1300

    Cleanup

--- src/main/java/net/pms/configuration/LinuxProgramPaths.java
+++ src/main/java/net/pms/configuration/LinuxProgramPaths.java
@@ -27,8 +27,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.pms.util.FilePermissions;
 import net.pms.util.FileUtil;
-import com.sun.jna.Platform;
-
 
 /**
  * This class keeps track of paths to external programs on Linux.
