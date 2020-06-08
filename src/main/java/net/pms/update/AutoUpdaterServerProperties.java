d2ef8bf761d2b4274a9dd7f55fef1e9cb89e5b70
valib <liborvala@seznam.cz>
2020-06-09 00:38:55 +0200

    Update comments

--- src/main/java/net/pms/update/AutoUpdaterServerProperties.java
+++ src/main/java/net/pms/update/AutoUpdaterServerProperties.java
@@ -47,7 +47,7 @@ public class AutoUpdaterServerProperties {
 
 	private String getPlatformSpecificKey(String key) {
 		String os = operatingSystem.toString();
-		 //we use only one windows version so the exact windows version is not relevant
+		 //we use only the one UMS Windows version so the exact OS Windows version is not relevant
 		if (os.startsWith("windows")){
 			os = "windows";
 		}
