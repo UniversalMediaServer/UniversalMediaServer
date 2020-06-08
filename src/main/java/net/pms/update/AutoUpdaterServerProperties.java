1e6c68d36f3e6ba3e6e13d4c4ccd0c3112c2409d
valib <liborvala@seznam.cz>
2020-06-08 23:02:31 +0200

    Fix the auto update for windows OS

--- src/main/java/net/pms/update/AutoUpdaterServerProperties.java
+++ src/main/java/net/pms/update/AutoUpdaterServerProperties.java
@@ -46,6 +46,11 @@ public class AutoUpdaterServerProperties {
 	}
 
 	private String getPlatformSpecificKey(String key) {
-		return key + "." + operatingSystem.toString();
+		String os = operatingSystem.toString();
+		if (os.startsWith("windows")){
+			os = "windows";
+		}
+
+		return key + "." + os;
 	}
 }
