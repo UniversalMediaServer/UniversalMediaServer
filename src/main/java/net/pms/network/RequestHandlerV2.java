311332500fc409167286735d4c908751b650b464
SubJunk <subjunk@gmail.com>
2020-06-15 17:15:55 +1200

    Revert "Fixes ignoring any cling renderers on the same host address (#2090)"
    This reverts commit c36a81b96faa27eef4d96af81889dce8f4e27e4d.

--- src/main/java/net/pms/network/RequestHandlerV2.java
+++ src/main/java/net/pms/network/RequestHandlerV2.java
@@ -103,7 +103,7 @@ public class RequestHandlerV2 extends SimpleChannelInboundHandler<FullHttpReques
 		InetSocketAddress remoteAddress = (InetSocketAddress) ctx.channel().remoteAddress();
 		InetAddress ia = remoteAddress.getAddress();
 		String userAgent = nettyRequest.headers().get(USER_AGENT);
-		boolean isSelf = isSelfRequest(ia, userAgent);
+		boolean isSelf = isLocalClingRequest(ia, userAgent);
 
 		// Filter if required
 		if (isSelf || filterIp(ia)) {
@@ -419,9 +419,10 @@ public class RequestHandlerV2 extends SimpleChannelInboundHandler<FullHttpReques
 		return false;
 	}
 
-	private boolean isSelfRequest(InetAddress ia, String userAgent) {
+	private boolean isLocalClingRequest(InetAddress ia, String userAgent) {
 		return userAgent != null &&
 			userAgent.contains("UMS/") &&
+			userAgent.contains("Cling/") &&
 			ia.getHostAddress().equals(PMS.get().getServer().getHost());
 	}
 
