16507324c9758035b3abb4c426daafa81c08e31f
SubJunk <subjunk@gmail.com>
2020-07-11 14:06:27 +1200

    Fixed running as a Windows service

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -1046,28 +1046,10 @@ public class PMS {
 			assert getConfiguration() != null;
 
 			// Warn the user that this instance will conflict with the Windows service
-			if (Platform.isWindows() && configuration.isShowServiceWarning()) {
+			if (Platform.isWindows()) {
 				boolean isUmsServiceInstalled = WindowsUtil.isUmsServiceInstalled();
 				if (isUmsServiceInstalled) {
-					int option = JOptionPane.showConfirmDialog(
-						null,
-						Messages.getString("Dialog.ServiceWarning"),
-						Messages.getString("Dialog.Warning"),
-						JOptionPane.YES_NO_CANCEL_OPTION,
-						JOptionPane.WARNING_MESSAGE);
-					if (option == JOptionPane.YES_OPTION) {
-						WindowsUtil.uninstallWin32Service();
-						LOGGER.info("Uninstalled UMS Windows service");
-
-						JOptionPane.showMessageDialog(
-							null,
-							Messages.getString("GeneralTab.3"),
-							Messages.getString("Dialog.Information"),
-							JOptionPane.INFORMATION_MESSAGE
-						);
-					} else if (option == JOptionPane.CANCEL_OPTION) {
-						configuration.setShowServiceWarning(false);
-					}
+					LOGGER.info("The Windows service is installed.");
 				}
 			}
 
