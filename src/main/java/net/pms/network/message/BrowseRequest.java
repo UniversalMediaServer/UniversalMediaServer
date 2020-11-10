f7714be3227b97c7c2369e53c21438baa5834b4e
valib <liborvala@seznam.cz>
2020-11-10 22:37:14 +0100

    Import only necessary annotations

--- src/main/java/net/pms/network/message/BrowseRequest.java
+++ src/main/java/net/pms/network/message/BrowseRequest.java
@@ -1,6 +1,8 @@
 package net.pms.network.message;
 
-import jakarta.xml.bind.annotation.*;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
 
 /**
  * Represents payload of `ContentDirectory:1#Browse` request.
