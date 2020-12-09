49c75a78b2ad6c19fb2dd5d88ca0226a2211f34e
sf666 <satfreak666@gmx.de>
2020-12-09 10:26:22 +0100

    if available within the songs audiotag section, MusicBrainz-ReleaseID will be included in a DIDL response as descMetadata. With this information a control point can clearly identify a song for additional services.

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -2405,8 +2405,19 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 				sb.append(getFileURL()).append(transcodedExtension);
 				closeTag(sb, "res");
 			}
+			
+            // DESC Metadata support: add ability for control point to identify song by MusicBrainz TrackID
+			if (media.getFirstAudioTrack() != null && media.getFirstAudioTrack().getMbidRecord() != null) {
+			    openTag(sb, "desc");
+			    addAttribute(sb, "id", "2");
+			    addAttribute(sb, "nameSpace", "http://ums/tags"); // TODO add real namespace  
+			    addAttribute(sb, "type", "ums-tags");
+			    endTag(sb);
+			    addXMLTagAndAttribute(sb, "musicbrainztrackid", media.getFirstAudioTrack().getMbidRecord());
+			    closeTag(sb, "desc");
+			}		    
 		}
-
+		
 		if (subsAreValidForStreaming) {
 			String subsURL = getSubsURL(media_subtitle);
 			if (mediaRenderer.useClosedCaption()) {
