a907d3e3bfb699374c63aac22306ccbfd8d4d33b
sf666 <satfreak666@gmx.de>
2020-12-16 15:55:21 +0100

    Removed symbol after concurrent update ...

--- src/main/java/net/pms/database/TableAudiotracks.java
+++ src/main/java/net/pms/database/TableAudiotracks.java
@@ -54,10 +54,8 @@ public class TableAudiotracks extends Tables{
 					} else if (version > TABLE_VERSION) {
 						LOGGER.warn(
 							"Database table \"" + TABLE_NAME +
-							"\" is from a newer version of UMS. If you experience problems, you could try to move, rename or delete database file \"" +
-							database.getDatabaseFilename() +
-							"\" before starting UMS"
-						);
+							"\" is from a newer version of UMS. If you experience problems, "
+							+ "you could try to move, rename or delete database file.");
 					}
 				} else {
 					// Moving sql from DLNAMediaDatabase to this class. 
