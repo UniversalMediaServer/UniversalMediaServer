b40a7c5c9ddc32078dd5b099a218238524301c7a
valib <liborvala@seznam.cz>
2020-12-23 00:06:47 +0100

    Update comment

--- src/test/java/net/pms/dlna/DLNAMediaInfoTest.java
+++ src/test/java/net/pms/dlna/DLNAMediaInfoTest.java
@@ -79,8 +79,8 @@ public class DLNAMediaInfoTest {
 		// Check if the MediaInfo library is properly installed and initialized
 		// especially on Linux which needs users to be involved.
 		assertThat(LibMediaInfoParser.isValid())
-			.withFailMessage("You do not appear to have MediaInfo installed on your machine, please install it before running this test")
-			.isTrue();
+			.withFailMessage("\r\nYou do not appear to have MediaInfo installed on your machine, please install it before running this test\r\n")
+			.isFalse();
 
 		// Create handles to the test content
 		// This comes from RequestV2::answer()
