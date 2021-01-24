490d058576ebaa2b7b38e830e96c406827b20111
valib <liborvala@seznam.cz>
2021-01-25 00:09:51 +0100

    Speed up the logic.

--- src/main/java/net/pms/encoders/FFMpegVideo.java
+++ src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -573,7 +573,7 @@ public class FFMpegVideo extends Player {
 			if (!bitrateLevel41Limited) {
 				// Set maximum bitrate for MPEG-2 which should be 10.08 Mbit/s
 				// for audio + video bitrate. See the https://en.wikipedia.org/wiki/MPEG-2
-				if (params.getMediaRenderer().isTranscodeToMPEG2() && defaultMaxBitrates[0] > 10 && !dtsRemux) {
+				if (!dtsRemux && params.getMediaRenderer().isTranscodeToMPEG2() && defaultMaxBitrates[0] > 10) {
 					defaultMaxBitrates[0] = 10;
 				}
 
