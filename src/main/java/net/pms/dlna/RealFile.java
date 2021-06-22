509327147ad5b51d8d4cafe3e077b698e9b9a19c
SubJunk <subjunk@gmail.com>
2021-06-22 19:55:06 +1200

    logging

--- src/main/java/net/pms/dlna/RealFile.java
+++ src/main/java/net/pms/dlna/RealFile.java
@@ -37,6 +37,8 @@ public class RealFile extends MapFile {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RealFile.class);
 
 	public RealFile(File file) {
+		System.out.println("1 ");
+		System.out.println("2 " + file);
 		addFileToConfFiles(file);
 		setLastModified(file.lastModified());
 	}
