46b3f917dffc86f302b23f82a0beb51309c96a02
Libor Vala <liborvala@seznam.cz>
2021-10-11 21:58:09 +0200

    Update src/test/java/net/pms/dlna/DLNAMediaInfoTest.java
    Co-authored-by: SubJunk <SubJunk@users.noreply.github.com>

--- src/test/java/net/pms/dlna/DLNAMediaInfoTest.java
+++ src/test/java/net/pms/dlna/DLNAMediaInfoTest.java
@@ -75,7 +75,6 @@ public class DLNAMediaInfoTest {
 		}
 
 		// Create a new PMS instance
-//		PMS.getNewInstance();
 
 		// Check if the MediaInfo library is properly installed and initialized
 		// especially on Linux which needs users to be involved.
