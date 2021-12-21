92ccf16bf8b1e6539ee786a29ac77db33471ceba
SurfaceS <nicoooo0@outlook.com>
2021-12-21 10:50:56 +0100

    remove InputStreamReader creation on lineIterator func

--- src/main/java/net/pms/io/OutputTextConsumer.java
+++ src/main/java/net/pms/io/OutputTextConsumer.java
@@ -21,7 +21,6 @@ package net.pms.io;
 import com.sun.jna.Platform;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -55,7 +54,7 @@ public class OutputTextConsumer extends OutputConsumer {
 
 	@Override
 	public void run() {
-		try (LineIterator it = IOUtils.lineIterator(new InputStreamReader(inputStream, charset))) {
+		try (LineIterator it = IOUtils.lineIterator(inputStream, charset)) {
 			while (it.hasNext()) {
 				String line = it.nextLine();
 
