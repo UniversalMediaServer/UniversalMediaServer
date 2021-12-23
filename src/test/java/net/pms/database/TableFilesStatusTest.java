ff0effa67b6773555aebe6f2dcc88b6c120d676f
Klaus Townsend <subjunk@gmail.com>
2021-12-23 14:27:32 +1300

    Test

--- src/test/java/net/pms/database/TableFilesStatusTest.java
+++ src/test/java/net/pms/database/TableFilesStatusTest.java
@@ -98,6 +98,7 @@ public class TableFilesStatusTest {
 	@Test
 	public void testIsFullyPlayed() throws Exception {
 		TableFilesStatus.setFullyPlayed("FileThatHasBeenPlayed", true);
+		TableFilesStatus.setFullyPlayed("FileThatHasBeenPlayed", true);
 		TableFilesStatus.setFullyPlayed("FileThatHasBeenMarkedNotPlayed", false);
 		Thread.sleep(4000);
 		assertThat(TableFilesStatus.isFullyPlayed("FileThatDoesntExist")).isNull();
