UniversalMediaServer/UniversalMediaServer

commit 2ef34120e3e0b98a08bad344f5ecb772a84bfa16

Always invoke stop at unexpected/premature stream closures

src/main/java/net/pms/remote/RemoteMediaHandler.java
@@ -138,6 +138,6 @@ public class RemoteMediaHandler implements HttpHandler {
 		if (sid != null) {
 			dlna.setMediaSubtitle(sid);
 		}
-		RemoteUtil.dump(in, os);
+		RemoteUtil.dump(in, os, render);
 	}
 }

src/main/java/net/pms/remote/RemoteUtil.java
@@ -81,6 +81,10 @@ public class RemoteUtil {
 
 
 	public static void dump(final InputStream in, final OutputStream os) {
+		dump(in, os, null);
+	}
+
+	public static void dump(final InputStream in, final OutputStream os, final WebRender renderer) {
 		Runnable r = new Runnable() {
 			@Override
 			public void run() {
@@ -105,6 +109,9 @@ public class RemoteUtil {
 				try {
 					os.close();
 				} catch (IOException e) {
+				}
+				if (renderer != null) {
+					renderer.stop();
 				}
 			}
 		};

src/main/java/net/pms/configuration/RendererConfiguration.java
@@ -2370,9 +2370,8 @@ public class RendererConfiguration extends UPNPHelper.Renderer {
 		if (dlna != null) {
 			player.getState().name = dlna.getDisplayName();
 			player.start();
-		} else if (player instanceof PlaybackTimer) {
-			player.getState().playback = BasicPlayer.STOPPED;
-			player.alert();
+		} else {
+			player.reset();
 		}
 	}
 

src/main/java/net/pms/network/Request.java
@@ -836,7 +836,12 @@ public class Request extends HTTPResource {
 				sendB = sendBytes(inputStream); //, ((lowRange > 0 && highRange > 0)?(highRange-lowRange):-1)
 			}
 
-			LOGGER.trace("Sending stream: " + sendB + " bytes of " + argument);
+			if (sendB > -1) {
+				LOGGER.trace("Sending stream: " + sendB + " bytes of " + argument);
+			} else {
+				// Premature end
+				startStopListenerDelegate.stop();
+			}
 		} else { // inputStream is null
 			if (lowRange > 0 && highRange > 0) {
 				output(output, "Content-Length: " + (highRange - lowRange + 1));
@@ -871,6 +876,7 @@ public class Request extends HTTPResource {
 			}
 		} catch (IOException e) {
 			LOGGER.trace("Sending stream with premature end: " + sendBytes + " bytes of " + argument + ". Reason: " + e.getMessage());
+			sendBytes = -1;
 		} finally {
 			fis.close();
 		}

src/main/java/net/pms/network/RequestHandlerV2.java
@@ -251,7 +251,7 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 
 		LOGGER.trace("HTTP: " + request.getArgument() + " / " + request.getLowRange() + "-" + request.getHighRange());
 
-		writeResponse(e, request, ia);
+		writeResponse(ctx, e, request, ia);
 	}
 
 	/**
@@ -266,7 +266,7 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 		return !PMS.getConfiguration().getIpFiltering().allowed(inetAddress);
 	}
 
-	private void writeResponse(MessageEvent e, RequestV2 request, InetAddress ia) {
+	private void writeResponse(ChannelHandlerContext ctx, MessageEvent e, RequestV2 request, InetAddress ia) {
 		// Decide whether to close the connection or not.
 		boolean close = HttpHeaders.Values.CLOSE.equalsIgnoreCase(nettyRequest.headers().get(HttpHeaders.Names.CONNECTION)) ||
 			nettyRequest.getProtocolVersion().equals(HttpVersion.HTTP_1_0) &&
@@ -291,6 +291,8 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 		}
 
 		StartStopListenerDelegate startStopListenerDelegate = new StartStopListenerDelegate(ia.getHostAddress());
+		// Attach it to the context so it can be invoked if connection is reset unexpectedly
+		ctx.setAttachment(startStopListenerDelegate);
 
 		try {
 			request.answer(response, e, close, startStopListenerDelegate);
@@ -313,8 +315,17 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 			sendError(ctx, HttpResponseStatus.BAD_REQUEST);
 			return;
 		}
-		if (cause != null && !cause.getClass().equals(ClosedChannelException.class) && !cause.getClass().equals(IOException.class)) {
-			LOGGER.debug("Caught exception", cause);
+		if (cause != null) {
+			if (cause.getClass().equals(IOException.class)) {
+				LOGGER.debug("Connection error: " + cause);
+				StartStopListenerDelegate startStopListenerDelegate = (StartStopListenerDelegate)ctx.getAttachment();
+				if (startStopListenerDelegate != null) {
+					LOGGER.debug("Premature end, stopping...");
+					startStopListenerDelegate.stop();
+				}
+			} else if (!cause.getClass().equals(ClosedChannelException.class)) {
+				LOGGER.debug("Caught exception: " + cause);
+			}
 		}
 		if (ch.isConnected()) {
 			sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR);

commit 0888373ec329e40258a2b4c85b4b52db1ac2e89d

Fix unsupported web videos (#2763)
* Fix unsupported web videos

RemotePlayHandler already handles more conditions than were being prevented here
This fixes #2258

* Lint

* !fixup

* Set files as fully played in case their move/recycle fails later

* Logging

* Note

* Stop artificial delay on web interface start page

* Don't call stop when there's nothing to stop

* Removed old logging

* Don't stop playback on dump

Fixes #2766
Bug was caused by 2ef34120e3e0b98a08bad344f5ecb772a84bfa16

* Fixed undefined Rated field on web interface

src/main/external-resources/web/jquery.ums.js
@@ -463,7 +463,7 @@ function populateMetadataDisplayFromGlobalVars() {
 		img.src = poster;
 		$('.posterContainer').html(img);
 	}
-	if (rated) {
+	if (rated && rated.id) {
 		$('.rated').html('<strong>' + ratedTranslation + ':</strong> <a href="/browse/' + rated.id + '" class="badge ' + badgeClass + '">' + rated.name + '</a>');
 	}
 	if (ratings && ratings[0]) {

src/main/external-resources/web/start.html
@@ -29,5 +29,5 @@
 	</a>
 	
 	</body>
-	<script>var delay = 3000; setTimeout(function(){ window.location = "/browse/0"; }, delay);</script>
+	<script>setTimeout(function(){ window.location = "/browse/0"; });</script>
 </html>

src/main/java/net/pms/configuration/WebRender.java
@@ -32,7 +32,6 @@ import net.pms.Messages;
 import net.pms.PMS;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAResource;
-import net.pms.dlna.virtual.VirtualVideoAction;
 import net.pms.encoders.FFMpegVideo;
 import net.pms.encoders.Player;
 import net.pms.external.StartStopListenerDelegate;
@@ -491,17 +490,6 @@ public class WebRender extends DeviceConfiguration implements RendererConfigurat
 		return false;
 	}
 
-	public static boolean supports(DLNAResource dlna) {
-		if (dlna instanceof VirtualVideoAction) {
-			return true;
-		}
-		DLNAMediaInfo media = dlna.getMedia();
-		return
-			media != null && RemoteUtil.directmime(media.getMimeType()) ||
-			supportedFormat(dlna.getFormat()) ||
-			dlna.getPlayer() instanceof FFMpegVideo;
-	}
-
 	/**
 	 * libvorbis transcodes very slowly, so we scale the video down to
 	 * speed it up.
@@ -574,9 +562,11 @@ public class WebRender extends DeviceConfiguration implements RendererConfigurat
 	}
 
 	public void start(DLNAResource dlna) {
-		if (getPlayingRes() != dlna) {
+		// Stop playing any previous media on the renderer
+		if (getPlayingRes() != null && getPlayingRes() != dlna) {
 			stop();
 		}
+
 		setPlayingRes(dlna);
 		if (startStop == null) {
 			startStop = new StartStopListenerDelegate(ip);

src/main/java/net/pms/database/MediaTableFilesStatus.java
@@ -283,7 +283,7 @@ public final class MediaTableFilesStatus extends MediaTable {
 		try (Connection connection = DATABASE.getConnection()) {
 			query = "SELECT * FROM " + TABLE_NAME + " WHERE FILENAME = " + sqlQuote(fullPathToFile) + " LIMIT 1";
 			if (trace) {
-				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before update", query);
+				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before setFullyPlayed", query);
 			}
 
 			TABLE_LOCK.writeLock().lock();
@@ -351,7 +351,7 @@ public final class MediaTableFilesStatus extends MediaTable {
 		try (Connection connection = DATABASE.getConnection()) {
 			query = "SELECT * FROM " + TABLE_NAME + " WHERE FILENAME = " + sqlQuote(fullPathToFile) + " LIMIT 1";
 			if (trace) {
-				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before update", query);
+				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before setLastPlayed", query);
 			}
 
 			TABLE_LOCK.writeLock().lock();
@@ -414,7 +414,7 @@ public final class MediaTableFilesStatus extends MediaTable {
 		try (Connection connection = DATABASE.getConnection()) {
 			String query = "SELECT ID, FILENAME FROM FILES WHERE FILENAME LIKE " + sqlQuote(pathWithWildcard);
 			if (trace) {
-				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before update", query);
+				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before setDirectoryFullyPlayed", query);
 			}
 
 			TABLE_LOCK.writeLock().lock();
@@ -536,7 +536,7 @@ public final class MediaTableFilesStatus extends MediaTable {
 		try (Connection connection = DATABASE.getConnection()) {
 			query = "SELECT * FROM " + TABLE_NAME + " WHERE FILENAME = " + sqlQuote(fullPathToFile) + " LIMIT 1";
 			if (trace) {
-				LOGGER.trace("Searching for file in " + TABLE_NAME + " with \"{}\" before update", query);
+				LOGGER.trace("Searching for file in {} with \"{}\" before setBookmark", TABLE_NAME, query);
 			}
 
 			TABLE_LOCK.writeLock().lock();
@@ -547,6 +547,9 @@ public final class MediaTableFilesStatus extends MediaTable {
 						result.updateTimestamp("MODIFIED", new Timestamp(System.currentTimeMillis()));
 						result.updateInt("BOOKMARK", bookmark);
 						result.updateRow();
+						if (trace) {
+							LOGGER.trace("Updating existing bookmark in {}: \"{}\" ", TABLE_NAME, bookmark);
+						}
 					} else {
 						result.moveToInsertRow();
 						result.updateString("FILENAME", fullPathToFile);
@@ -554,6 +557,9 @@ public final class MediaTableFilesStatus extends MediaTable {
 						result.updateBoolean("ISFULLYPLAYED", false);
 						result.updateInt("BOOKMARK", bookmark);
 						result.insertRow();
+						if (trace) {
+							LOGGER.trace("Inserting bookmark in {}: \"{}\" ", TABLE_NAME, bookmark);
+						}
 					}
 				} finally {
 					connection.commit();

src/main/java/net/pms/dlna/MediaMonitor.java
@@ -220,15 +220,11 @@ public class MediaMonitor extends VirtualFolder {
 		) {
 			DLNAResource fileParent = realFile.getParent();
 			if (fileParent != null && !isFullyPlayed(fullPathToFile, true)) {
-				// Only set fully played if the file will stay where it is
-				if (
-					fullyPlayedAction != FullyPlayedAction.MOVE_FOLDER &&
-					fullyPlayedAction != FullyPlayedAction.MOVE_FOLDER_AND_MARK &&
-					fullyPlayedAction != FullyPlayedAction.MOVE_TRASH
-				) {
-					setFullyPlayed(fullPathToFile, true, elapsed);
-				}
-
+				/*
+				 * Set to fully played even if it will be deleted or moved, because
+				 * the entry will be cleaned up later in those cases.
+				 */
+				setFullyPlayed(fullPathToFile, true, elapsed);
 				setDiscovered(false);
 				getChildren().clear();
 

src/main/java/net/pms/dlna/virtual/MediaLibraryFolder.java
@@ -577,14 +577,10 @@ public class MediaLibraryFolder extends VirtualFolder {
 								}
 								nameToDisplay = "Unknown";
 							}
-							LOGGER.trace("2 sqls2: " + Arrays.toString(sqls2));
 						}
 					}
 					boolean isExpectedTVSeries = expectedOutput == TVSERIES || expectedOutput == TVSERIES_NOSORT || expectedOutput == TVSERIES_WITH_FILTERS;
 					boolean isExpectedMovieFolder = expectedOutput == MOVIE_FOLDERS;
-//					LOGGER.info("33 " + virtualFolderName);
-//					LOGGER.info("34 " + Arrays.toString(sqls2));
-//					LOGGER.info("35 " + Arrays.toString(expectedOutputs2));
 					addChild(new MediaLibraryFolder(virtualFolderName, sqls2, expectedOutputs2, nameToDisplay, isExpectedTVSeries, isExpectedMovieFolder));
 				}
 			}

src/main/java/net/pms/remote/RemoteBrowseHandler.java
@@ -13,14 +13,12 @@ import net.pms.Messages;
 import net.pms.PMS;
 import net.pms.configuration.PmsConfiguration;
 import net.pms.configuration.RendererConfiguration;
-import net.pms.configuration.WebRender;
 import net.pms.dlna.CodeEnter;
 import net.pms.dlna.DLNAResource;
 import net.pms.dlna.Playlist;
 import net.pms.dlna.RootFolder;
 import net.pms.dlna.virtual.MediaLibraryFolder;
 import net.pms.dlna.virtual.VirtualVideoAction;
-import net.pms.formats.Format;
 import net.pms.util.PropertiesUtil;
 import net.pms.util.UMSUtils;
 import org.apache.commons.text.StringEscapeUtils;
@@ -90,7 +88,19 @@ public class RemoteBrowseHandler implements HttpHandler {
 
 		item.put("actions", bumpHTML.toString());
 
-		if (resource.isFolder() || WebRender.supports(resource) || resource.isResume() || resource.getType() == Format.IMAGE) {
+		if (
+			resource.isFolder() ||
+			resource.isResume() ||
+			resource instanceof VirtualVideoAction ||
+			(
+				resource.getFormat() != null &&
+				(
+					resource.getFormat().isVideo() ||
+					resource.getFormat().isAudio() ||
+					resource.getFormat().isImage()
+				)
+			)
+		) {
 			StringBuilder thumbHTML = new StringBuilder();
 			thumbHTML.append("<a href=\"").append(pageTypeUri).append(idForWeb)
 				.append("\" title=\"").append(name).append("\">")
@@ -103,19 +113,6 @@ public class RemoteBrowseHandler implements HttpHandler {
 				.append("\" title=\"").append(name).append("\">")
 				.append("<span class=\"caption\">").append(name).append("</span>")
 				.append("</a>");
-			item.put("caption", captionHTML.toString());
-		} else if (upnpControl && upnpAllowed) {
-			// Include it as a web-disabled item so it can be thrown via upnp
-			StringBuilder thumbHTML = new StringBuilder();
-			thumbHTML.append("<a class=\"webdisabled\" href=\"javascript:notify('warn','")
-				.append(RemoteUtil.getMsgString("Web.6", t)).append("')\"")
-				.append(" title=\"").append(name).append(' ').append(RemoteUtil.getMsgString("Web.7", t)).append("\">")
-				.append("<img class=\"thumb\" loading=\"lazy\" src=\"").append(thumb).append("\" alt=\"").append(name).append("\">")
-				.append("</a>");
-			item.put("thumb", thumbHTML.toString());
-
-			StringBuilder captionHTML = new StringBuilder();
-			captionHTML.append("<span class=\"webdisabled caption\">").append(name).append("</span>");
 			item.put("caption", captionHTML.toString());
 		}
 

src/main/java/net/pms/remote/RemoteMediaHandler.java
@@ -148,7 +148,7 @@ public class RemoteMediaHandler implements HttpHandler {
 			if (sid != null) {
 				resource.setMediaSubtitle(sid);
 			}
-			RemoteUtil.dump(in, os, render);
+			RemoteUtil.dump(in, os);
 		} catch (IOException e) {
 			throw e;
 		} catch (Exception e) {

src/main/java/net/pms/remote/RemoteUtil.java
@@ -82,12 +82,7 @@ public class RemoteUtil {
 		LOGGER.debug("dump of " + f.getName() + " done");
 	}
 
-
 	public static void dump(final InputStream in, final OutputStream os) {
-		dump(in, os, null);
-	}
-
-	public static void dump(final InputStream in, final OutputStream os, final WebRender renderer) {
 		Runnable r = () -> {
 			byte[] buffer = new byte[32 * 1024];
 			int bytes;
@@ -107,12 +102,10 @@ public class RemoteUtil {
 				} catch (IOException e) {
 				}
 			}
+
 			try {
 				os.close();
 			} catch (IOException e) {
-			}
-			if (renderer != null) {
-				renderer.stop();
 			}
 		};
 		new Thread(r).start();
@@ -175,6 +168,15 @@ public class RemoteUtil {
 		dump(in, os);
 	}
 
+	/**
+	 * Whether the MIME type is supported by all browsers.
+	 * Note: This is a flawed approach because while browsers
+	 * may support the container format, they may not support
+	 * the codecs within. For example, most browsers support
+	 * MP4 with H.264, but do not support it with H.265 (HEVC)
+	 *
+	 * @todo refactor to be more specific
+	 */
 	public static boolean directmime(String mime) {
 		if (
 			mime != null &&

src/main/resources/i18n/messages.properties
@@ -693,8 +693,6 @@ Web.2=No UPnP-controllable renderers suitable for receiving pushed media are ava
 Web.3=No other renderers available
 Web.4=Remove from playlist
 Web.5=Add to playlist
-Web.6=This item isn't playable via browser but can be sent to other renderers.
-Web.7=(NOT PLAYABLE IN BROWSER)
 Web.8=Enter search string:
 Web.9=Enter code:
 Web.10=Back