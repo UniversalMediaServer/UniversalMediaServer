UniversalMediaServer/UniversalMediaServer

commit 2ef34120e3e0b98a08bad344f5ecb772a84bfa16

Always invoke stop at unexpected/premature stream closures

src/main/java/net/pms/remote/RemoteMediaHandler.java
@@ -138,6 +138,6 @@ public class RemoteMediaHandler implements HttpHandler {
 		if (sid != null) {
 			dlna.setMediaSubtitle(sid);
 		}
-		RemoteUtil.dump(in, os);
+		RemoteUtil.dump(in, os, render);
 	}
 }

src/main/java/net/pms/remote/RemoteUtil.java
@@ -81,6 +81,10 @@ public class RemoteUtil {
 
 
 	public static void dump(final InputStream in, final OutputStream os) {
+		dump(in, os, null);
+	}
+
+	public static void dump(final InputStream in, final OutputStream os, final WebRender renderer) {
 		Runnable r = new Runnable() {
 			@Override
 			public void run() {
@@ -105,6 +109,9 @@ public class RemoteUtil {
 				try {
 					os.close();
 				} catch (IOException e) {
+				}
+				if (renderer != null) {
+					renderer.stop();
 				}
 			}
 		};

src/main/java/net/pms/configuration/RendererConfiguration.java
@@ -2370,9 +2370,8 @@ public class RendererConfiguration extends UPNPHelper.Renderer {
 		if (dlna != null) {
 			player.getState().name = dlna.getDisplayName();
 			player.start();
-		} else if (player instanceof PlaybackTimer) {
-			player.getState().playback = BasicPlayer.STOPPED;
-			player.alert();
+		} else {
+			player.reset();
 		}
 	}
 

src/main/java/net/pms/network/Request.java
@@ -836,7 +836,12 @@ public class Request extends HTTPResource {
 				sendB = sendBytes(inputStream); //, ((lowRange > 0 && highRange > 0)?(highRange-lowRange):-1)
 			}
 
-			LOGGER.trace("Sending stream: " + sendB + " bytes of " + argument);
+			if (sendB > -1) {
+				LOGGER.trace("Sending stream: " + sendB + " bytes of " + argument);
+			} else {
+				// Premature end
+				startStopListenerDelegate.stop();
+			}
 		} else { // inputStream is null
 			if (lowRange > 0 && highRange > 0) {
 				output(output, "Content-Length: " + (highRange - lowRange + 1));
@@ -871,6 +876,7 @@ public class Request extends HTTPResource {
 			}
 		} catch (IOException e) {
 			LOGGER.trace("Sending stream with premature end: " + sendBytes + " bytes of " + argument + ". Reason: " + e.getMessage());
+			sendBytes = -1;
 		} finally {
 			fis.close();
 		}

src/main/java/net/pms/network/RequestHandlerV2.java
@@ -251,7 +251,7 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 
 		LOGGER.trace("HTTP: " + request.getArgument() + " / " + request.getLowRange() + "-" + request.getHighRange());
 
-		writeResponse(e, request, ia);
+		writeResponse(ctx, e, request, ia);
 	}
 
 	/**
@@ -266,7 +266,7 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 		return !PMS.getConfiguration().getIpFiltering().allowed(inetAddress);
 	}
 
-	private void writeResponse(MessageEvent e, RequestV2 request, InetAddress ia) {
+	private void writeResponse(ChannelHandlerContext ctx, MessageEvent e, RequestV2 request, InetAddress ia) {
 		// Decide whether to close the connection or not.
 		boolean close = HttpHeaders.Values.CLOSE.equalsIgnoreCase(nettyRequest.headers().get(HttpHeaders.Names.CONNECTION)) ||
 			nettyRequest.getProtocolVersion().equals(HttpVersion.HTTP_1_0) &&
@@ -291,6 +291,8 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 		}
 
 		StartStopListenerDelegate startStopListenerDelegate = new StartStopListenerDelegate(ia.getHostAddress());
+		// Attach it to the context so it can be invoked if connection is reset unexpectedly
+		ctx.setAttachment(startStopListenerDelegate);
 
 		try {
 			request.answer(response, e, close, startStopListenerDelegate);
@@ -313,8 +315,17 @@ public class RequestHandlerV2 extends SimpleChannelUpstreamHandler {
 			sendError(ctx, HttpResponseStatus.BAD_REQUEST);
 			return;
 		}
-		if (cause != null && !cause.getClass().equals(ClosedChannelException.class) && !cause.getClass().equals(IOException.class)) {
-			LOGGER.debug("Caught exception", cause);
+		if (cause != null) {
+			if (cause.getClass().equals(IOException.class)) {
+				LOGGER.debug("Connection error: " + cause);
+				StartStopListenerDelegate startStopListenerDelegate = (StartStopListenerDelegate)ctx.getAttachment();
+				if (startStopListenerDelegate != null) {
+					LOGGER.debug("Premature end, stopping...");
+					startStopListenerDelegate.stop();
+				}
+			} else if (!cause.getClass().equals(ClosedChannelException.class)) {
+				LOGGER.debug("Caught exception: " + cause);
+			}
 		}
 		if (ch.isConnected()) {
 			sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR);

commit b61636983eacfb5e70e14c6389c23edfa7b9d2b2

Don't stop playback on dump
Fixes #2766
Bug was caused by 2ef34120e3e0b98a08bad344f5ecb772a84bfa16

src/main/java/net/pms/remote/RemoteMediaHandler.java
@@ -148,7 +148,7 @@ public class RemoteMediaHandler implements HttpHandler {
 			if (sid != null) {
 				resource.setMediaSubtitle(sid);
 			}
-			RemoteUtil.dump(in, os, render);
+			RemoteUtil.dump(in, os);
 		} catch (IOException e) {
 			throw e;
 		} catch (Exception e) {

src/main/java/net/pms/remote/RemoteUtil.java
@@ -82,12 +82,7 @@ public class RemoteUtil {
 		LOGGER.debug("dump of " + f.getName() + " done");
 	}
 
-
 	public static void dump(final InputStream in, final OutputStream os) {
-		dump(in, os, null);
-	}
-
-	public static void dump(final InputStream in, final OutputStream os, final WebRender renderer) {
 		Runnable r = () -> {
 			byte[] buffer = new byte[32 * 1024];
 			int bytes;
@@ -107,12 +102,10 @@ public class RemoteUtil {
 				} catch (IOException e) {
 				}
 			}
+
 			try {
 				os.close();
 			} catch (IOException e) {
-			}
-			if (renderer != null) {
-				renderer.stop();
 			}
 		};
 		new Thread(r).start();