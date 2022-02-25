b0da7d279d9d8c7f86876748d825a1de053520b5
SurfaceS <nicoooo0@outlook.com>
2022-02-25 10:37:42 +0100

    set script to send status only if it change instead of flooding remove AccessController as it's obsolete added ability to debug from sources

--- src/main/java/net/pms/network/webinterfaceserver/WebInterfaceServer.java
+++ src/main/java/net/pms/network/webinterfaceserver/WebInterfaceServer.java
@@ -41,11 +41,12 @@ public abstract class WebInterfaceServer implements WebInterfaceServerInterface 
 	public WebInterfaceServer() throws IOException {
 		roots = new HashMap<>();
 		// Add "classpaths" for resolving web resources
-		resources = AccessController.doPrivileged((PrivilegedAction<WebInterfaceServerUtil.ResourceManager>) () -> new WebInterfaceServerUtil.ResourceManager(
+		resources = new WebInterfaceServerUtil.ResourceManager(
 				"file:" + CONFIGURATION.getProfileDirectory() + "/web/",
 				"jar:file:" + CONFIGURATION.getProfileDirectory() + "/web.zip!/",
-				"file:" + CONFIGURATION.getWebPath() + "/"
-		));
+				"file:" + CONFIGURATION.getWebPath() + "/",
+				"file:" + "src/main/external-resources/web/"
+		);
 	}
 
 	public String getTag(String user) {
