ba9a5b72da444fb77e1b6793b8904654777c5d75
sf666 <satfreak666@gmx.de>
2022-03-20 08:22:47 +0100

    Revert "Bugfix: audio files get identified as video files -> Wrong 'protocolInfo' section in DIDL string."
    This reverts commit a6b84d61e2c032d185193e0e150d6122a4dccc7f.

--- src/main/java/net/pms/dlna/LibMediaInfoParser.java
+++ src/main/java/net/pms/dlna/LibMediaInfoParser.java
@@ -549,6 +549,10 @@ public class LibMediaInfoParser {
 				media.setContainer(DLNAMediaLang.UND);
 			}
 
+			if (media.getCodecV() == null) {
+				media.setCodecV(DLNAMediaLang.UND);
+			}
+
 			media.setMediaparsed(true);
 		}
 	}
