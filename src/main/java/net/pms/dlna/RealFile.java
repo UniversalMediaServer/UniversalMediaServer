36e3258737c65f555ec059af606e9ba46804fca1
SubJunk <subjunk@gmail.com>
2022-03-28 20:43:50 +1300

    Lint

--- src/main/java/net/pms/dlna/RealFile.java
+++ src/main/java/net/pms/dlna/RealFile.java
@@ -25,7 +25,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InvalidClassException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
