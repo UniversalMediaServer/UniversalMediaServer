ce1dfa5cee567e54a5fda0575a5a18aebc0192b1
SubJunk <subjunk@gmail.com>
2022-04-26 14:48:49 +1200

    Catch more

--- src/main/java/net/pms/util/APIUtils.java
+++ src/main/java/net/pms/util/APIUtils.java
@@ -416,20 +416,20 @@ public class APIUtils {
 					}
 					MediaTableVideoMetadataReleased.set(connection, file.getAbsolutePath(), (String) metadataFromAPI.get("released"), -1);
 				}
-			} catch (SQLException ex) {
+			} catch (Exception ex) {
 				LOGGER.trace("Error in API parsing:", ex);
 			} finally {
 				try {
 					if (connection != null) {
 						connection.commit();
 					}
-				} catch (SQLException e) {
+
+					MediaDatabase.close(connection);
+					frame.setSecondaryStatusLine(null);
+				} catch (Exception e) {
 					LOGGER.error("Error in commit in APIUtils.backgroundLookupAndAdd: {}", e.getMessage());
 					LOGGER.trace("", e);
 				}
-
-				MediaDatabase.close(connection);
-				frame.setSecondaryStatusLine(null);
 			}
 		};
 		BACKGROUND_EXECUTOR.execute(r);
