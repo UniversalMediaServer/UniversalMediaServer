b652c4f2e5e59dca0347beed2f9ff17e353252e5
Klaus Townsend <subjunk@gmail.com>
2022-06-05 12:15:44 +1200

    Formatting

--- src/main/java/net/pms/network/mediaserver/javahttpserver/ConfigurationApiHandler.java
+++ src/main/java/net/pms/network/mediaserver/javahttpserver/ConfigurationApiHandler.java
@@ -66,63 +66,83 @@ public class ConfigurationApiHandler implements HttpHandler {
 				exchange.close();
 				return;
 			}
-			String call = "";
-			int pos = exchange.getRequestURI().getPath().indexOf("configuration-api/");
-			if (pos != -1) {
-				call = exchange.getRequestURI().getPath().substring(pos + "configuration-api/".length());
-			}
-			try {
-				// this is called by the web interface settings React app on page load
-				if (call.equals("settings")) {
-					if (exchange.getRequestMethod().equals("GET")) {
-						// this would be implemented higher in the stack, and on all the protected endpoints
-						final List<String> authHeader = exchange.getRequestHeaders().get("Authorization");
-						// this is a dumb check, representing that we would verify the JWT has been signed by this UMS server
-						// for now it just matches the token sent by the dummy login endpoint
-						if (authHeader != null & authHeader.get(0).equals("Bearer XXYYXX")) {
-							String configurationAsJson = pmsConfiguration.getConfigurationAsJson();
-							WebInterfaceServerUtil.respond(exchange, configurationAsJson, 200, "application/json");
-						} else {
-							WebInterfaceServerUtil.respond(exchange, "Unauthorized", 401, "application/json");
-						}
 
-					} else if (exchange.getRequestMethod().equals("POST")) {
-						// Here we possibly received some updates to config values
-						String configToSave = IOUtils.toString(exchange.getRequestBody(), StandardCharsets.UTF_8);
-						HashMap<String, ?> data = gson.fromJson(configToSave, HashMap.class);
-						Iterator iterator = data.entrySet().iterator();
-						while (iterator.hasNext()) {
-							Map.Entry configurationSetting = (Map.Entry) iterator.next();
-							String key = (String) configurationSetting.getKey();
-							if (!Arrays.asList(validKeys).contains(key)) {
-								LOGGER.trace("The key {} is not allowed", key);
-								continue;
-							}
-
-							if (configurationSetting.getValue() instanceof String) {
-								LOGGER.trace("Saving key {} and String value {}", key, configurationSetting.getValue());
-								configuration.setProperty(key, (String) configurationSetting.getValue());
-							} else if (configurationSetting.getValue() instanceof Boolean) {
-								LOGGER.trace("Saving key {} and Boolean value {}", key, configurationSetting.getValue());
-								configuration.setProperty(key, (Boolean) configurationSetting.getValue());
-							} else if (configurationSetting.getValue() instanceof Integer) {
-								LOGGER.trace("Saving key {} and Integer value {}", key, configurationSetting.getValue());
-								configuration.setProperty(key, (Integer) configurationSetting.getValue());
-							} else {
-								LOGGER.trace("Invalid value passed from client: {}, {}", key, configurationSetting.getValue());
-							}
-						}
-						WebInterfaceServerUtil.respond(exchange, null, 200, "application/json");
+			/**
+			 * Helpers for HTTP methods and paths.
+			 */
+			var api = new Object(){
+				private String getEndpoint() {
+					String endpoint = "";
+					int pos = exchange.getRequestURI().getPath().indexOf("configuration-api");
+					if (pos != -1) {
+						endpoint = exchange.getRequestURI().getPath().substring(pos + "configuration-api".length());
 					}
-				} else if (call.equals("i18n")) {
-					if (exchange.getRequestMethod().equals("GET")) {
-						String i18nAsJson = Messages.getStringsAsJson();
-						WebInterfaceServerUtil.respond(exchange, i18nAsJson, 200, "application/json");
+					return endpoint;
+				}
+				/**
+				 * @return whether this was a GET request for the specified path.
+				 */
+				public Boolean get(String path) {
+					return exchange.getRequestMethod().equals("GET") && getEndpoint().equals(path);
+				}
+				/**
+				 * @return whether this was a POST request for the specified path.
+				 */
+				public Boolean post(String path) {
+					return exchange.getRequestMethod().equals("POST") && getEndpoint().equals(path);
+				}
+			};
+
+			/**
+			 * API endpoints
+			 */
+			// this is called by the web interface settings React app on page load
+			if (api.get("/settings")) {
+				// this would be implemented higher in the stack, and on all the protected endpoints
+				final List<String> authHeader = exchange.getRequestHeaders().get("Authorization");
+				// this is a dumb check, representing that we would verify the JWT has been signed by this UMS server
+				// for now it just matches the token sent by the dummy login endpoint
+				if (authHeader != null & authHeader.get(0).equals("Bearer XXYYXX")) {
+					String configurationAsJson = pmsConfiguration.getConfigurationAsJson();
+					WebInterfaceServerUtil.respond(exchange, configurationAsJson, 200, "application/json");
+				} else {
+					WebInterfaceServerUtil.respond(exchange, "Unauthorized", 401, "application/json");
+				}
+			}
+			if (api.post("/settings")) {
+				// Here we possibly received some updates to config values
+				String configToSave = IOUtils.toString(exchange.getRequestBody(), StandardCharsets.UTF_8);
+				HashMap<String, ?> data = gson.fromJson(configToSave, HashMap.class);
+				Iterator iterator = data.entrySet().iterator();
+				while (iterator.hasNext()) {
+					Map.Entry configurationSetting = (Map.Entry) iterator.next();
+					String key = (String) configurationSetting.getKey();
+					if (!Arrays.asList(validKeys).contains(key)) {
+						LOGGER.trace("The key {} is not allowed", key);
+						continue;
+					}
+
+					if (configurationSetting.getValue() instanceof String) {
+						LOGGER.trace("Saving key {} and String value {}", key, configurationSetting.getValue());
+						configuration.setProperty(key, (String) configurationSetting.getValue());
+					} else if (configurationSetting.getValue() instanceof Boolean) {
+						LOGGER.trace("Saving key {} and Boolean value {}", key, configurationSetting.getValue());
+						configuration.setProperty(key, (Boolean) configurationSetting.getValue());
+					} else if (configurationSetting.getValue() instanceof Integer) {
+						LOGGER.trace("Saving key {} and Integer value {}", key, configurationSetting.getValue());
+						configuration.setProperty(key, (Integer) configurationSetting.getValue());
+					} else {
+						LOGGER.trace("Invalid value passed from client: {}, {}", key, configurationSetting.getValue());
 					}
 				}
-			} catch (RuntimeException e) {
-				exchange.sendResponseHeaders(500, 0); //Internal Server Error
+				WebInterfaceServerUtil.respond(exchange, null, 200, "application/json");
 			}
+			if (api.get("/i18n")) {
+				String i18nAsJson = Messages.getStringsAsJson();
+				WebInterfaceServerUtil.respond(exchange, i18nAsJson, 200, "application/json");
+			}
+		} catch (RuntimeException e) {
+			exchange.sendResponseHeaders(500, 0); //Internal Server Error
 		} catch (IOException e) {
 			throw e;
 		} catch (Exception e) {
