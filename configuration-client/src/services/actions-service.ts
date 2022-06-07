d3e2b9c52e81063f2f28a101c826cd39397e9fc2
Kyle Martin <kyle.martin@hotmail.co.nz>
2022-06-08 10:05:40 +1200

    Update

--- configuration-client/src/services/actions-service.ts
+++ configuration-client/src/services/actions-service.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const PERMITTED_ACTIONS = ['Server.Restart'];
 
-export const sendAction = async(action: string) => {
-  if (PERMITTED_ACTIONS.includes(action)) {
+export const sendAction = async(operation: string) => {
+  if (PERMITTED_ACTIONS.includes(operation)) {
     return axios
       .post('/v1/api/actions', {
-        operation: 'Server.Restart',
+        operation,
       })
       .then((response) => {
         return response.data;
