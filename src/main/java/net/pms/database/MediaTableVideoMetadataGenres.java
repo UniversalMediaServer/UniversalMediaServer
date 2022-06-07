3f6ef71b4c4c0c5ecb5264d1d42e02ad93fb0aeb
SubJunk <subjunk@gmail.com>
2022-06-07 16:42:42 +1200

    Fixed database fields truncating to less than the limit

--- src/main/java/net/pms/database/MediaTableVideoMetadataGenres.java
+++ src/main/java/net/pms/database/MediaTableVideoMetadataGenres.java
@@ -168,8 +168,8 @@ public final class MediaTableVideoMetadataGenres extends MediaTable {
 					)
 				) {
 					ps.setLong(1, tvSeriesID);
-					ps.setString(2, left(fullPathToFile, 255));
-					ps.setString(3, left(genre, 255));
+					ps.setString(2, left(fullPathToFile, 1024));
+					ps.setString(3, left(genre, 1024));
 					try (ResultSet rs = ps.executeQuery()) {
 						if (rs.next()) {
 							LOGGER.trace("Record already exists {} {} {}", tvSeriesID, fullPathToFile, genre);
@@ -186,8 +186,8 @@ public final class MediaTableVideoMetadataGenres extends MediaTable {
 							) {
 								insertStatement.clearParameters();
 								insertStatement.setLong(1, tvSeriesID);
-								insertStatement.setString(2, left(fullPathToFile, 255));
-								insertStatement.setString(3, left(genre, 255));
+								insertStatement.setString(2, left(fullPathToFile, 1024));
+								insertStatement.setString(3, left(genre, 1024));
 
 								insertStatement.executeUpdate();
 								try (ResultSet rs2 = insertStatement.getGeneratedKeys()) {
