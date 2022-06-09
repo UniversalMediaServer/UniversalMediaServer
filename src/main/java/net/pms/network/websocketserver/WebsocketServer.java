1efeeb60bf09170257e4d65bb2871da37b0ea5fc
Kyle <kyle.martin@hotmail.co.nz>
2022-06-09 19:57:07 +1200

    format

--- src/main/java/net/pms/network/websocketserver/WebsocketServer.java
+++ src/main/java/net/pms/network/websocketserver/WebsocketServer.java
@@ -29,93 +29,92 @@ import javax.net.ssl.TrustManagerFactory;
  */
 public class WebsocketServer extends WebSocketServer {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketServer.class);
-  private static final PmsConfiguration CONFIGURATION = PMS.getConfiguration();
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketServer.class);
+	private static final PmsConfiguration CONFIGURATION = PMS.getConfiguration();
 
+	public WebsocketServer(int port) throws UnknownHostException {
+		super(new InetSocketAddress(port));
+	}
 
-  public WebsocketServer(int port) throws UnknownHostException {
-    super(new InetSocketAddress(port));
-  }
+	public WebsocketServer(InetSocketAddress address) {
+		super(address);
+	}
 
-  public WebsocketServer(InetSocketAddress address) {
-    super(address);
-  }
+	public WebsocketServer(int port, Draft_6455 draft) {
+		super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
+	}
 
-  public WebsocketServer(int port, Draft_6455 draft) {
-    super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
-  }
+	@Override
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		conn.send("UMS Server online");
+	}
 
-  @Override
-  public void onOpen(WebSocket conn, ClientHandshake handshake) {
-    conn.send("UMS Server online");
-  }
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+	}
 
-  @Override
-  public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-  }
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+		broadcast(message);
+		System.out.println(conn + ": " + message);
+	}
 
-  @Override
-  public void onMessage(WebSocket conn, String message) {
-    broadcast(message);
-    System.out.println(conn + ": " + message);
-  }
+	@Override
+	public void onMessage(WebSocket conn, ByteBuffer message) {
 
-  @Override
-  public void onMessage(WebSocket conn, ByteBuffer message) {
+	}
 
-  }
-
-  public static WebsocketServer createServer(int port) throws Exception {
-    try {
-        WebsocketServer s = new WebsocketServer(port);
-        if (CONFIGURATION.getWebSocketHttps()) {
-          final KeyStore keyStore;
-          final KeyManagerFactory keyManagerFactory;
-          final TrustManagerFactory trustManagerFactory;
-          final SSLContext sslContext;
-          // Initialize the keystore
-          char[] password = "umsums".toCharArray();
-          keyStore = KeyStore.getInstance("JKS");
-          try (FileInputStream fis = new FileInputStream(FileUtil.appendPathSeparator(CONFIGURATION.getProfileDirectory()) + "UMS_WEBSOCKETS.jks")) {
-            keyStore.load(fis, password);
-          }
-          // Setup the key manager factory
-          keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-          keyManagerFactory.init(keyStore, password);
-
-          // Setup the trust manager factory
-          trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
-          trustManagerFactory.init(keyStore);
-
-          sslContext = SSLContext.getInstance("TLS");
-          sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
-          s.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(sslContext));
-        }
-        s.start();
-        return s;
-    } catch (IOException e) {
-        LOGGER.error("Failed to start websocket server: {}", e.getMessage());
-        if (e.getMessage().contains("UMS_WEBSOCKETS.jks")) {
-					LOGGER.info(
-							"To enable HTTPS please generate a self-signed keystore file " +
-							"called \"UMS_WEBSOCKETS.jks\" with password \"umsums\" using the java " +
-							"'keytool' commandline utility, and place it in the profile folder"
-					);
+	public static WebsocketServer createServer(int port) throws Exception {
+		try {
+			WebsocketServer s = new WebsocketServer(port);
+			if (CONFIGURATION.getWebSocketHttps()) {
+				final KeyStore keyStore;
+				final KeyManagerFactory keyManagerFactory;
+				final TrustManagerFactory trustManagerFactory;
+				final SSLContext sslContext;
+				// Initialize the keystore
+				char[] password = "umsums".toCharArray();
+				keyStore = KeyStore.getInstance("JKS");
+				try (FileInputStream fis = new FileInputStream(
+						FileUtil.appendPathSeparator(CONFIGURATION.getProfileDirectory()) + "UMS_WEBSOCKETS.jks")) {
+					keyStore.load(fis, password);
 				}
-        return null;
-    }
-  }
+				// Setup the key manager factory
+				keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+				keyManagerFactory.init(keyStore, password);
 
-  @Override
-  public void onError(WebSocket conn, Exception ex) {
-    LOGGER.error("Websocket error: {}", ex.getMessage());
-  }
+				// Setup the trust manager factory
+				trustManagerFactory = TrustManagerFactory.getInstance("SunX509");
+				trustManagerFactory.init(keyStore);
 
-  @Override
-  public void onStart() {
-    LOGGER.info("Websocket server started");
-    setConnectionLostTimeout(0);
-    setConnectionLostTimeout(100);
-  }
+				sslContext = SSLContext.getInstance("TLS");
+				sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+				s.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(sslContext));
+			}
+			s.start();
+			return s;
+		} catch (IOException e) {
+			LOGGER.error("Failed to start websocket server: {}", e.getMessage());
+			if (e.getMessage().contains("UMS_WEBSOCKETS.jks")) {
+				LOGGER.info(
+						"To enable HTTPS please generate a self-signed keystore file " +
+								"called \"UMS_WEBSOCKETS.jks\" with password \"umsums\" using the java " +
+								"'keytool' commandline utility, and place it in the profile folder");
+			}
+			return null;
+		}
+	}
+
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
+		LOGGER.error("Websocket error: {}", ex.getMessage());
+	}
+
+	@Override
+	public void onStart() {
+		LOGGER.info("Websocket server started");
+		setConnectionLostTimeout(0);
+		setConnectionLostTimeout(100);
+	}
 
 }
