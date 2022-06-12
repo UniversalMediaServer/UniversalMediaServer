1d2d9552cefaf0474a2416258f7f7943a7f80f6e
Kyle Martin <kyle.martin@hotmail.co.nz>
2022-06-12 16:03:15 +1200

    password ux (#3047)
    * password ux
    
    * linting

--- configuration-client/src/components/ChangePassword/ChangePassword.tsx
+++ configuration-client/src/components/ChangePassword/ChangePassword.tsx
@@ -1,75 +1,97 @@
 import { showNotification } from '@mantine/notifications';
 import { changePassword } from '../../services/auth.service';
-import { TextInput, Button, Group, Box } from '@mantine/core';
+import { TextInput, Button, Group, Box, Text, Space } from '@mantine/core';
+import { Lock, LockOpen } from 'tabler-icons-react';
 import { useForm } from '@mantine/form';
 
 const ChangePassword = () => {
-    const form = useForm({
-        initialValues: {
-            password: '',
-            confirmpassword: '',
-        },
-      });
+  const form = useForm({
+      initialValues: {
+          password: '',
+          confirmpassword: '',
+          newpassword: '',
+      },
+    });
 
-      const handleChangePassword = (values: typeof form.values) => {
-        const { password, confirmpassword } = values;
-        if (password !== confirmpassword) {
-            showNotification({
-                id: 'pwd-error',
-                color: 'red',
-                title: 'Error',
-                message: 'Passwords do not match, try again',
-                autoClose: 3000,
-              });
-              return;
+  const handleChangePassword = (values: typeof form.values) => {
+    const { password, confirmpassword, newpassword } = values;
+    if (newpassword !== confirmpassword) {
+        showNotification({
+            id: 'pwd-error',
+            color: 'red',
+            title: 'Error',
+            message: 'New passwords do not match, try again',
+            autoClose: 3000,
+          });
+          return;
+    }
+    changePassword(password, confirmpassword).then(
+      (data) => {
+        if (data.error) {
+          showNotification({
+            id: 'data-loading',
+            color: 'red',
+            title: 'Error',
+            message: data.error,
+            autoClose: 3000,
+          });
+          return;
         }
-        changePassword(password).then(
-          () => {
-            showNotification({
-                id: 'data-loading',
-                color: 'teal',
-                title: 'Success',
-                message: 'Password updated successfully',
-                autoClose: 3000,
-              });
-              setTimeout(()=> {
+        showNotification({
+            id: 'data-loading',
+            color: 'teal',
+            title: 'Success',
+            message: 'Password updated successfully',
+            autoClose: 3000,
+          });
+          setTimeout(()=> {
+            window.location.href = '/';
+          }, 3000);
+      },
+      () => {
+        showNotification({
+            id: 'pwd-error',
+            color: 'red',
+            title: 'Error',
+            message: 'Error updating password',
+            autoClose: 3000,
+          });
+      }
+    );
+  };
 
-              }, 3000);
-              window.location.href = '/';
-          },
-          () => {
-            showNotification({
-                id: 'pwd-error',
-                color: 'red',
-                title: 'Error',
-                message: 'Error updating password',
-                autoClose: 3000,
-              });
-          }
-        );
-      };
-    return (
-        <Box sx={{ maxWidth: 300 }} mx='auto'>
-          <form onSubmit={form.onSubmit(handleChangePassword)}>
-            <h1>Change password</h1>
-            <TextInput
-              required
-              label='Password'
-              type='password'
-              {...form.getInputProps('password')}
-            />
-            <TextInput
-              required
-              label='Confirm Password'
-              type='password'
-              {...form.getInputProps('confirmpassword')}
-            />
-            <Group position='right' mt='md'>
-              <Button type='submit'>Submit</Button>
-            </Group>
-          </form>
-        </Box>
-      );
+  return (
+    <Box sx={{ maxWidth: 300 }} mx='auto'>
+      <form onSubmit={form.onSubmit(handleChangePassword)}>
+        <Text size="xl">Change password</Text>
+        <Space h="md" />
+        <TextInput
+          required
+          label='Current password'
+          type='password'
+          icon={<LockOpen size={14} />}
+          {...form.getInputProps('password')}
+        />
+        <TextInput
+          required
+          label='New password'
+          type='password'
+          icon={<Lock size={14} />}
+          {...form.getInputProps('newpassword')}
+        />
+        <TextInput
+          required
+          label='Confirm new password'
+          type='password'
+          icon={<Lock size={14} />}
+          {...form.getInputProps('confirmpassword')}
+          />
+        <Group position='right' mt='md'>
+          <Button type='submit'>Submit</Button>
+        </Group>
+      </form>
+    </Box>
+    );
 };
 
 export default ChangePassword;
