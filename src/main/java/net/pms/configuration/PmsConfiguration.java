1c3dc6c4f6f221f8e75d6b349e839230c704ee53
SurfaceS <nicoooo0@outlook.com>
2022-07-07 12:48:38 +0200

    Fix overwritten values

--- src/main/java/net/pms/configuration/PmsConfiguration.java
+++ src/main/java/net/pms/configuration/PmsConfiguration.java
@@ -4220,7 +4220,7 @@ public class PmsConfiguration extends RendererConfiguration {
 	 * @return The folder name.
 	 */
 	public String getTranscodeFolderName() {
-		return getString(KEY_TRANSCODE_FOLDER_NAME, Messages.getString("Transcode"));
+		return getString(KEY_TRANSCODE_FOLDER_NAME, Messages.getString("Transcode_FolderName"));
 	}
 
 	/**
