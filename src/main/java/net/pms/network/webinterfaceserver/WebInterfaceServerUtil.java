1318ff93f0c4b62d1860db52ff5721a6beb0a384
Klaus Townsend <subjunk@gmail.com>
2022-08-08 18:32:33 +1200

    Fix import

--- src/main/java/net/pms/network/webinterfaceserver/WebInterfaceServerUtil.java
+++ src/main/java/net/pms/network/webinterfaceserver/WebInterfaceServerUtil.java
@@ -18,6 +18,7 @@
 package net.pms.network.webinterfaceserver;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
