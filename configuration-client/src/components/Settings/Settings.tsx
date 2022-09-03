eac6295e5f116b2a9143ff87dbb4971020e51ee1
Klaus Townsend <subjunk@gmail.com>
2022-09-03 14:25:54 +1200

    Formatting

--- configuration-client/src/components/Settings/Settings.tsx
+++ configuration-client/src/components/Settings/Settings.tsx
@@ -192,168 +192,168 @@ export default function Settings() {
 
   const getGeneralSettingsTab = () => {
     return (
-        <>
-          <Accordion>
-            <Accordion.Item value='Application'>
-              <Accordion.Control>{i18n.get['Application']}</Accordion.Control>
-              <Accordion.Panel>
-                <Select
+      <>
+        <Accordion>
+          <Accordion.Item value='Application'>
+            <Accordion.Control>{i18n.get['Application']}</Accordion.Control>
+            <Accordion.Panel>
+              <Select
+                disabled={!canModify}
+                label={i18n.get['Language']}
+                data={getLanguagesSelectData()}
+                {...form.getInputProps('language')}
+              />
+              <Stack align="flex-start" mt="sm">
+                <Checkbox
                   disabled={!canModify}
-                  label={i18n.get['Language']}
-                  data={getLanguagesSelectData()}
-                  {...form.getInputProps('language')}
+                  label={i18n.get['StartMinimizedSystemTray']}
+                  {...form.getInputProps('minimized', { type: 'checkbox' })}
                 />
-                <Stack align="flex-start" mt="sm">
+                <Checkbox
+                  disabled={!canModify}
+                  label={i18n.get['EnableSplashScreen']}
+                  {...form.getInputProps('show_splash_screen', { type: 'checkbox' })}
+                />
+                <Checkbox
+                  disabled={!canModify}
+                  label={i18n.get['CheckAutomaticallyForUpdates']}
+                  {...form.getInputProps('auto_update', { type: 'checkbox' })}
+                />
+              </Stack>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value='Services'>
+            <Accordion.Control>{i18n.get['Services']}</Accordion.Control>
+            <Accordion.Panel>
+              <Group>
+                <TextInput
+                  disabled={!canModify}
+                  label={i18n.get['ServerName']}
+                  placeholder={defaultConfiguration.server_name}
+                  name="server_name"
+                  sx={{ flex: 1 }}
+                  {...form.getInputProps('server_name')}
+                />
+                <Tooltip label={allowHtml(i18n.get['WhenEnabledUmsProfileName'])} {...defaultTooltipSettings}>
                   <Checkbox
                     disabled={!canModify}
-                    label={i18n.get['StartMinimizedSystemTray']}
-                    {...form.getInputProps('minimized', { type: 'checkbox' })}
-                  />
-                  <Checkbox
-                    disabled={!canModify}
-                    label={i18n.get['EnableSplashScreen']}
-                    {...form.getInputProps('show_splash_screen', { type: 'checkbox' })}
-                  />
-                  <Checkbox
-                    disabled={!canModify}
-                    label={i18n.get['CheckAutomaticallyForUpdates']}
-                    {...form.getInputProps('auto_update', { type: 'checkbox' })}
-                  />
-                </Stack>
-              </Accordion.Panel>
-            </Accordion.Item>
-            <Accordion.Item value='Services'>
-              <Accordion.Control>{i18n.get['Services']}</Accordion.Control>
-              <Accordion.Panel>
-                <Group>
-                  <TextInput
-                    disabled={!canModify}
-                    label={i18n.get['ServerName']}
-					placeholder={defaultConfiguration.server_name}
-                    name="server_name"
-                    sx={{ flex: 1 }}
-                    {...form.getInputProps('server_name')}
-                  />
-                  <Tooltip label={allowHtml(i18n.get['WhenEnabledUmsProfileName'])} {...defaultTooltipSettings}>
-                    <Checkbox
-                      disabled={!canModify}
-                      mt="xl"
-                      label={i18n.get['AppendProfileName']}
-                      {...form.getInputProps('append_profile_name', { type: 'checkbox' })}
-                    />
-                  </Tooltip>
-                </Group>
-                <Tooltip label={allowHtml(i18n.get['DefaultOptionIsHighlyRecommended'])} {...defaultTooltipSettings}>
-                  <Select
-                    disabled={!canModify}
-                    label={i18n.get['MediaServerEngine']}
-                    data={getI18nSelectData(selectionSettings.serverEngines)}
-                    {...form.getInputProps('server_engine')}
+                    mt="xl"
+                    label={i18n.get['AppendProfileName']}
+                    {...form.getInputProps('append_profile_name', { type: 'checkbox' })}
                   />
                 </Tooltip>
-              </Accordion.Panel>
-            </Accordion.Item>
-            <Accordion.Item value='NetworkSettingsAdvanced'>
-              <Accordion.Control>{i18n.get['NetworkSettingsAdvanced']}</Accordion.Control>
-              <Accordion.Panel>
+              </Group>
+              <Tooltip label={allowHtml(i18n.get['DefaultOptionIsHighlyRecommended'])} {...defaultTooltipSettings}>
                 <Select
                   disabled={!canModify}
-                  label={i18n.get['ForceNetworkingInterface']}
-                  data={selectionSettings.networkInterfaces}
-                  {...form.getInputProps('network_interface')}
+                  label={i18n.get['MediaServerEngine']}
+                  data={getI18nSelectData(selectionSettings.serverEngines)}
+                  {...form.getInputProps('server_engine')}
                 />
+              </Tooltip>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value='NetworkSettingsAdvanced'>
+            <Accordion.Control>{i18n.get['NetworkSettingsAdvanced']}</Accordion.Control>
+            <Accordion.Panel>
+              <Select
+                disabled={!canModify}
+                label={i18n.get['ForceNetworkingInterface']}
+                data={selectionSettings.networkInterfaces}
+                {...form.getInputProps('network_interface')}
+              />
 
+              <TextInput
+                disabled={!canModify}
+                mt="xs"
+                label={i18n.get['ForceIpServer']}
+                {...form.getInputProps('hostname')}
+              />
+
+              <TextInput
+                disabled={!canModify}
+                mt="xs"
+                label={i18n.get['ForcePortServer']}
+                {...form.getInputProps('port')}
+              />
+
+              <TextInput
+                disabled={!canModify}
+                mt="xs"
+                label={i18n.get['UseIpFilter']}
+                {...form.getInputProps('ip_filter')}
+              />
+
+              <Group mt="xs">
                 <TextInput
-                  disabled={!canModify}
-                  mt="xs"
-                  label={i18n.get['ForceIpServer']}
-                  {...form.getInputProps('hostname')}
+                  sx={{ flex: 1 }}
+                  label={i18n.get['MaximumBandwidthMbs']}
+                  disabled={!canModify || form.values['automatic_maximum_bitrate']}
+                  {...form.getInputProps('maximum_bitrate')}
                 />
-
-                <TextInput
-                  disabled={!canModify}
-                  mt="xs"
-                  label={i18n.get['ForcePortServer']}
-                  {...form.getInputProps('port')}
-                />
-
-                <TextInput
-                  disabled={!canModify}
-                  mt="xs"
-                  label={i18n.get['UseIpFilter']}
-                  {...form.getInputProps('ip_filter')}
-                />
-
-                <Group mt="xs">
-                  <TextInput
-                    sx={{ flex: 1 }}
-                    label={i18n.get['MaximumBandwidthMbs']}
-                    disabled={!canModify || form.values['automatic_maximum_bitrate']}
-                    {...form.getInputProps('maximum_bitrate')}
+                
+                <Tooltip label={allowHtml(i18n.get['ItSetsOptimalBandwidth'])} {...defaultTooltipSettings}>
+                  <Checkbox
+                    disabled={!canModify}
+                    mt="xl"
+                    label={i18n.get['UseAutomaticMaximumBandwidth']}
+                    {...form.getInputProps('automatic_maximum_bitrate', { type: 'checkbox' })}
                   />
-                  
-                  <Tooltip label={allowHtml(i18n.get['ItSetsOptimalBandwidth'])} {...defaultTooltipSettings}>
-                    <Checkbox
-                      disabled={!canModify}
-                      mt="xl"
-                      label={i18n.get['UseAutomaticMaximumBandwidth']}
-                      {...form.getInputProps('automatic_maximum_bitrate', { type: 'checkbox' })}
-                    />
-                  </Tooltip>
-                </Group>
-              </Accordion.Panel>
-            </Accordion.Item>
-            <Accordion.Item value='ExternalOutgoingTraffic'>
-              <Accordion.Control>{i18n.get['ExternalOutgoingTraffic']}</Accordion.Control>
-              <Accordion.Panel>
-                <Stack>
-                  <Tooltip label={allowHtml(i18n.get['ThisControlsWhetherUmsTry'])} {...defaultTooltipSettings}>
-                    <Checkbox
-                      disabled={!canModify}
-                      label={i18n.get['EnableExternalNetwork']}
-                      {...form.getInputProps('external_network', { type: 'checkbox' })}
-                    />
-                  </Tooltip>
-                  <Tooltip label={allowHtml(i18n.get['UsesInformationApiAllowBrowsing'])} {...defaultTooltipSettings}>
-                    <Checkbox
-                      disabled={!canModify}
-                      label={i18n.get['UseInfoFromOurApi']}
-                      {...form.getInputProps('use_imdb_info', { type: 'checkbox' })}
-                    />
                 </Tooltip>
-				</Stack>
-              </Accordion.Panel>
-            </Accordion.Item>
-            <Accordion.Item value='Renderers'>
-              <Accordion.Control>{i18n.get['Renderers']}</Accordion.Control>
-              <Accordion.Panel>
-                <Stack>
-                  <MultiSelect
+              </Group>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value='ExternalOutgoingTraffic'>
+            <Accordion.Control>{i18n.get['ExternalOutgoingTraffic']}</Accordion.Control>
+            <Accordion.Panel>
+              <Stack>
+                <Tooltip label={allowHtml(i18n.get['ThisControlsWhetherUmsTry'])} {...defaultTooltipSettings}>
+                  <Checkbox
                     disabled={!canModify}
-                    data={getI18nSelectData(selectionSettings.allRendererNames)}
-                    label={i18n.get['EnabledRenderers']}
-                    {...form.getInputProps('selected_renderers')}
+                    label={i18n.get['EnableExternalNetwork']}
+                    {...form.getInputProps('external_network', { type: 'checkbox' })}
                   />
-                  <Select
+                </Tooltip>
+                <Tooltip label={allowHtml(i18n.get['UsesInformationApiAllowBrowsing'])} {...defaultTooltipSettings}>
+                  <Checkbox
                     disabled={!canModify}
-                    sx={{ flex: 1 }}
-                    label={i18n.get['DefaultRendererWhenAutoFails']}
-                    data={getI18nSelectData(selectionSettings.enabledRendererNames)}
-                    {...form.getInputProps('renderer_default')}
-                    searchable
+                    label={i18n.get['UseInfoFromOurApi']}
+                    {...form.getInputProps('use_imdb_info', { type: 'checkbox' })}
                   />
-                  <Tooltip label={allowHtml(i18n.get['DisablesAutomaticDetection'])} {...defaultTooltipSettings}>
-                    <Checkbox
-                      disabled={!canModify}
-                      label={i18n.get['ForceDefaultRenderer']}
-                      {...form.getInputProps('renderer_force_default', { type: 'checkbox' })}
-                    />
-                  </Tooltip>
-                </Stack>
-              </Accordion.Panel>
-            </Accordion.Item>
-          </Accordion>
-        </>
+                </Tooltip>
+              </Stack>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value='Renderers'>
+            <Accordion.Control>{i18n.get['Renderers']}</Accordion.Control>
+            <Accordion.Panel>
+              <Stack>
+                <MultiSelect
+                  disabled={!canModify}
+                  data={getI18nSelectData(selectionSettings.allRendererNames)}
+                  label={i18n.get['EnabledRenderers']}
+                  {...form.getInputProps('selected_renderers')}
+                />
+                <Select
+                  disabled={!canModify}
+                  sx={{ flex: 1 }}
+                  label={i18n.get['DefaultRendererWhenAutoFails']}
+                  data={getI18nSelectData(selectionSettings.enabledRendererNames)}
+                  {...form.getInputProps('renderer_default')}
+                  searchable
+                />
+                <Tooltip label={allowHtml(i18n.get['DisablesAutomaticDetection'])} {...defaultTooltipSettings}>
+                  <Checkbox
+                    disabled={!canModify}
+                    label={i18n.get['ForceDefaultRenderer']}
+                    {...form.getInputProps('renderer_force_default', { type: 'checkbox' })}
+                  />
+                </Tooltip>
+              </Stack>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </>
     );
   }
 
@@ -389,7 +389,7 @@ export default function Settings() {
                     </Button>
                   </Tooltip>)}
                 </Group>
-				<Stack align="flex-start" mt="xs">
+                <Stack align="flex-start" mt="xs">
                   <Tooltip label={allowHtml(i18n.get['WhenEnabledPartiallyWatchVideo'])} {...defaultTooltipSettings}>
                     <Checkbox
                       disabled={!canModify}
@@ -423,7 +423,7 @@ export default function Settings() {
                       {...form.getInputProps('atz_limit')}
                     />
                   </Tooltip>
-				</Stack>
+                </Stack>
                 <Group mt="md">
                   <Select
                     sx={{ flex: 1 }}
@@ -432,7 +432,7 @@ export default function Settings() {
                     data={getI18nSelectData(selectionSettings.fullyPlayedActions)}
                     {...form.getInputProps('fully_played_action')}
                   />
-				  {(form.values['fully_played_action'] === "3" || form.values['fully_played_action'] === "4") && (
+                  {(form.values['fully_played_action'] === "3" || form.values['fully_played_action'] === "4") && (
                     <DirectoryChooser
                       disabled={!canModify}
                       label={i18n.get['DestinationFolder']}
@@ -440,7 +440,7 @@ export default function Settings() {
                       callback={form.setFieldValue}
                       formKey="fully_played_output_directory"
                     />
-				  )}
+                  )}
                 </Group>
               </Accordion.Panel>
             </Accordion.Item>
@@ -724,7 +724,6 @@ export default function Settings() {
                       </td>
                       <td>
                         <TextInput
-                          sx={{ flex: 1 }}
                           size="xs"
                           disabled={!canModify}
                           value={value.folders}
@@ -732,7 +731,6 @@ export default function Settings() {
                       </td>
                       <td>
                         <TextInput
-                          sx={{ flex: 1 }}
                           size="xs"
                           disabled={!canModify}
                           value={value.source}
