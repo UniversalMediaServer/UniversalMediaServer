86a34998d90dad41240cc5b358de049cfdee4b7d
Klaus Townsend <subjunk@gmail.com>
2022-10-03 15:01:38 +1300

    Lint and formatting

--- src/main/java/net/pms/encoders/FFMpegVideo.java
+++ src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -321,7 +321,6 @@ public class FFMpegVideo extends Engine {
 	 * @return a {@link List} of <code>String</code>s representing the FFmpeg output parameters for the renderer according
 	 * to its <code>TranscodeVideo</code> profile.
 	 */
-	@SuppressWarnings("checkstyle:EmptyBlock")
 	public synchronized List<String> getVideoTranscodeOptions(DLNAResource dlna, DLNAMediaInfo media, OutputParams params, boolean canMuxVideoWithFFmpeg) {
 		List<String> transcodeOptions = new ArrayList<>();
 		final String filename = dlna.getFileName();
