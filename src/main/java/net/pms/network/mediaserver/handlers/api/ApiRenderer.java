354dffd5dee10c302ba3e897f425a256edc9bbf3
sf666 <satfreak666@gmx.de>
2022-10-10 17:22:29 +0200

    adjusted renderer params

--- src/main/java/net/pms/network/mediaserver/handlers/api/ApiRenderer.java
+++ src/main/java/net/pms/network/mediaserver/handlers/api/ApiRenderer.java
@@ -12,7 +12,6 @@ public class ApiRenderer extends DeviceConfiguration {
 
 	@Override
 	public boolean load(File f) {
-		// TODO: These are just preliminary
 		configuration.addProperty(MEDIAPARSERV2, true);
 		configuration.addProperty(MEDIAPARSERV2_THUMB, true);
 		configuration.addProperty(SUPPORTED, "f:mpegts v:h264 a:aac-lc|aac-ltp|aac-main|aac-ssr|he-aac|ac3|eac3 m:video/mp2t");
@@ -22,10 +21,6 @@ public class ApiRenderer extends DeviceConfiguration {
 		configuration.addProperty(SUPPORTED, "f:m4a|3ga   a:aac-lc|he-aac m:audio/x-m4a");
 		configuration.addProperty(SUPPORTED, "f:wav                       m:audio/wav");
 		configuration.addProperty(SUPPORTED, "f:m4a|3ga   a:aac-lc|he-aac m:audio/x-m4a");
-		configuration.addProperty(TRANSCODE_AUDIO, MP3);
-		configuration.addProperty(TRANSCODE_VIDEO, HLSMPEGTSH264AAC);
-		configuration.addProperty(HLS_MULTI_VIDEO_QUALITY, true);
-		configuration.addProperty(HLS_VERSION, 6);
 		return true;
 	}
 
