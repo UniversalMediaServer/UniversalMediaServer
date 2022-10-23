294791c8904645598bd6eee2660febed07042650
Klaus Townsend <subjunk@gmail.com>
2022-10-24 09:05:02 +1300

    Ok then

--- react-client/.eslintrc.js
+++ react-client/.eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
             "files": ["**/**"],
             "rules": {
                 "@typescript-eslint/no-empty-function": "off",
+                "@typescript-eslint/no-unused-vars": "off",
+                "@typescript-eslint/no-explicit-any": "off",
             },
         },
     ],
