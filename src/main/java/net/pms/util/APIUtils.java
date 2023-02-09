ce2d2af18d1d6c93b9d996ec2dc508326a822cfe
Iridias <Iridias@users.noreply.github.com>
2023-02-09 04:31:22 +0100

    bugfix: avoid NPE in APIUtils.setApiMetadataVersions (#3819)

--- src/main/java/net/pms/util/APIUtils.java
+++ src/main/java/net/pms/util/APIUtils.java
@@ -163,7 +163,7 @@ public class APIUtils {
 
 				try {
 					JsonElement element = GSON.fromJson(apiResult, JsonElement.class);
-					if (element.isJsonObject()) {
+					if (element != null && element.isJsonObject()) {
 						jsonData = element.getAsJsonObject();
 					}
 				} catch (JsonSyntaxException e) {
