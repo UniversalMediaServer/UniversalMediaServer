f840aa798e1b5a8ebc416ba91b16d0afa629b6e8
SubJunk <subjunk@gmail.com>
2023-03-12 13:12:38 +1300

    Set HDR format from compatibilty info if it exists
    We are using the value to determine compatibility so this field is better

--- src/main/java/net/pms/parsers/MediaInfoParser.java
+++ src/main/java/net/pms/parsers/MediaInfoParser.java
@@ -320,15 +320,13 @@ public class MediaInfoParser {
 							}
 						}
 
-						value = MI.get(video, i, "HDR_Format");
+						value = MI.get(video, i, "HDR_Format_Compatibility");
 						if (!value.isEmpty()) {
-							if (value.startsWith("Dolby Vision")) {
+							media.setVideoHDRFormat(value);
+						} else {
+							value = MI.get(video, i, "HDR_Format");
+							if (!value.isEmpty()) {
 								media.setVideoHDRFormat(value);
-							} else {
-								value = MI.get(video, i, "HDR_Format_Compatibility");
-								if (value.startsWith("HDR10") || value.startsWith("HDR10+")) {
-									media.setVideoHDRFormat(value);
-								}
 							}
 						}
 
