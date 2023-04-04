1c5d8a60cb5f0868e0c373a5bd688274b2a1fb5b
sf666 <satfreak666@gmx.de>
2023-04-04 10:09:31 +0200

    added composer to audiotracks (db only)

--- src/main/java/net/pms/parsers/MediaInfoParser.java
+++ src/main/java/net/pms/parsers/MediaInfoParser.java
@@ -393,6 +393,8 @@ public class MediaInfoParser {
 					currentAudioTrack.setAlbumArtist(MI.get(general, 0, "Album/Performer"));
 					currentAudioTrack.setArtist(getArtist());
 					currentAudioTrack.setGenre(MI.get(general, 0, "Genre"));
+					currentAudioTrack.setComposer(MI.get(general, 0, "Composer"));
+
 					if (videoTrackCount == 0) {
 						try {
 							AudioFile af;
