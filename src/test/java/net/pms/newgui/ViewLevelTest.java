f5dce1f344817a67a90793cc14dc88b325c681d5
RichardIcecube <Richardtheicecube@gmail.com>
2023-06-11 10:35:18 -0500

    fixed a copy-pasted grammar error in the documentation under the src/test/java\net\pms/newgui folder

--- src/test/java/net/pms/newgui/ViewLevelTest.java
+++ src/test/java/net/pms/newgui/ViewLevelTest.java
@@ -29,7 +29,7 @@ public class ViewLevelTest {
 
 	@BeforeEach
 	public void setUp() throws ConfigurationException {
-		// Silence all log messages from the UMS code that is being tested
+		// Silence all log messages from the UMS code that are being tested
 		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
 		context.getLogger(Logger.ROOT_LOGGER_NAME).setLevel(Level.OFF);
 	}
