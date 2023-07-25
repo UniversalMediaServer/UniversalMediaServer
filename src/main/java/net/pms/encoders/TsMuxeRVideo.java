UniversalMediaServer/UniversalMediaServer

commit 85d9adda8ab50f3dfc93900c1ac299aeb78d8555

Fix support for Dolby Vision in MKV on LG TVs (#3974)
* Fixed H.265 muxing via tsMuxeR

* Avoid unnecessary FFmpeg engine logic when deferring to tsMuxeR

* Fix support for Dolby Vision in MKV on LG TVs

src/main/java/net/pms/encoders/TsMuxeRVideo.java
@@ -50,7 +50,7 @@ public class TsMuxeRVideo extends Engine {
 	public static final EngineId ID = StandardEngineId.TSMUXER_VIDEO;
 
 	/** The {@link Configuration} key for the custom tsMuxeR path. */
-	public static final String KEY_TSMUXER_PATH     = "tsmuxer_path";
+	public static final String KEY_TSMUXER_PATH = "tsmuxer_path";
 
 	/** The {@link Configuration} key for the tsMuxeR executable type. */
 	public static final String KEY_TSMUXER_EXECUTABLE_TYPE = "tsmuxer_executable_type";
@@ -111,7 +111,7 @@ public class TsMuxeRVideo extends Engine {
 		DLNAMediaInfo media,
 		OutputParams params
 	) throws IOException {
-		// Use device-specific pms conf
+		// Use device-specific ums conf
 		UmsConfiguration prev = configuration;
 		configuration = params.getMediaRenderer().getUmsConfiguration();
 		final String filename = dlna.getFileName();
@@ -123,7 +123,7 @@ public class TsMuxeRVideo extends Engine {
 		PipeIPCProcess[] ffAudioPipe = null;
 		ProcessWrapperImpl[] ffAudio = null;
 
-		String fps = media.getValidFps(false);
+		String fps = null;
 
 		int width  = media.getWidth();
 		int height = media.getHeight();
@@ -237,7 +237,7 @@ public class TsMuxeRVideo extends Engine {
 				"-ss", params.getTimeSeek() > 0 ? "" + params.getTimeSeek() : "0",
 				"-i", filename,
 				"-c", "copy",
-				"-f", "rawvideo",
+				"-f", codecV.equals(FormatConfiguration.H265) ? "hevc" : "rawvideo",
 				"-y",
 				ffVideoPipe.getInputPipe()
 			};
@@ -510,7 +510,7 @@ public class TsMuxeRVideo extends Engine {
 			if (configuration.isFix25FPSAvMismatch()) {
 				fps = "25";
 			}
-			pw.println(videoType + ", \"" + ffVideoPipe.getOutputPipe() + "\", " + (fps != null ? ("fps=" + fps + ", ") : "") + (width != -1 ? ("video-width=" + width + ", ") : "") + (height != -1 ? ("video-height=" + height + ", ") : "") + videoparams);
+			pw.println(videoType + ", \"" + ffVideoPipe.getOutputPipe() + "\", " + (fps != null ? ("fps=" + fps + ", ") : "") + videoparams);
 
 			if (ffAudioPipe != null && ffAudioPipe.length == 1) {
 				String timeshift = "";

src/main/java/net/pms/configuration/FormatConfiguration.java
@@ -27,6 +27,8 @@ import net.pms.dlna.DLNAMediaAudio;
 import net.pms.dlna.DLNAMediaInfo;
 import net.pms.dlna.DLNAResource;
 import net.pms.dlna.InputFile;
+import net.pms.encoders.EngineFactory;
+import net.pms.encoders.TsMuxeRVideo;
 import net.pms.formats.Format;
 import net.pms.formats.Format.Identifier;
 import net.pms.io.OutputParams;
@@ -35,6 +37,7 @@ import net.pms.renderers.Renderer;
 import net.pms.util.AudioUtils;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -374,6 +377,8 @@ public class FormatConfiguration {
 		 * @param videoWidth
 		 * @param videoHeight
 		 * @param videoBitDepth
+		 * @param videoHdrFormatInRendererFormat a sanitized HDR format based on compatibility
+		 * @param videoHdrFormat the raw HDR format, not compatibility/fallback info
 		 * @param extras map containing 0 or more key/value pairs for:
 		 *               - qpel
 		 *               - gmc
@@ -395,7 +400,8 @@ public class FormatConfiguration {
 			int videoWidth,
 			int videoHeight,
 			int videoBitDepth,
-			String videoHdrInRendererFormat,
+			String videoHdrFormatInRendererFormat,
+			String videoHdrFormatCompatibilityInRendererFormat,
 			Map<String, String> extras,
 			String subsFormat,
 			boolean isExternalSubs,
@@ -464,10 +470,63 @@ public class FormatConfiguration {
 				}
 			}
 
-			if (videoHdrInRendererFormat != null && miExtras != null && miExtras.get(MI_HDR) != null) {
-				if (!miExtras.get(MI_HDR).matcher(videoHdrInRendererFormat).matches()) {
-					LOGGER.trace("Video HDR format value \"{}\" failed to match support line {}", videoHdrInRendererFormat, supportLine);
-					return false;
+			if (videoHdrFormatInRendererFormat != null && miExtras != null && miExtras.get(MI_HDR) != null) {
+				if (!miExtras.get(MI_HDR).matcher(videoHdrFormatInRendererFormat).matches()) {
+					/**
+					 * We know now the strict HDR format is not compatible with the renderer.
+					 *
+					 * BUT the choice is more complicated because of HDR compatibility.
+					 *
+					 * Some video streams are a hybrid of Dolby Vision with a fallback.
+					 * Some TVs support ONLY the fallback (compatibility stream) while
+					 * others support the best quality (Dolby Vision) stream.
+					 *
+					 * It gets even more complicated with LG TVs because they support
+					 * Dolby Vision within MP4 and TS containers ONLY, and will play the
+					 * DV file within MKV container as just HDR (lower quality).
+					 *
+					 * So here we have logic to handle that special case, and report that
+					 * the file is incompatible, to let UMS remux the MKV file into TS
+					 * to allow the LG TV to play it as Dolby Vision.
+					 */
+					LOGGER.trace("Video HDR format value \"{}\" failed to match support line {}", videoHdrFormatInRendererFormat, supportLine);
+
+					final boolean isTsMuxeRVideoEngineActive = EngineFactory.isEngineActive(TsMuxeRVideo.ID);
+					if (!StringUtils.equalsIgnoreCase(format, "mpegts") && isTsMuxeRVideoEngineActive) {
+						/**
+						 * Calls this function again, with a TS container and without
+						 * HDR compatibility info, so we get either a STRICT match or none
+						 */
+						boolean wouldBeCompatibleInTsContainer = renderer.getFormatConfiguration().getMatchedMIMEtype(
+							"mpegts",
+							videoCodec,
+							audioCodec,
+							nbAudioChannels,
+							frequency,
+							bitrate,
+							framerate,
+							videoWidth,
+							videoHeight,
+							videoBitDepth,
+							videoHdrFormatInRendererFormat,
+							null,
+							extras,
+							subsFormat,
+							isExternalSubs,
+							renderer
+						) != null;
+
+						if (wouldBeCompatibleInTsContainer) {
+							LOGGER.trace("Video HDR format value \"{}\" is compatible in TS container, but not this container \"{}\", so will report it as incompatible to allow on-the-fly remuxing with tsMuxeR {}", videoHdrFormatInRendererFormat, format, supportLine);
+							return false;
+						}
+					}
+
+					// Last chance, see if the HDR format compatibility/fallback exists and matches
+					if (videoHdrFormatCompatibilityInRendererFormat == null || !miExtras.get(MI_HDR).matcher(videoHdrFormatCompatibilityInRendererFormat).matches()) {
+						LOGGER.trace("Video HDR format compatibility value \"{}\" also failed to match support line {}", videoHdrFormatCompatibilityInRendererFormat, supportLine);
+						return false;
+					}
 				}
 			}
 
@@ -641,6 +700,7 @@ public class FormatConfiguration {
 				media.getHeight(),
 				media.getVideoBitDepth(),
 				media.getVideoHDRFormatForRenderer(),
+				media.getVideoHDRFormatCompatibilityForRenderer(),
 				media.getExtras(),
 				dlna.getMediaSubtitle() != null ? dlna.getMediaSubtitle().getType().toString() : null,
 				dlna.getMediaSubtitle() != null && dlna.getMediaSubtitle().isExternal(),
@@ -672,6 +732,7 @@ public class FormatConfiguration {
 				media.getHeight(),
 				media.getVideoBitDepth(),
 				media.getVideoHDRFormatForRenderer(),
+				media.getVideoHDRFormatCompatibilityForRenderer(),
 				media.getExtras(),
 				dlna.getMediaSubtitle() != null ? dlna.getMediaSubtitle().getType().toString() : null,
 				dlna.getMediaSubtitle() != null && dlna.getMediaSubtitle().isExternal(),
@@ -694,6 +755,7 @@ public class FormatConfiguration {
 				media.getHeight(),
 				media.getVideoBitDepth(),
 				media.getVideoHDRFormatForRenderer(),
+				media.getVideoHDRFormatCompatibilityForRenderer(),
 				media.getExtras(),
 				dlna.getMediaSubtitle() != null ? dlna.getMediaSubtitle().getType().toString() : null,
 				dlna.getMediaSubtitle() != null && dlna.getMediaSubtitle().isExternal(),
@@ -720,6 +782,7 @@ public class FormatConfiguration {
 			0,
 			0,
 			0,
+			null,
 			null,
 			null,
 			null,
@@ -752,6 +815,7 @@ public class FormatConfiguration {
 			0,
 			media.getVideoBitDepth(),
 			media.getVideoHDRFormatForRenderer(),
+			media.getVideoHDRFormatCompatibilityForRenderer(),
 			null,
 			params.getSid().getType().name(),
 			params.getSid().isExternal(),
@@ -770,7 +834,8 @@ public class FormatConfiguration {
 		int videoWidth,
 		int videoHeight,
 		int videoBitDepth,
-		String videoHdrInRendererFormat,
+		String videoHdrFormatInRendererFormat,
+		String videoHdrFormatCompatibilityInRendererFormat,
 		Map<String, String> extras,
 		String subsFormat,
 		boolean isInternal,
@@ -790,7 +855,8 @@ public class FormatConfiguration {
 				videoWidth,
 				videoHeight,
 				videoBitDepth,
-				videoHdrInRendererFormat,
+				videoHdrFormatInRendererFormat,
+				videoHdrFormatCompatibilityInRendererFormat,
 				extras,
 				subsFormat,
 				isInternal,

src/main/java/net/pms/dlna/DLNAMediaInfo.java
@@ -312,7 +312,7 @@ public class DLNAMediaInfo implements Cloneable {
 	 */
 	public boolean isMuxable(Renderer renderer) {
 		// Make sure the file is H.264 video
-		if (isH264()) {
+		if (isH264() || isH265()) {
 			muxable = true;
 		}
 
@@ -334,7 +334,7 @@ public class DLNAMediaInfo implements Cloneable {
 		}
 
 		// Bravia does not support AVC video at less than 288px high
-		if (renderer.isBRAVIA() && height < 288) {
+		if (renderer.isBRAVIA() && isH264() && height < 288) {
 			muxable = false;
 		}
 
@@ -1771,11 +1771,14 @@ public class DLNAMediaInfo implements Cloneable {
 			if (isNotBlank(getVideoHDRFormat())) {
 				result.append(", Video HDR Format: ").append(getVideoHDRFormat());
 			}
+			if (isNotBlank(getVideoHDRFormatForRenderer())) {
+				result.append(" (").append(getVideoHDRFormatForRenderer()).append(")");
+			}
 			if (isNotBlank(getVideoHDRFormatCompatibility())) {
 				result.append(", Video HDR Format Compatibility: ").append(getVideoHDRFormatCompatibility());
 			}
-			if (isNotBlank(getVideoHDRFormatForRenderer())) {
-				result.append(" (").append(getVideoHDRFormatForRenderer()).append(")");
+			if (isNotBlank(getVideoHDRFormatCompatibilityForRenderer())) {
+				result.append(" (").append(getVideoHDRFormatCompatibilityForRenderer()).append(")");
 			}
 			if (isNotBlank(getFileTitleFromMetadata())) {
 				result.append(", File Title from Metadata: ").append(getFileTitleFromMetadata());
@@ -2205,22 +2208,23 @@ public class DLNAMediaInfo implements Cloneable {
 	}
 
 	/**
-	 * Uses the HDR format and HDR format compatibility information
-	 * to return a string that the renderer config can match.
+	 * Uses the HDR format compatibility information to return
+	 * a string that the renderer config can match if the format
+	 * failed to match.
 	 *
 	 * Note: Sometimes HDR files have a "SDR" compatibility, this means
 	 * that any player can play them, so we return null for that just like
 	 * any other SDR video.
 	 */
-	public String getVideoHDRFormatForRenderer() {
-		if (StringUtils.isBlank(videoHDRFormat) && StringUtils.isBlank(videoHDRFormatCompatibility)) {
+	public String getVideoHDRFormatCompatibilityForRenderer() {
+		if (StringUtils.isBlank(videoHDRFormatCompatibility)) {
 			return null;
 		}
 
-		String hdrValueInRendererFormat = null;
+		String hdrFormatCompatibilityInRendererFormat = null;
 		if (StringUtils.isNotBlank(videoHDRFormatCompatibility)) {
 			if (videoHDRFormatCompatibility.startsWith("Dolby Vision")) {
-				hdrValueInRendererFormat = "dolbyvision";
+				hdrFormatCompatibilityInRendererFormat = "dolbyvision";
 			} else if (
 				(
 					videoHDRFormatCompatibility.startsWith("HDR10") &&
@@ -2228,25 +2232,40 @@ public class DLNAMediaInfo implements Cloneable {
 				) ||
 				videoHDRFormatCompatibility.endsWith("HDR10") // match "HDR10+ Profile A / HDR10"
 			) {
-				hdrValueInRendererFormat = "hdr10";
+				hdrFormatCompatibilityInRendererFormat = "hdr10";
 			} else if (videoHDRFormatCompatibility.startsWith("HDR10+")) {
-				hdrValueInRendererFormat = "hdr10+";
+				hdrFormatCompatibilityInRendererFormat = "hdr10+";
 			} else if (videoHDRFormatCompatibility.startsWith("HLG")) {
-				hdrValueInRendererFormat = "hlg";
-			}
-		} else if (StringUtils.isNotBlank(videoHDRFormat)) {
-			if (videoHDRFormat.startsWith("Dolby Vision")) {
-				hdrValueInRendererFormat = "dolbyvision";
-			} else if (videoHDRFormat.startsWith("HDR10+")) {
-				hdrValueInRendererFormat = "hdr10+";
-			} else if (videoHDRFormat.startsWith("HDR10")) {
-				hdrValueInRendererFormat = "hdr10";
-			} else if (videoHDRFormat.startsWith("HLG")) {
-				hdrValueInRendererFormat = "hlg";
+				hdrFormatCompatibilityInRendererFormat = "hlg";
 			}
 		}
 
-		return hdrValueInRendererFormat;
+		return hdrFormatCompatibilityInRendererFormat;
+	}
+
+	/**
+	 * Uses the HDR format and HDR format information
+	 * to return a string that the renderer config can match.
+	 */
+	public String getVideoHDRFormatForRenderer() {
+		if (StringUtils.isBlank(videoHDRFormat)) {
+			return null;
+		}
+
+		String hdrFormatInRendererFormat = null;
+		if (StringUtils.isNotBlank(videoHDRFormat)) {
+			if (videoHDRFormat.startsWith("Dolby Vision")) {
+				hdrFormatInRendererFormat = "dolbyvision";
+			} else if (videoHDRFormat.startsWith("HDR10+")) {
+				hdrFormatInRendererFormat = "hdr10+";
+			} else if (videoHDRFormat.startsWith("HDR10")) {
+				hdrFormatInRendererFormat = "hdr10";
+			} else if (videoHDRFormat.startsWith("HLG")) {
+				hdrFormatInRendererFormat = "hlg";
+			}
+		}
+
+		return hdrFormatInRendererFormat;
 	}
 
 	public int getPlaybackCount() {

src/main/java/net/pms/encoders/FFMpegVideo.java
@@ -425,7 +425,6 @@ public class FFMpegVideo extends Engine {
 						transcodeOptions.add("copy");
 					}
 				} else {
-
 					String selectedTranscodeAccelerationMethod = null;
 
 					if (!customFFmpegOptions.contains("-c:v")) {
@@ -445,7 +444,6 @@ public class FFMpegVideo extends Engine {
 							transcodeOptions.add("-preset");
 							transcodeOptions.add("llhp");
 						}
-
 					}
 
 					if (selectedTranscodeAccelerationMethod == null || selectedTranscodeAccelerationMethod.startsWith("libx264")) {
@@ -905,6 +903,7 @@ public class FFMpegVideo extends Engine {
 		 * - The setting is enabled
 		 * - There are subtitles to transcode
 		 * - The file is not being played via the transcode folder
+		 * - The file is not Dolby Vision, because our MEncoder implementation can't handle that yet
 		 */
 		if (
 			EngineFactory.isEngineActive(MEncoderVideo.ID) &&
@@ -916,48 +915,16 @@ public class FFMpegVideo extends Engine {
 			(
 				params.getSid().getType().isText() ||
 				params.getSid().getType() == SubtitleType.VOBSUB
-			)
+			) &&
+			!(media.getVideoHDRFormatForRenderer() != null && media.getVideoHDRFormatForRenderer().equals("dolbyvision"))
 		) {
 			LOGGER.debug("Switching from FFmpeg to MEncoder to transcode subtitles because the user setting is enabled.");
 			MEncoderVideo mv = (MEncoderVideo) EngineFactory.getEngine(StandardEngineId.MENCODER_VIDEO, false, true);
 			return mv.launchTranscode(dlna, media, params);
 		}
 
-		boolean deferToTsmuxer = true;
-		boolean canMuxVideoWithFFmpeg = true;
-		String prependFfmpegTraceReason = "Not muxing the video stream with FFmpeg because ";
-		if (!(renderer instanceof OutputOverride)) {
-			if (!params.getMediaRenderer().isVideoStreamTypeSupportedInTranscodingContainer(media)) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the video codec is not the same as the transcoding goal.");
-			} else if (dlna.isInsideTranscodeFolder()) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the file is being played via a FFmpeg entry in the TRANSCODE folder.");
-			} else if (params.getSid() != null) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "we need to burn subtitles.");
-			} else if (isAviSynthEngine()) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "we are using AviSynth.");
-			} else if (media.isH264() && params.getMediaRenderer().isH264Level41Limited() && !media.isVideoWithinH264LevelLimits(newInput, params.getMediaRenderer())) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the video stream is not within H.264 level limits for this renderer.");
-			} else if ("bt.601".equals(media.getMatrixCoefficients())) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the colorspace probably isn't supported by the renderer.");
-			} else if ((params.getMediaRenderer().isKeepAspectRatio() || params.getMediaRenderer().isKeepAspectRatioTranscoding()) && !"16:9".equals(media.getAspectRatioContainer())) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the renderer needs us to add borders so it displays the correct aspect ratio of " + media.getAspectRatioContainer() + ".");
-			} else if (!params.getMediaRenderer().isResolutionCompatibleWithRenderer(media.getWidth(), media.getHeight())) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the resolution is incompatible with the renderer.");
-			} else if (media.getVideoHDRFormatForRenderer() != null && media.getVideoHDRFormatForRenderer().equals("dolbyvision")) {
-				canMuxVideoWithFFmpeg = false;
-				LOGGER.debug(prependFfmpegTraceReason + "the file is a strict Dolby Vision profile and FFmpeg seems to not preserve Dolby Vision data (worth re-checking periodically).");
-			}
-		}
-
 		// Decide whether to defer to tsMuxeR or continue to use FFmpeg
+		boolean deferToTsmuxer = true;
 		if (!(renderer instanceof OutputOverride) && configuration.isFFmpegMuxWithTsMuxerWhenCompatible()) {
 			// Decide whether to defer to tsMuxeR or continue to use FFmpeg
 			String prependTraceReason = "Not muxing the video stream with tsMuxeR via FFmpeg because ";
@@ -1014,6 +981,39 @@ public class FFMpegVideo extends Engine {
 				LOGGER.debug("Deferring from FFmpeg to tsMuxeR");
 
 				return tsMuxeRVideoInstance.launchTranscode(dlna, media, params);
+			}
+		}
+
+		boolean canMuxVideoWithFFmpeg = true;
+		String prependFfmpegTraceReason = "Not muxing the video stream with FFmpeg because ";
+		if (!(renderer instanceof OutputOverride)) {
+			if (!params.getMediaRenderer().isVideoStreamTypeSupportedInTranscodingContainer(media)) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the video codec is not the same as the transcoding goal.");
+			} else if (dlna.isInsideTranscodeFolder()) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the file is being played via a FFmpeg entry in the TRANSCODE folder.");
+			} else if (params.getSid() != null) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "we need to burn subtitles.");
+			} else if (isAviSynthEngine()) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "we are using AviSynth.");
+			} else if (media.isH264() && params.getMediaRenderer().isH264Level41Limited() && !media.isVideoWithinH264LevelLimits(newInput, params.getMediaRenderer())) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the video stream is not within H.264 level limits for this renderer.");
+			} else if ("bt.601".equals(media.getMatrixCoefficients())) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the colorspace probably isn't supported by the renderer.");
+			} else if ((params.getMediaRenderer().isKeepAspectRatio() || params.getMediaRenderer().isKeepAspectRatioTranscoding()) && !"16:9".equals(media.getAspectRatioContainer())) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the renderer needs us to add borders so it displays the correct aspect ratio of " + media.getAspectRatioContainer() + ".");
+			} else if (!params.getMediaRenderer().isResolutionCompatibleWithRenderer(media.getWidth(), media.getHeight())) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the resolution is incompatible with the renderer.");
+			} else if (media.getVideoHDRFormatForRenderer() != null && media.getVideoHDRFormatForRenderer().equals("dolbyvision")) {
+				canMuxVideoWithFFmpeg = false;
+				LOGGER.debug(prependFfmpegTraceReason + "the file is a strict Dolby Vision profile and FFmpeg seems to not preserve Dolby Vision data (worth re-checking periodically).");
 			}
 		}
 

src/test/java/net/pms/dlna/DLNAMediaInfoTest.java
@@ -271,7 +271,7 @@ public class DLNAMediaInfoTest {
 					break;
 				case 23:
 					assertEquals(
-						"Container: MKV, Size: 7799945, Overall Bitrate: 10372267, Video Tracks: 1, Video Codec: h265, Duration: 0:00:06.016, Video Resolution: 1920 x 1080, Display Aspect Ratio: 16:9, Frame Rate: 59.940, Frame Rate Mode: CFR (CFR), Frame Rate Mode Raw: CFR, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision / SMPTE ST 2086, Video HDR Format Compatibility: HDR10 / HDR10 (hdr10), Audio Tracks: 1 [Audio Codec: Enhanced AC-3, Bitrate: 640000, Channels: 6, Sample Frequency: 48000 Hz], Mime Type: video/x-matroska",
+						"Container: MKV, Size: 7799945, Overall Bitrate: 10372267, Video Tracks: 1, Video Codec: h265, Duration: 0:00:06.016, Video Resolution: 1920 x 1080, Display Aspect Ratio: 16:9, Frame Rate: 59.940, Frame Rate Mode: CFR (CFR), Frame Rate Mode Raw: CFR, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision / SMPTE ST 2086 (dolbyvision), Video HDR Format Compatibility: HDR10 / HDR10 (hdr10), Audio Tracks: 1 [Audio Codec: Enhanced AC-3, Bitrate: 640000, Channels: 6, Sample Frequency: 48000 Hz], Mime Type: video/x-matroska",
 						mediaInfo.toString()
 					);
 					break;
@@ -289,7 +289,7 @@ public class DLNAMediaInfoTest {
 					break;
 				case 26:
 					assertEquals(
-						"Container: MPEGTS, Size: 32636236, Overall Bitrate: 31110533, Video Tracks: 1, Video Codec: h265, Duration: 0:00:08.341, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Frame Rate: 23.976, Video Bit Depth: 10, Video HDR Format: Dolby Vision, Video HDR Format Compatibility: SDR, Mime Type: video/vnd.dlna.mpeg-tts",
+						"Container: MPEGTS, Size: 32636236, Overall Bitrate: 31110533, Video Tracks: 1, Video Codec: h265, Duration: 0:00:08.341, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Frame Rate: 23.976, Video Bit Depth: 10, Video HDR Format: Dolby Vision (dolbyvision), Video HDR Format Compatibility: SDR, Mime Type: video/vnd.dlna.mpeg-tts",
 						mediaInfo.toString()
 					);
 					break;
@@ -307,19 +307,19 @@ public class DLNAMediaInfoTest {
 					break;
 				case 29:
 					assertEquals(
-						"Container: MPEGTS, Size: 2448136, Overall Bitrate: 3926871, Video Tracks: 1, Video Codec: h265, Duration: 0:00:04.921, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Frame Rate: 23.976, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision / SMPTE ST 2086, Video HDR Format Compatibility: Blu-ray / HDR10 (hdr10), Mime Type: video/vnd.dlna.mpeg-tts",
+						"Container: MPEGTS, Size: 2448136, Overall Bitrate: 3926871, Video Tracks: 1, Video Codec: h265, Duration: 0:00:04.921, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Frame Rate: 23.976, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision / SMPTE ST 2086 (dolbyvision), Video HDR Format Compatibility: Blu-ray / HDR10 (hdr10), Mime Type: video/vnd.dlna.mpeg-tts",
 						mediaInfo.toString()
 					);
 					break;
 				case 30:
 					assertEquals(
-						"Container: MPEGTS, Size: 16063660, Overall Bitrate: 28310726, Video Tracks: 1, Video Codec: h265, Duration: 0:00:04.504, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision, Video HDR Format Compatibility: HLG (hlg), Mime Type: video/vnd.dlna.mpeg-tts",
+						"Container: MPEGTS, Size: 16063660, Overall Bitrate: 28310726, Video Tracks: 1, Video Codec: h265, Duration: 0:00:04.504, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision (dolbyvision), Video HDR Format Compatibility: HLG (hlg), Mime Type: video/vnd.dlna.mpeg-tts",
 						mediaInfo.toString()
 					);
 					break;
 				case 31:
 					assertEquals(
-						"Container: MKV, Size: 11413502, Overall Bitrate: 6110012, Video Tracks: 1, Video Codec: h265, Duration: 0:00:14.944, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Frame Rate: 25.000, Frame Rate Mode: CFR (CFR), Frame Rate Mode Raw: CFR, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision / SMPTE ST 2086, Video HDR Format Compatibility: HDR10 / HDR10 (hdr10), Audio Tracks: 1 [Id: 0, Language Code: eng, Audio Codec: Enhanced AC-3, Bitrate: 768000, Channels: 6, Sample Frequency: 48000 Hz], Mime Type: video/x-matroska",
+						"Container: MKV, Size: 11413502, Overall Bitrate: 6110012, Video Tracks: 1, Video Codec: h265, Duration: 0:00:14.944, Video Resolution: 3840 x 2160, Display Aspect Ratio: 16:9, Frame Rate: 25.000, Frame Rate Mode: CFR (CFR), Frame Rate Mode Raw: CFR, Matrix Coefficients: BT.2020 non-constant, Video Bit Depth: 10, Video HDR Format: Dolby Vision / SMPTE ST 2086 (dolbyvision), Video HDR Format Compatibility: HDR10 / HDR10 (hdr10), Audio Tracks: 1 [Id: 0, Language Code: eng, Audio Codec: Enhanced AC-3, Bitrate: 768000, Channels: 6, Sample Frequency: 48000 Hz], Mime Type: video/x-matroska",
 						mediaInfo.toString()
 					);
 					break;

commit 5bc067dc782d398ff10a38345f05374f2112f968

Fix bad audio sync in tsMuxeR caused by 85d9adda8ab50f3dfc93900c1ac299aeb78d8555

src/main/java/net/pms/encoders/TsMuxeRVideo.java
@@ -123,7 +123,7 @@ public class TsMuxeRVideo extends Engine {
 		PipeIPCProcess[] ffAudioPipe = null;
 		ProcessWrapperImpl[] ffAudio = null;
 
-		String fps = null;
+		String fps = media.getValidFps(false);
 
 		int width  = media.getWidth();
 		int height = media.getHeight();