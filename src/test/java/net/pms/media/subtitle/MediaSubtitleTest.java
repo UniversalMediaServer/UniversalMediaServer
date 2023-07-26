b09aab326b4131c624a13686b6b5a7acdb2cbcbc
SurfaceS <nicoooo0@outlook.com>
2023-07-26 13:16:05 +0200

    fix tests folders

--- src/test/java/net/pms/media/subtitle/MediaSubtitleTest.java
+++ src/test/java/net/pms/media/subtitle/MediaSubtitleTest.java
@@ -64,7 +64,7 @@ public class MediaSubtitleTest {
 
 	@Test
 	public void testSetExternalFile_UTF() throws Exception {
-		File file_utf8 = FileUtils.toFile(CLASS.getResource("../util/russian-utf8-without-bom.srt"));
+		File file_utf8 = FileUtils.toFile(CLASS.getResource("russian-utf8-without-bom.srt"));
 		MediaSubtitle sub1 = new MediaSubtitle();
 		sub1.setExternalFile(file_utf8);
 		assertEquals(sub1.getSubCharacterSet(), CHARSET_UTF_8);
@@ -72,7 +72,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub1.isExternalFileUtf16());
 		assertTrue(sub1.isExternalFileUtf());
 
-		File file_utf8_2 = FileUtils.toFile(CLASS.getResource("../util/russian-utf8-with-bom.srt"));
+		File file_utf8_2 = FileUtils.toFile(CLASS.getResource("russian-utf8-with-bom.srt"));
 		MediaSubtitle sub2 = new MediaSubtitle();
 		sub2.setExternalFile(file_utf8_2);
 		assertEquals(sub2.getSubCharacterSet(), CHARSET_UTF_8);
@@ -80,7 +80,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub2.isExternalFileUtf16());
 		assertTrue(sub2.isExternalFileUtf());
 
-		File file_utf16_le = FileUtils.toFile(CLASS.getResource("../util/russian-utf16-le.srt"));
+		File file_utf16_le = FileUtils.toFile(CLASS.getResource("russian-utf16-le.srt"));
 		MediaSubtitle sub3 = new MediaSubtitle();
 		sub3.setExternalFile(file_utf16_le);
 		assertEquals(sub3.getSubCharacterSet(), CHARSET_UTF_16LE);
@@ -88,7 +88,7 @@ public class MediaSubtitleTest {
 		assertTrue(sub3.isExternalFileUtf16());
 		assertTrue(sub3.isExternalFileUtf());
 
-		File file_utf16_be = FileUtils.toFile(CLASS.getResource("../util/russian-utf16-be.srt"));
+		File file_utf16_be = FileUtils.toFile(CLASS.getResource("russian-utf16-be.srt"));
 		MediaSubtitle sub4 = new MediaSubtitle();
 		sub4.setExternalFile(file_utf16_be);
 		assertEquals(sub4.getSubCharacterSet(), CHARSET_UTF_16BE);
@@ -96,7 +96,7 @@ public class MediaSubtitleTest {
 		assertTrue(sub4.isExternalFileUtf16());
 		assertTrue(sub4.isExternalFileUtf());
 
-		File file_utf32_le = FileUtils.toFile(CLASS.getResource("../util/russian-utf32-le.srt"));
+		File file_utf32_le = FileUtils.toFile(CLASS.getResource("russian-utf32-le.srt"));
 		MediaSubtitle sub5 = new MediaSubtitle();
 		sub5.setExternalFile(file_utf32_le);
 		assertEquals(sub5.getSubCharacterSet(), CHARSET_UTF_32LE);
@@ -104,7 +104,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub5.isExternalFileUtf16());
 		assertTrue(sub5.isExternalFileUtf());
 
-		File file_utf32_be = FileUtils.toFile(CLASS.getResource("../util/russian-utf32-be.srt"));
+		File file_utf32_be = FileUtils.toFile(CLASS.getResource("russian-utf32-be.srt"));
 		MediaSubtitle sub6 = new MediaSubtitle();
 		sub6.setExternalFile(file_utf32_be);
 		assertEquals(sub6.getSubCharacterSet(), CHARSET_UTF_32BE);
@@ -112,7 +112,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub6.isExternalFileUtf16());
 		assertTrue(sub6.isExternalFileUtf());
 
-		File file_utf8_3 = FileUtils.toFile(CLASS.getResource("../util/english-utf8-with-bom.srt"));
+		File file_utf8_3 = FileUtils.toFile(CLASS.getResource("english-utf8-with-bom.srt"));
 		MediaSubtitle sub7 = new MediaSubtitle();
 		sub7.setExternalFile(file_utf8_3);
 		assertEquals(sub7.getSubCharacterSet(), CHARSET_UTF_8);
@@ -123,7 +123,7 @@ public class MediaSubtitleTest {
 
 	@Test
 	public void testSetExternalFile_charset() throws Exception {
-		File file_big5 = FileUtils.toFile(CLASS.getResource("../util/chinese-big5.srt"));
+		File file_big5 = FileUtils.toFile(CLASS.getResource("chinese-big5.srt"));
 		MediaSubtitle sub1 = new MediaSubtitle();
 		sub1.setExternalFile(file_big5);
 		assertEquals(sub1.getSubCharacterSet(), CHARSET_BIG5);
@@ -132,7 +132,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub1.isExternalFileUtf());
 		assertEquals(sub1.getLang(), "zh");
 
-		File file_gb18030 = FileUtils.toFile(CLASS.getResource("../util/chinese-gb18030.srt"));
+		File file_gb18030 = FileUtils.toFile(CLASS.getResource("chinese-gb18030.srt"));
 		MediaSubtitle sub2 = new MediaSubtitle();
 		sub2.setExternalFile(file_gb18030);
 		assertEquals(sub2.getSubCharacterSet(), CHARSET_GB18030);
@@ -141,7 +141,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub2.isExternalFileUtf());
 		assertEquals(sub2.getLang(), "zh");
 
-		File file_cp1250 = FileUtils.toFile(CLASS.getResource("../util/czech-cp1250.srt"));
+		File file_cp1250 = FileUtils.toFile(CLASS.getResource("czech-cp1250.srt"));
 		MediaSubtitle sub3 = new MediaSubtitle();
 		sub3.setExternalFile(file_cp1250);
 		assertEquals(sub3.getSubCharacterSet(), CHARSET_WINDOWS_1250);
@@ -150,7 +150,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub3.isExternalFileUtf());
 		assertEquals(sub3.getLang(), "cs");
 
-		File file_cp1251 = FileUtils.toFile(CLASS.getResource("../util/russian-cp1251.srt"));
+		File file_cp1251 = FileUtils.toFile(CLASS.getResource("russian-cp1251.srt"));
 		MediaSubtitle sub4 = new MediaSubtitle();
 		sub4.setExternalFile(file_cp1251);
 		assertEquals(sub4.getSubCharacterSet(), CHARSET_WINDOWS_1251);
@@ -159,7 +159,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub4.isExternalFileUtf());
 		assertEquals(sub4.getLang(), "ru");
 
-		File file_iso_8859_2 = FileUtils.toFile(CLASS.getResource("../util/hungarian-iso-8859-2.srt"));
+		File file_iso_8859_2 = FileUtils.toFile(CLASS.getResource("hungarian-iso-8859-2.srt"));
 		MediaSubtitle sub5 = new MediaSubtitle();
 		sub5.setExternalFile(file_iso_8859_2);
 		assertEquals(sub5.getSubCharacterSet(), CHARSET_ISO_8859_2);
@@ -168,7 +168,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub5.isExternalFileUtf());
 		assertEquals(sub5.getLang(), "hu");
 
-		File file_koi8_r = FileUtils.toFile(CLASS.getResource("../util/russian-koi8-r.srt"));
+		File file_koi8_r = FileUtils.toFile(CLASS.getResource("russian-koi8-r.srt"));
 		MediaSubtitle sub6 = new MediaSubtitle();
 		sub6.setExternalFile(file_koi8_r);
 		assertEquals(sub6.getSubCharacterSet(), CHARSET_KOI8_R);
@@ -177,7 +177,7 @@ public class MediaSubtitleTest {
 		assertFalse(sub6.isExternalFileUtf());
 		assertEquals(sub6.getLang(), "ru");
 
-		File file_iso_8859_8 = FileUtils.toFile(CLASS.getResource("../util/hebrew-iso-8859-8.srt"));
+		File file_iso_8859_8 = FileUtils.toFile(CLASS.getResource("hebrew-iso-8859-8.srt"));
 		MediaSubtitle sub7 = new MediaSubtitle();
 		sub7.setExternalFile(file_iso_8859_8);
 		assertEquals(sub7.getSubCharacterSet(), CHARSET_ISO_8859_8);
@@ -189,7 +189,7 @@ public class MediaSubtitleTest {
 
 	@Test
 	public void testSetExternalFile_bitmapSubs() throws Exception {
-		File file_cp1251 = FileUtils.toFile(CLASS.getResource("../util/russian-cp1251.srt"));
+		File file_cp1251 = FileUtils.toFile(CLASS.getResource("russian-cp1251.srt"));
 
 		MediaSubtitle sub1 = new MediaSubtitle();
 		sub1.setType(VOBSUB);
@@ -214,7 +214,7 @@ public class MediaSubtitleTest {
 
 	@Test
 	public void testSetExternalFile_textSubs() throws Exception {
-		File file_cp1251 = FileUtils.toFile(CLASS.getResource("../util/russian-cp1251.srt"));
+		File file_cp1251 = FileUtils.toFile(CLASS.getResource("russian-cp1251.srt"));
 
 		MediaSubtitle sub1 = new MediaSubtitle();
 		sub1.setType(SUBRIP);
@@ -229,7 +229,7 @@ public class MediaSubtitleTest {
 
 	@Test
 	public void testIsEmbedded() throws Exception {
-		File file_cp1251 = FileUtils.toFile(CLASS.getResource("../util/russian-cp1251.srt"));
+		File file_cp1251 = FileUtils.toFile(CLASS.getResource("russian-cp1251.srt"));
 		MediaSubtitle sub1 = new MediaSubtitle();
 		sub1.setType(SUBRIP);
 		sub1.setExternalFile(file_cp1251);
