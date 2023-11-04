2a968b11803ebcc14996e861cefa5dc57016a58c
Klaus Townsend <subjunk@gmail.com>
2023-11-04 15:34:06 +1300

    Ugh string comparison

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -1567,8 +1567,8 @@ public class PMS {
 	public static boolean isRunningTests() {
 		LOGGER.info("CI is set to " + System.getenv("CI"));
 		LOGGER.info("1 " + (System.getProperty("surefire.real.class.path") != null ? "true" : "false"));
-		LOGGER.info("2 " + (System.getenv("CI") == "true" ? "true" : "false"));
-		return System.getProperty("surefire.real.class.path") != null || System.getenv("CI") == "true";
+		LOGGER.info("2 " + (System.getenv("CI").equals("true") ? "true" : "false"));
+		return System.getProperty("surefire.real.class.path") != null || System.getenv("CI").equals("true");
 	}
 
 	/**
