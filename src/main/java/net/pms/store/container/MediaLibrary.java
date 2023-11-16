a6fc4573c7a896f02daa6203353fc1a195dd4461
SurfaceS <nicoooo0@outlook.com>
2023-11-16 20:19:59 +0100

    fix MediaLibrary AudioFolder

--- src/main/java/net/pms/store/container/MediaLibrary.java
+++ src/main/java/net/pms/store/container/MediaLibrary.java
@@ -258,7 +258,7 @@ public class MediaLibrary extends MediaLibraryAbstract {
 			new String[]{
 				SELECT_DISTINCT + COALESCE_ARTIST + AS + "ARTIST" + FROM_FILES + ", " + MediaTableAudioMetadata.TABLE_NAME + WHERE + MediaTableFiles.TABLE_COL_ID + EQUAL + MediaTableAudioMetadata.TABLE_COL_FILEID + AND + FORMAT_TYPE_AUDIO + ORDER_BY + "ARTIST" + ASC,
 				SELECT_DISTINCT + MediaTableAudioMetadata.TABLE_COL_ALBUM + FROM_FILES + ", " + MediaTableAudioMetadata.TABLE_NAME + WHERE + MediaTableFiles.TABLE_COL_ID + EQUAL + MediaTableAudioMetadata.TABLE_COL_FILEID + AND + FORMAT_TYPE_AUDIO + AND + COALESCE_ARTIST + EQUAL + "'${0}'" + ORDER_BY + MediaTableAudioMetadata.TABLE_COL_ALBUM + ASC,
-				SELECT + MediaTableFiles.TABLE_COL_FILENAME + ", " + MediaTableFiles.TABLE_COL_MODIFIED + FROM_FILES + ", " + MediaTableAudioMetadata.TABLE_NAME + WHERE + MediaTableFiles.TABLE_COL_ID + EQUAL + MediaTableAudioMetadata.TABLE_COL_FILEID + AND + FORMAT_TYPE_AUDIO + AND + COALESCE_ARTIST + EQUAL + "'${1}' AND " + MediaTableAudioMetadata.TABLE_COL_ALBUM + EQUAL + "'${0}'" + ORDER_BY + MediaTableAudioMetadata.TABLE_COL_TRACK + ASC + ", " + MediaTableFiles.TABLE_COL_FILENAME + ASC
+				SELECT + MediaTableFiles.TABLE_COL_FILENAME + ", " + MediaTableFiles.TABLE_COL_MODIFIED + FROM_FILES + ", " + MediaTableAudioMetadata.TABLE_NAME + WHERE + MediaTableFiles.TABLE_COL_ID + EQUAL + MediaTableAudioMetadata.TABLE_COL_FILEID + AND + FORMAT_TYPE_AUDIO + AND + COALESCE_ARTIST + EQUAL + "'${1}'" + AND + MediaTableAudioMetadata.TABLE_COL_ALBUM + EQUAL + "'${0}'" + ORDER_BY + MediaTableAudioMetadata.TABLE_COL_TRACK + ASC + ", " + MediaTableFiles.TABLE_COL_FILENAME + ASC
 			},
 			new int[]{TEXTS, TEXTS, FILES}
 		);
