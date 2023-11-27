b0e8db2698c0a5996f30123bb14c59c185f0319f
ik666 <satfreak666@gmx.de>
2023-11-27 14:03:37 +0100

    no transcoding for media scanner needed

--- src/main/java/net/pms/store/StoreItem.java
+++ src/main/java/net/pms/store/StoreItem.java
@@ -58,6 +58,7 @@ import net.pms.media.video.MediaVideo;
 import net.pms.network.HTTPResource;
 import net.pms.parsers.Parser;
 import net.pms.renderers.Renderer;
+import net.pms.renderers.devices.MediaScannerDevice;
 import net.pms.store.container.ChapterFileTranscodeVirtualFolder;
 import net.pms.store.container.OpenSubtitleFolder;
 import net.pms.store.item.DVDISOTitle;
@@ -432,6 +433,9 @@ public abstract class StoreItem extends StoreResource {
 	 * @return An engine if transcoding or null if streaming
 	 */
 	public Engine resolveEngine() {
+		if (renderer instanceof MediaScannerDevice) {
+			return null;
+		}
 		// Use device-specific conf, if any
 		boolean parserV2 = mediaInfo != null && renderer.isUseMediaInfo();
 		Engine resolvedEngine;
