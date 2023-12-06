f240476abd34c5811c47bd0363d014078aa64d20
ik666 <satfreak666@gmx.de>
2023-12-06 15:38:26 +0100

    read correct tags for artists and album-artists

--- src/main/java/net/pms/parsers/MediaInfoParser.java
+++ src/main/java/net/pms/parsers/MediaInfoParser.java
@@ -1187,8 +1187,16 @@ public class MediaInfoParser {
 		MediaAudioMetadata audioMetadata = new MediaAudioMetadata();
 		audioMetadata.setSongname(StreamContainer.getTrack(MI, 0));
 		audioMetadata.setAlbum(StreamContainer.getAlbum(MI, 0));
-		audioMetadata.setAlbumArtist(StreamContainer.getAlbumPerformer(MI, 0));
-		audioMetadata.setArtist(StreamContainer.getPerformer(MI, 0));
+		String albumArtists = StreamContainer.getAlbumArtists(MI, 0);
+		if (StringUtils.isAllBlank(albumArtists)) {
+			albumArtists = StreamContainer.getAlbumPerformer(MI, 0);
+		}
+		audioMetadata.setAlbumArtist(albumArtists);
+		String artists = StreamContainer.getArtists(MI, 0);
+		if (StringUtils.isAllBlank(artists)) {
+			artists = StreamContainer.getPerformer(MI, 0);
+		}
+		audioMetadata.setArtist(artists);
 		audioMetadata.setGenre(StreamContainer.getGenre(MI, 0));
 		audioMetadata.setComposer(StreamContainer.getComposer(MI, 0));
 		audioMetadata.setConductor(StreamContainer.getConductor(MI, 0));
