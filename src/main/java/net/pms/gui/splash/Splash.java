7f2f9af4740284115bae5265b86bc736ca267f75
SurfaceS <nicoooo0@outlook.com>
2024-01-11 21:42:37 +0100

    ConcurrentHashMap instead of HashMap as multiple thread can access it.

--- src/main/java/net/pms/gui/splash/Splash.java
+++ src/main/java/net/pms/gui/splash/Splash.java
@@ -14,7 +14,6 @@
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
 package net.pms.gui.splash;
 
 import java.awt.Color;
