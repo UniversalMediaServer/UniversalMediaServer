672899715aa01931f24ec79f69b9a28bba904bcf
SurfaceS <SurfaceS@users.noreply.github.com>
2024-05-19 00:35:52 +0200

    main fix #4653 (#4666)
    * log the file that createResourceFromFile fail.
    
    * log a bit more.
    
    * allow hidden SharedContent root.

--- src/main/java/net/pms/store/MediaScanner.java
+++ src/main/java/net/pms/store/MediaScanner.java
@@ -169,8 +169,12 @@ public class MediaScanner implements SharedContentListener {
 		List<SharedContent> sharedContents = SharedContentConfiguration.getSharedContentArray();
 		for (SharedContent sharedContent : sharedContents) {
 			if (sharedContent instanceof FolderContent folder && folder.getFile() != null && folder.isActive()) {
-				StoreResource realSystemFileResource = RENDERER.getMediaStore().createResourceFromFile(folder.getFile());
-				RENDERER.getMediaStore().addChild(realSystemFileResource);
+				StoreResource realSystemFileResource = RENDERER.getMediaStore().createResourceFromFile(folder.getFile(), true);
+				if (realSystemFileResource != null) {
+					RENDERER.getMediaStore().addChild(realSystemFileResource);
+				} else {
+					LOGGER.trace("createResourceFromFile has failed for {}", folder.getFile());
+				}
 			}
 		}
 	}
