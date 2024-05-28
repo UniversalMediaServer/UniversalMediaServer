1d6e66b9aab36a0436a56fc3a8f00846aeb1a608
SurfaceS <SurfaceS@users.noreply.github.com>
2024-05-28 10:28:39 +0200

    Update AACP.java

--- src/main/java/net/pms/formats/audio/AACP.java
+++ src/main/java/net/pms/formats/audio/AACP.java
@@ -1,9 +1,22 @@
+/*
+ * This file is part of Universal Media Server, based on PS3 Media Server.
+ *
+ * This program is a free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2 of the License only.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package net.pms.formats.audio;
 
 public class AACP extends AudioBase {
-
-	public AACP() {
-	}
 
 	/**
 	 * {@inheritDoc}
@@ -18,11 +31,12 @@ public class AACP extends AudioBase {
 	 */
 	@Override
 	public String[] getSupportedExtensions() {
-		return new String[] {"aacp"};
+		return new String[]{"aacp"};
 	}
 
 	@Override
 	public boolean transcodable() {
 		return false;
 	}
+
 }
