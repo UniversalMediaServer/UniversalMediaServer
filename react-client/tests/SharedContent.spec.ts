cb12583410da90bef516049d44cf9d379ceaec35
SubJunk <subjunk@gmail.com>
2024-08-04 15:39:13 +1200

    Fix tests

--- react-client/tests/SharedContent.spec.ts
+++ react-client/tests/SharedContent.spec.ts
@@ -18,12 +18,12 @@ test('should be able to add a YouTube channel as a video feed', async ({ page })
 
   await expect(page).toHaveScreenshot();
 
-  await page.getByLabel('Type').click();
+  await page.getByLabel('Type').first().click();
   await page.getByText('Video feed').click();
 
   await page.getByLabel('Source/URL').fill('https://www.youtube.com/@kurzgesagt');
 
-  await page.getByText('Add').click();
+  await page.getByText('Add', { exact: true }).click();
 
   await expect(page.getByText('Kurzgesagt – In a Nutshell')).toBeVisible();
 
