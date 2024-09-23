0d79714ea2000e1ab8d4921ed22bc0f6f963037d
SurfaceS <nicoooo0@outlook.com>
2024-09-23 12:30:52 +0200

    main fix #4968 fix Ambiguous URI path separator

--- src/main/java/net/pms/store/StoreResource.java
+++ src/main/java/net/pms/store/StoreResource.java
@@ -925,6 +925,7 @@ public abstract class StoreResource implements Cloneable, Runnable {
 			if (s == null) {
 				return "";
 			}
+			s = s.replace('\\', '-').replace('/', '-');
 			return URLEncoder.encode(s, StandardCharsets.UTF_8);
 		} catch (IllegalArgumentException e) {
 			LOGGER.debug("Error while URL encoding \"{}\": {}", s, e.getMessage());
