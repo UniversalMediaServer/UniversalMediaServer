c57c9b4c512fd439fbd7f5b4f7006385fc1841ca
SurfaceS <nicoooo0@outlook.com>
2025-03-31 09:53:20 +0200

    lint

--- react-client/tests/StandalonePlayer.spec.ts
+++ react-client/tests/StandalonePlayer.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test'
 
 test.use({
-  baseURL: 'http://localhost:9002'
-});
+  baseURL: 'http://localhost:9002',
+})
 
 test('should be able to browse the Standalone Player', async ({ page }) => {
   await page.goto('/')
