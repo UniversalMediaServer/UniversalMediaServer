d3fa135ab9dbb503443b7ffc0fb14f5334359eb6
SurfaceS <nicoooo0@outlook.com>
2025-04-03 16:01:08 +0200

    fix the need for this initial delay/reload

--- react-client/tests/global.teardown.ts
+++ react-client/tests/global.teardown.ts
@@ -2,6 +2,5 @@ import { test as setup } from '@playwright/test'
 
 setup('Put back authentication', async ({ page }) => {
   await page.goto('/accounts')
-
-  await page.getByText('Enable').click()
+  await page.getByRole('button', { name: 'Enable' }).click()
 })
