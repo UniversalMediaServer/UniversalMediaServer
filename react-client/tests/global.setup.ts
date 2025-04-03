9aec6b8b2134c67fe0a0d8b316fb8cca9ee9a78e
SurfaceS <nicoooo0@outlook.com>
2025-04-03 16:12:02 +0200

    lint

--- react-client/tests/global.setup.ts
+++ react-client/tests/global.setup.ts
@@ -2,6 +2,6 @@ import { test as setup } from '@playwright/test'
 
 setup('Disable authentication', async ({ page }) => {
   await page.goto('/')
-  await page.getByRole('button', { name: 'Disable authentication'}).click()
-  await page.getByRole('button', { name: 'Confirm'}).click()
+  await page.getByRole('button', { name: 'Disable authentication' }).click()
+  await page.getByRole('button', { name: 'Confirm' }).click()
 })
