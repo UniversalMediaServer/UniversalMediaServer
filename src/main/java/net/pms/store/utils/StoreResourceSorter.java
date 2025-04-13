2828bcaf314c085a5aeb6dbc8900e501bfe78942
SurfaceS <nicoooo0@outlook.com>
2025-04-13 16:14:52 +0200

    added same check for string

--- src/main/java/net/pms/store/utils/StoreResourceSorter.java
+++ src/main/java/net/pms/store/utils/StoreResourceSorter.java
@@ -211,7 +211,7 @@ public class StoreResourceSorter {
 
 	private static int compareToNormalizedString(String str1, String str2, boolean asc) {
 		if (str2 == null) {
-			return 1;
+			return str1 == null ? 0 : 1;
 		} else if (str1 == null) {
 			return -1;
 		}
