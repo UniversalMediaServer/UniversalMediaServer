fbc868d1d3a17e0f31ec1839423dfa7dc44a556a
SubJunk <subjunk@gmail.com>
2025-04-23 11:41:37 +1200

    Log any errors

--- react-client/tests/global.setup.ts
+++ react-client/tests/global.setup.ts
@@ -1,6 +1,17 @@
 import { test as setup } from '@playwright/test'
 
 setup('Disable authentication', async ({ page }) => {
+  page.on("pageerror", (err) => {
+    console.log(err.message)
+  })
+  page.on("console", (message) => {
+    if (message.type() === "error") {
+      console.error(message)
+    }
+  })
+  await page.evaluate(() => {
+    console.error("hello from the browser")
+  })
   await page.goto('/')
   await page.getByRole('button', { name: 'Disable authentication' }).click()
   await page.getByRole('button', { name: 'Confirm' }).click()
