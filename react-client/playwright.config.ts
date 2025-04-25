fb2fc3bbc5693af856db0b8a3e3bd0804b775b42
SubJunk <subjunk@gmail.com>
2025-04-25 17:18:47 +1200

    cleanup

--- react-client/playwright.config.ts
+++ react-client/playwright.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
 
-  timeout: 60_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
