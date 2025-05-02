afc0d94b59e8d9b4f4e1bbcd8a02e2270bd9d67c
SubJunk <subjunk@gmail.com>
2025-05-03 08:37:52 +1200

    Trivial web change

--- react-client/tests/SharedContent.spec.ts
+++ react-client/tests/SharedContent.spec.ts
@@ -26,6 +26,4 @@ test('should be able to add a YouTube channel as a video feed', async ({ page })
   await page.getByText('Add', { exact: true }).click()
 
   await expect(page.getByText('Kurzgesagt – In a Nutshell')).toBeVisible()
-
-  // await expect(page).toHaveScreenshot()
 })
