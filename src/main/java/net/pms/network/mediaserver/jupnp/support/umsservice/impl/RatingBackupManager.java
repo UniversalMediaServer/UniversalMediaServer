eabc68ac96fe0358e217a94bc389c7877a4516cd
ik666 <satfreak666@gmx.de>
2025-09-30 12:58:40 +0200

    Initial rating backup manager version

---
+++ src/main/java/net/pms/network/mediaserver/jupnp/support/umsservice/impl/RatingBackupManager.java
@@ -0,0 +1,91 @@
+package net.pms.network.mediaserver.jupnp.support.umsservice.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.pms.configuration.UmsConfiguration;
+import net.pms.database.MediaDatabase;
+
+public class RatingBackupManager {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RatingBackupManager.class.getName());
+	private final static String RATINGS_READ = "SELECT FILES.RUID, AUDIO_METADATA.RATING FROM FILES LEFT JOIN AUDIO_METADATA ON FILES.ID = AUDIO_METADATA.FILEID WHERE RATING is not null";
+	private final static String RATINGS_WRITE = "UPDATE AUDIO_METADATA a SET a.RATING = ? WHERE a.FILEID = (SELECT ID from FILES WHERE RUID = ?)";
+
+	public RatingBackupManager() {
+
+	}
+
+	public static void backupRatings() {
+		Properties p = new Properties();
+		Connection c = MediaDatabase.getConnectionIfAvailable();
+		try (PreparedStatement selectStatement = c.prepareStatement(RATINGS_READ)) {
+			try (ResultSet rs = selectStatement.executeQuery()) {
+				while (rs.next()) {
+					p.put(rs.getString("ruid"), rs.getInt("rating"));
+				}
+				String backupFilename = getBackupFilename();
+				try (FileOutputStream fs = new FileOutputStream(new File(backupFilename))) {
+					DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+					p.store(fs, "ratings backup from " + formatter.format(LocalDate.now()));
+				} catch (IOException e) {
+					LOGGER.error("backup rating failed", e);
+				}
+			}
+		} catch (SQLException e) {
+			LOGGER.error("backup rating failed", e);
+		}
+	}
+
+	public static void restoreRating() {
+		String backupFilename = getBackupFilename();
+		try (FileInputStream fis = new FileInputStream(new File(backupFilename))) {
+			Properties p = new Properties();
+			p.load(fis);
+			LOGGER.debug("[restoreRating] read {} items.", p.size());
+			int updated = 0;
+			int skipped = 0;
+
+			Connection c = MediaDatabase.getConnectionIfAvailable();
+
+			for (Object oruid : p.keySet()) {
+				try (PreparedStatement updateStatement = c.prepareStatement(RATINGS_WRITE)) {
+					String ruid = (String) oruid;
+					Integer rating = Integer.parseInt(ruid);
+					updateStatement.setLong(1, rating);
+					updateStatement.setString(2, ruid);
+					if (updateStatement.executeUpdate() == 1) {
+						updated++;
+					} else if (updateStatement.executeUpdate() > 1) {
+						LOGGER.warn("Got multiple results for RUID : '{}'. This shouldn't happen.", ruid);
+						updated++;
+					} else {
+						skipped++;
+					}
+				} catch (SQLException e) {
+					LOGGER.warn("restoreRating failed for entry {} ", oruid, e);
+				}
+			}
+		} catch (IOException e) {
+			LOGGER.error("restore rating failed", e);
+		}
+	}
+
+	private static String getBackupFilename() {
+		String dir = FilenameUtils.concat(UmsConfiguration.getProfileDirectory(), "database_backup");
+		String backupFilename = FilenameUtils.concat(dir, "ratings_backup");
+		return backupFilename;
+	}
+
+}
