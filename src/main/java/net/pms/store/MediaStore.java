82b7f1eae3736a265501ab4e0f29beb291a108a9
SubJunk <SubJunk@users.noreply.github.com>
2025-10-06 12:25:08 +1300

    Revert "FileWatcher UPDATE event / invalidate cache (#5777)" (#5784)
    This reverts commit ded3dd7584771cd386c261b5f914d0ca109632de.

--- src/main/java/net/pms/store/MediaStore.java
+++ src/main/java/net/pms/store/MediaStore.java
@@ -726,21 +726,6 @@ public class MediaStore extends StoreContainer {
 		}
 	}
 
-	/**
-	 * File can have different structure after an update. Therefore, read file metadata again.
-	 * @param file
-	 */
-	public void fileUpdated(File file) {
-		for (StoreResource storeResource : findSystemFileResources(file)) {
-			if (storeResource instanceof RealFile rf) {
-				rf.setMediaInfo(null);
-				MediaInfoStore.removeMediaEntryFromCache(file.getAbsolutePath());
-				rf.resolve();
-			}
-		}
-	}
-
-
 	private StoreResource findResourceFromFile(List<StoreResource> resources, File file) {
 		if (file == null || file.isHidden() || !file.canRead() || !(file.isFile() || file.isDirectory())) {
 			return null;
@@ -972,4 +957,5 @@ public class MediaStore extends StoreContainer {
 			Thread.sleep(100);
 		}
 	}
+
 }
