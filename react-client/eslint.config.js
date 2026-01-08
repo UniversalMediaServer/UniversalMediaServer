008c5d06d34debbcfb21fa1780e29ddabfc9e7b2
SubJunk <subjunk@gmail.com>
2026-01-08 14:12:40 +1300

    Config cleanup

--- react-client/eslint.config.js
+++ react-client/eslint.config.js
@@ -1,12 +1,16 @@
-import tseslint from 'typescript-eslint'
-import reactPlugin from 'eslint-plugin-react'
-import hooksPlugin from 'eslint-plugin-react-hooks'
-import refreshPlugin from 'eslint-plugin-react-refresh'
+import eslint from '@eslint/js'
 import stylisticPlugin from '@stylistic/eslint-plugin'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
 import { defineConfig } from 'eslint/config'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import refreshPlugin from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
   {
     ignores: [
       '.yarn/**',
@@ -15,40 +19,25 @@ export default defineConfig(
     ],
   },
   {
-    files: ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2021,
-      },
-    },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      'react': reactPlugin,
-      'react-hooks': hooksPlugin,
       'react-refresh': refreshPlugin,
       '@stylistic': stylisticPlugin,
       'jsx-a11y': jsxA11y,
+      'react': reactPlugin,
     },
     rules: {
-      ...tseslint.plugin.configs['eslint-recommended'].rules,
-      ...tseslint.plugin.configs.recommended.rules,
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       ...reactPlugin.configs['jsx-runtime'].rules,
-      ...hooksPlugin.configs.recommended.rules,
       'react-hooks/exhaustive-deps': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/static-components': 'off',
+      'react-hooks/immutability': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       ...jsxA11y.flatConfigs.recommended.rules,
       ...stylisticPlugin.configs.recommended.rules,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
     },
   },
 )
