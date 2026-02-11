407f8221a7bcfcd9f91c4ed4ecd31aa6d4fedf66
ik666 <satfreak666@gmx.de>
2026-02-11 10:34:27 +0100

    change implementation to JDK 17 compatibility.

--- src/main/java/net/pms/util/ResourceIdentifier.java
+++ src/main/java/net/pms/util/ResourceIdentifier.java
@@ -17,9 +17,8 @@
 package net.pms.util;
 
 import java.io.File;
-import java.lang.foreign.Arena;
-import java.lang.foreign.MemorySegment;
 import java.net.URI;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
@@ -37,9 +36,8 @@ public class ResourceIdentifier {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ResourceIdentifier.class);
 
-	// 100 MB threshold for small files
-	private static final int SMALL_BYTES_THRESHOLD = 1024 * 1024 * 100;
-
+	private static final int SMALL_BYTES_THRESHOLD = 1024 * 1024 * 100; // 100 MB
+	private static final int BIG_BYTES_THRESHOLD = Integer.MAX_VALUE;
 
 	private static final String LOG_RUID_CREATE = "Creating ruid ({}) for \"{}\"";
 	private static final String LOG_RUID_RESULTS = "RUID for \"{}\": {}";
@@ -68,8 +66,11 @@ public class ResourceIdentifier {
 			long fileSize = file.length();
 			if (fileSize < SMALL_BYTES_THRESHOLD) {
 				return getSmallFileIdentifier(file, fileSize);
-			} else {
+			}
+			if (fileSize <= BIG_BYTES_THRESHOLD) {
 				return getBigFileIdentifier(file);
+			} else {
+				return getVeryBigFileIdentifier(file);
 			}
 		}
 		return getTextIdentifier(uri);
@@ -93,12 +94,87 @@ public class ResourceIdentifier {
 
 	/**
 	 * Calculates the XXH3 hash and returns the value as a 16 character hex
-	 * string.
+	 * string. File has to be smaller than 2 GB for correct hashing.
+	 *
+	 * This method can be removed, when switching to Java 21 or above (see comment below).
 	 *
 	 * @param file the file to identify
 	 * @return the pseudo unique file identifier.
 	 * @throws IOException
 	 */
+	private static String getBigFileIdentifier(final File file) {
+		String pathname = file.getAbsolutePath();
+		try (FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+			MappedByteBuffer mbb = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+			String ruid = Long.toHexString(LongHashFunction.xx3().hashBytes(mbb));
+			LOGGER.trace(LOG_RUID_RESULTS, pathname, ruid);
+			return ruid;
+		} catch (Exception ex) {
+			LOGGER.error(LOG_RUID_ERROR, "big file", pathname);
+			LOGGER.trace("", ex);
+		}
+		return null;
+	}
+
+	/**
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex for files larger than 2 GB.
+	 * Will process the file in chunks of 2 GB to avoid memory issues.
+	 *
+	 * This method can be removed, when switching to Java 21 or above (see comment below).
+	 *
+	 * @param file
+	 * @return
+	 */
+	private static String getVeryBigFileIdentifier(final File file) {
+		String pathname = file.getAbsolutePath();
+		final long fileSize = file.length();
+		final long maxChunkSize = Integer.MAX_VALUE;
+		long acc = LongHashFunction.xx3().hashLongs(new long[]{fileSize, maxChunkSize});
+		long offset = 0L;
+		long chunkIndex = 0L;
+		try (FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+			while (offset < fileSize) {
+				long remaining = fileSize - offset;
+				long chunkSize = Math.min(remaining, maxChunkSize);
+				MappedByteBuffer mbb = fileChannel.map(FileChannel.MapMode.READ_ONLY, offset, chunkSize);
+				long chunkHash = LongHashFunction.xx3().hashBytes(mbb);
+				acc = LongHashFunction.xx3().hashLongs(new long[]{acc, chunkHash, chunkSize, chunkIndex});
+				offset += chunkSize;
+				chunkIndex++;
+			}
+			String ruid = Long.toHexString(acc);
+			LOGGER.trace(LOG_RUID_RESULTS, pathname, ruid);
+			return ruid;
+		} catch (Exception ex) {
+			LOGGER.error(LOG_RUID_ERROR, "very big file", pathname);
+			LOGGER.trace("", ex);
+		}
+		return null;
+	}
+
+	// Use the method below when Java 21 or above is used, as it can handle files larger than 2 GB without chunking.
+	// The methods "getVeryBigFileIdentifier" and "getBigFileIdentifier" above this comment can be removed at that point and replaced by
+	// the "getBigFileIdentifier" below this comment. The "BIG_BYTES_THRESHOLD" constant can also be removed, as it will no longer be needed.
+	//
+	// Action to do with usage of JDK 21 LTS :
+	// =======================================================
+	// We still need to add the "--enable-preview" flag to the runtime, because the
+	// "Foreign Function & Memory (FFM) API" used in the method below is still in preview in JDK 21. Replace the option
+	// "--add-opens java.base/sun.nio.ch=ALL-UNNAMED" with "--enable-preview" in the pom.xml, UMS.launch and Docker files.
+	//
+	// Action to do with usage of JDK 25 LTS or above :
+	// =======================================================
+	// Remove the option "--add-opens java.base/sun.nio.ch=ALL-UNNAMED" in the pom.xml, UMS.launch and Docker files.
+
+	/**
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex. It can be applied to files larger than 2 GB, as
+	 * it uses the Foreign Function & Memory (FFM) API to map the file into memory.
+	 *
+	 * @param file the file to identify
+	 * @return the pseudo unique file identifier.
+	 * @throws IOException
+	 */
+	/*
 	private static String getBigFileIdentifier(final File file) {
 		try (Arena arena = Arena.ofConfined(); FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
 
@@ -114,7 +190,8 @@ public class ResourceIdentifier {
 			LOGGER.trace("", ex);
 		}
 		return null;
-	}
+	} */
+
 
 	/**
 	 * Calculates the XXH3 hash and returns the value as a 16 character hex
