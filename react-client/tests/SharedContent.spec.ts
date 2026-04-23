05fe0b5c59c989a78dbec502d821d999a8c7c9ba
SubJunk <subjunk@gmail.com>
2026-04-24 08:27:52 +1200

    Disable YouTube tests (Fixes #6124)
    YouTube itself has significant daily downtimes on their RSS feeds which is how this feature works, so until we figure out another way to support YouTube, it's too flaky to test for

--- react-client/tests/SharedContent.spec.ts
+++ react-client/tests/SharedContent.spec.ts
@@ -8,31 +8,31 @@ test('should be able to navigate to Shared Content', async ({ page }) => {
   await expect(page).toHaveURL(/.*shared/)
 })
 
-test('should be able to add a YouTube channel as a video feed', async ({ page }) => {
-  await page.goto('/shared')
+// test('should be able to add a YouTube channel as a video feed', async ({ page }) => {
+//   await page.goto('/shared')
 
-  await page.getByText('Add new shared content').click()
+//   await page.getByText('Add new shared content').click()
 
-  await page.getByLabel('Type').first().click()
+//   await page.getByLabel('Type').first().click()
 
-  await page.getByRole('option', { name: 'Video feed' }).locator('span').click()
+//   await page.getByRole('option', { name: 'Video feed' }).locator('span').click()
 
-  await page.getByLabel('Source/URL').fill('https://www.youtube.com/@kurzgesagt')
+//   await page.getByLabel('Source/URL').fill('https://www.youtube.com/@kurzgesagt')
 
-  await page.getByText('Add', { exact: true }).click()
+//   await page.getByText('Add', { exact: true }).click()
 
-  await expect(page.getByText('Kurzgesagt – In a Nutshell')).toBeVisible()
+//   await expect(page.getByText('Kurzgesagt – In a Nutshell')).toBeVisible()
 
-  await page.getByText('Save').click()
-})
+//   await page.getByText('Save').click()
+// })
 
-test('should be able to select a YouTube video to watch', async ({ page }) => {
-  await page.goto('/player')
+// test('should be able to select a YouTube video to watch', async ({ page }) => {
+//   await page.goto('/player')
 
-  await page.getByText('Kurzgesagt – In a Nutshell').click()
-  // will update this part of test once fix has been made
-  await page.locator('.thumbnail-container').first().click()
-  await page.getByText('Play', { exact: true }).click()
-  await page.screenshot({ path: 'screenshot.png' })
-  // await expect(page.getByTitle('Play Video')).toBeVisible()
-})
+//   await page.getByText('Kurzgesagt – In a Nutshell').click()
+//   // will update this part of test once fix has been made
+//   await page.locator('.thumbnail-container').first().click()
+//   await page.getByText('Play', { exact: true }).click()
+//   await page.screenshot({ path: 'screenshot.png' })
+//   // await expect(page.getByTitle('Play Video')).toBeVisible()
+// })
