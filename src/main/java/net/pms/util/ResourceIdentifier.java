698f26f8b7707b377295cb5ac458a8e145d8c04a
ik666 <8244468+ik666@users.noreply.github.com>
2026-06-21 01:27:32 +0200

    Switched RUID hash implementation to xx3hash (#6195)
    * added xxh3 libraries
    
    * Implemented xxh3 hash test
    
    * increased blocksize
    
    * adjusted chunk to 256mb for very large files.
    
    * Switched to JDK 25, for a stable Memory API.
    
    * modified comment
    
    * removed x86 builds (32 bit)
    
    * change implementation to JDK 17 compatibility.
    
    * Revert "removed x86 builds (32 bit)"
    
    This reverts commit 3a259a77c48cf9f7a5d86a3f941aa88437be169a.
    
    * reset ruid values
    
    * lint error
    
    ---------
    
    Co-authored-by: ik666 <satfreak666@gmx.de>

--- src/main/java/net/pms/util/ResourceIdentifier.java
+++ src/main/java/net/pms/util/ResourceIdentifier.java
@@ -16,21 +16,18 @@
  */
 package net.pms.util;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.codec.digest.Blake3;
+import java.nio.file.StandardOpenOption;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.openhft.hashing.LongHashFunction;
 
 /**
  * @author Surf@ceS
@@ -39,22 +36,9 @@ public class ResourceIdentifier {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ResourceIdentifier.class);
 
-	/**
-	 * The number of buffer block to analyze.
-	 *
-	 * ex : 1024 will analyze file block of 4194304 bytes (4 MB)
-	 */
-	private static final int BUFFER_COUNT = 1024;
-	/**
-	 * The number of file block to analyze.
-	 *
-	 * Should not be less than 2 (start/end of the file).
-	 */
-	private static final int BLOCKS_COUNT = 5;
+	private static final int SMALL_BYTES_THRESHOLD = 1024 * 1024 * 100; // 100 MB
+	private static final int BIG_BYTES_THRESHOLD = Integer.MAX_VALUE;
 
-	private static final int BLOCKS_DIVIDER = BLOCKS_COUNT - 1;
-	private static final int BUFFER_SIZE = 4096;
-	private static final int PROCESS_BYTES = BUFFER_SIZE * BUFFER_COUNT * BLOCKS_COUNT;
 	private static final String LOG_RUID_CREATE = "Creating ruid ({}) for \"{}\"";
 	private static final String LOG_RUID_RESULTS = "RUID for \"{}\": {}";
 	private static final String LOG_RUID_ERROR = "Error creating ruid ({}) for \"{}\"";
@@ -66,7 +50,7 @@ public class ResourceIdentifier {
 	}
 
 	/**
-	 * Calculates the Blake3 digest and returns the value as a 32 character hex
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex
 	 * string.
 	 *
 	 * @param uri the file/url to identify
@@ -80,68 +64,52 @@ public class ResourceIdentifier {
 		File file = getFile(uri);
 		if (file != null && file.exists() && file.isFile()) {
 			long fileSize = file.length();
-			if (fileSize > PROCESS_BYTES) {
-				return getBigFileIdentifier(file, fileSize);
-			} else {
+			if (fileSize < SMALL_BYTES_THRESHOLD) {
 				return getSmallFileIdentifier(file, fileSize);
+			}
+			if (fileSize <= BIG_BYTES_THRESHOLD) {
+				return getBigFileIdentifier(file);
+			} else {
+				return getVeryBigFileIdentifier(file);
 			}
 		}
 		return getTextIdentifier(uri);
 	}
 
 	private static File getFile(final String uri) {
-		//try URI
+		// try URI
 		try {
 			return new File(URI.create(uri));
 		} catch (IllegalArgumentException es) {
-			//not a file URI
+			// not a file URI
 		}
-		//try path
+		// try path
 		try {
 			return Path.of(uri).toFile();
 		} catch (InvalidPathException e) {
-			//not a path
+			// not a path
 		}
 		return null;
 	}
 
 	/**
-	 * Calculates the Blake3 digest and returns the value as a 32 character hex
-	 * string.
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex
+	 * string. File has to be smaller than 2 GB for correct hashing.
 	 *
-	 * Will process 5 blocks of 4Mb including start and end files bytes.
+	 * This method can be removed, when switching to Java 21 or above (see comment below).
 	 *
 	 * @param file the file to identify
 	 * @return the pseudo unique file identifier.
 	 * @throws IOException
 	 */
-	private static String getBigFileIdentifier(final File file, final long fileSize) {
+	private static String getBigFileIdentifier(final File file) {
 		String pathname = file.getAbsolutePath();
-		LOGGER.debug(LOG_RUID_CREATE, "big file", pathname);
-		Blake3 blake3 = Blake3.initHash();
-		try (InputStream is = Files.newInputStream(file.toPath())) {
-			//put file size
-			blake3.update(ByteBuffer.wrap(new byte[8]).putLong(fileSize).array());
-			//we will hash 4 block of the file
-			long skipper = (fileSize - PROCESS_BYTES) / BLOCKS_DIVIDER;
-			int count = 0;
-			byte[] buffer = new byte[BUFFER_SIZE];
-			int read;
-			while ((read = is.read(buffer)) != -1) {
-				blake3.update(buffer, 0, read);
-				count++;
-				if (count == BUFFER_COUNT) {
-					//we proceed 4 Mb, move to next block
-					count = 0;
-					is.skip(skipper);
-				}
-			}
-			byte[] hash = new byte[32];
-			blake3.doFinalize(hash);
-			String ruid = Hex.encodeHexString(hash);
+		try (FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+			MappedByteBuffer mbb = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+			String ruid = Long.toHexString(LongHashFunction.xx3().hashBytes(mbb));
 			LOGGER.trace(LOG_RUID_RESULTS, pathname, ruid);
 			return ruid;
-		} catch (IOException ex) {
+		} catch (Exception ex) {
 			LOGGER.error(LOG_RUID_ERROR, "big file", pathname);
 			LOGGER.trace("", ex);
 		}
@@ -149,7 +117,84 @@ public class ResourceIdentifier {
 	}
 
 	/**
-	 * Calculates the Blake3 digest and returns the value as a 32 character hex
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex for files larger than 2 GB.
+	 * Will process the file in chunks of 2 GB to avoid memory issues.
+	 *
+	 * This method can be removed, when switching to Java 21 or above (see comment below).
+	 *
+	 * @param file
+	 * @return
+	 */
+	private static String getVeryBigFileIdentifier(final File file) {
+		String pathname = file.getAbsolutePath();
+		final long fileSize = file.length();
+		final long maxChunkSize = Integer.MAX_VALUE;
+		long acc = LongHashFunction.xx3().hashLongs(new long[]{fileSize, maxChunkSize});
+		long offset = 0L;
+		long chunkIndex = 0L;
+		try (FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+			while (offset < fileSize) {
+				long remaining = fileSize - offset;
+				long chunkSize = Math.min(remaining, maxChunkSize);
+				MappedByteBuffer mbb = fileChannel.map(FileChannel.MapMode.READ_ONLY, offset, chunkSize);
+				long chunkHash = LongHashFunction.xx3().hashBytes(mbb);
+				acc = LongHashFunction.xx3().hashLongs(new long[]{acc, chunkHash, chunkSize, chunkIndex});
+				offset += chunkSize;
+				chunkIndex++;
+			}
+			String ruid = Long.toHexString(acc);
+			LOGGER.trace(LOG_RUID_RESULTS, pathname, ruid);
+			return ruid;
+		} catch (Exception ex) {
+			LOGGER.error(LOG_RUID_ERROR, "very big file", pathname);
+			LOGGER.trace("", ex);
+		}
+		return null;
+	}
+
+	// Use the method below when Java 21 or above is used, as it can handle files larger than 2 GB without chunking.
+	// The methods "getVeryBigFileIdentifier" and "getBigFileIdentifier" above this comment can be removed at that point and replaced by
+	// the "getBigFileIdentifier" below this comment. The "BIG_BYTES_THRESHOLD" constant can also be removed, as it will no longer be needed.
+	//
+	// Action to do with usage of JDK 21 LTS :
+	// =======================================================
+	// We still need to add the "--enable-preview" flag to the runtime, because the
+	// "Foreign Function & Memory (FFM) API" used in the method below is still in preview in JDK 21. Replace the option
+	// "--add-opens java.base/sun.nio.ch=ALL-UNNAMED" with "--enable-preview" in the pom.xml, UMS.launch and Docker files.
+	//
+	// Action to do with usage of JDK 25 LTS or above :
+	// =======================================================
+	// Remove the option "--add-opens java.base/sun.nio.ch=ALL-UNNAMED" in the pom.xml, UMS.launch and Docker files.
+
+	/**
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex. It can be applied to files larger than 2 GB, as
+	 * it uses the Foreign Function & Memory (FFM) API to map the file into memory.
+	 *
+	 * @param file the file to identify
+	 * @return the pseudo unique file identifier.
+	 * @throws IOException
+	 */
+	/*
+	private static String getBigFileIdentifier(final File file) {
+		try (Arena arena = Arena.ofConfined(); FileChannel channel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
+
+			MemorySegment segment = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size(), arena);
+
+			long hash = LongHashFunction.xx3().hashMemory(segment.address(), segment.byteSize());
+
+			String ruid = Long.toHexString(hash);
+			LOGGER.trace(LOG_RUID_RESULTS, file.getAbsolutePath(), ruid);
+			return ruid;
+		} catch (Exception ex) {
+			LOGGER.error(LOG_RUID_ERROR, "big file", file.getAbsolutePath());
+			LOGGER.trace("", ex);
+		}
+		return null;
+	} */
+
+
+	/**
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex
 	 * string.
 	 *
 	 * Will process the entire file bytes.
@@ -160,30 +205,19 @@ public class ResourceIdentifier {
 	 */
 	private static String getSmallFileIdentifier(final File file, final long fileSize) {
 		String pathname = file.getAbsolutePath();
-		LOGGER.debug(LOG_RUID_CREATE, "small file", pathname);
-		Blake3 blake3 = Blake3.initHash();
-		try (InputStream is = Files.newInputStream(file.toPath())) {
-			//put file size
-			blake3.update(ByteBuffer.wrap(new byte[8]).putLong(fileSize).array());
-			byte[] buffer = new byte[BUFFER_SIZE];
-			int read;
-			while ((read = is.read(buffer)) != -1) {
-				blake3.update(buffer, 0, read);
-			}
-			byte[] hash = new byte[32];
-			blake3.doFinalize(hash);
-			String ruid = Hex.encodeHexString(hash);
+		try {
+			String ruid = Long.toHexString(LongHashFunction.xx3().hashBytes(Files.readAllBytes(file.toPath())));
 			LOGGER.trace(LOG_RUID_RESULTS, pathname, ruid);
 			return ruid;
-		} catch (IOException ex) {
+		} catch (Exception e) {
 			LOGGER.error(LOG_RUID_ERROR, "small file", pathname);
-			LOGGER.trace("", ex);
+			LOGGER.trace("", e);
 		}
 		return null;
 	}
 
 	/**
-	 * Calculates the Blake3 digest and returns the value as a 32 character hex
+	 * Calculates the XXH3 hash and returns the value as a 16 character hex
 	 * string.
 	 *
 	 * Will process the entire text bytes.
@@ -193,25 +227,15 @@ public class ResourceIdentifier {
 	 * @throws IOException
 	 */
 	private static String getTextIdentifier(final String text) {
-		LOGGER.debug(LOG_RUID_CREATE, "text", text);
-		Blake3 blake3 = Blake3.initHash();
-		byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
-		try (InputStream is = new ByteArrayInputStream(bytes)) {
-			//put bytes size
-			blake3.update(ByteBuffer.wrap(new byte[8]).putLong(bytes.length).array());
-			byte[] buffer = new byte[BUFFER_SIZE];
-			int read;
-			while ((read = is.read(buffer)) != -1) {
-				blake3.update(buffer, 0, read);
-			}
-			byte[] hash = new byte[32];
-			blake3.doFinalize(hash);
-			String ruid = Hex.encodeHexString(hash);
+		try {
+			LOGGER.debug(LOG_RUID_CREATE, "text", text);
+			long hash = LongHashFunction.xx3().hashChars(text);
+			String ruid = Long.toHexString(hash);
 			LOGGER.trace(LOG_RUID_RESULTS, text, ruid);
 			return ruid;
-		} catch (IOException ex) {
+		} catch (Exception e) {
 			LOGGER.error(LOG_RUID_ERROR, "text", text);
-			LOGGER.trace("", ex);
+			LOGGER.trace("", e);
 		}
 		return null;
 	}
