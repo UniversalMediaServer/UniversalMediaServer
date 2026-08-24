2d8cc25f2610ecb0bb0f139607c044628ec39c3b
ik666 <satfreak666@gmx.de>
2026-08-24 08:15:47 +0200

    fix broken import

--- src/main/java/net/pms/parsers/YoutubeParser.java
+++ src/main/java/net/pms/parsers/YoutubeParser.java
@@ -7,7 +7,7 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.databind.JsonNode;
