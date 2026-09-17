165f3763a66657b6bc4c1a28a35372b0d4101199
ik666 <satfreak666@gmx.de>
2026-09-17 16:14:24 +0200

    find more docs

--- src/main/java/net/pms/database/lucene/UmsFullTextLucene.java
+++ src/main/java/net/pms/database/lucene/UmsFullTextLucene.java
@@ -458,11 +458,7 @@ public class UmsFullTextLucene extends FullText {
 				Analyzer analyzer = access.writer.getAnalyzer();
 				StandardQueryParser parser = new StandardQueryParser(analyzer);
 				Query query = parser.parse(text, LUCENE_FIELD_DATA);
-				// Lucene insists on a hard limit and will not provide
-				// a total hits value. Take at least 100 which is
-				// an optimal limit for Lucene as any more
-				// will trigger writing results to disk.
-				int maxResults = (limit == 0 ? 100 : limit) + offset;
+				int maxResults = (limit == 0 ? Math.max(searcher.count(query), 1) : limit) + offset;
 				TopDocs docs = searcher.search(query, maxResults);
 				long totalHits;
 				try {
